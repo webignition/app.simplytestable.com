687e885a23d5f12dba77f5f815adf2fd79775626
Jon Cram <jon@webignition.net>
2012-07-12 14:44:25 +0100

    Added DoctrineMigrationsBundle to appkernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new SimplyTestable\ApiBundle\SimplyTestableApiBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
