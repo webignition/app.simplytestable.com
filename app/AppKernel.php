e32fcf5615d6c285aed8211b07e2dc6302c4ce4c
Jon Cram <jon@webignition.net>
2012-07-12 13:20:12 +0100

    Enabled fosuserbundle in kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new SimplyTestable\ApiBundle\SimplyTestableApiBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
