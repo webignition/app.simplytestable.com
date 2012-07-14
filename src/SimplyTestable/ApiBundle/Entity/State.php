3a74cadda986b8083370fb3e37ca9ae5328dbac4
Jon Cram <jon@webignition.net>
2012-07-14 14:14:55 +0100

    generated getters and setters for SimplyTestable/ApiBundle/Entity/State

--- src/SimplyTestable/ApiBundle/Entity/State.php
+++ src/SimplyTestable/ApiBundle/Entity/State.php
@@ -36,4 +36,58 @@ class State
      * @ORM\OneToOne(targetEntity="State")
      */
     protected $nextState;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return State
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nextState
+     *
+     * @param SimplyTestable\ApiBundle\Entity\State $nextState
+     * @return State
+     */
+    public function setNextState(\SimplyTestable\ApiBundle\Entity\State $nextState = null)
+    {
+        $this->nextState = $nextState;
+        return $this;
+    }
+
+    /**
+     * Get nextState
+     *
+     * @return SimplyTestable\ApiBundle\Entity\State 
+     */
+    public function getNextState()
+    {
+        return $this->nextState;
+    }
 }
