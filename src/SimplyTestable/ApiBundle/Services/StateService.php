982c315eea3ac3fc965c75924cc6e0284fe87f55
Jon Cram <jon@webignition.net>
2012-07-16 17:30:27 +0100

    Added StateService

---
+++ src/SimplyTestable/ApiBundle/Services/StateService.php
@@ -0,0 +1,66 @@
+<?php
+namespace SimplyTestable\ApiBundle\Services;
+
+use Doctrine\ORM\EntityManager;
+use SimplyTestable\ApiBundle\Entity\State;
+
+
+class StateService extends EntityService {
+    
+    const ENTITY_NAME = 'SimplyTestable\ApiBundle\Entity\State';
+    
+    /**
+     *
+     * @return string
+     */
+    protected function getEntityName() {
+        return self::ENTITY_NAME;
+    }
+
+    
+    /**
+     * @param string $name
+     * @return \SimplyTestable\ApiBundle\Entity\State
+     */
+    public function fetch($name) {        
+        if (!$this->has($name)) {
+            $this->create($name);
+        }
+
+        return $this->find($name);
+    }
+    
+    
+    /**
+     *
+     * @param string $name
+     * @return \SimplyTestable\ApiBundle\Entity\State 
+     */
+    public function find($name) {
+        return $this->getEntityRepository()->findOneByName($name);
+    }
+    
+    
+    /**
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public function has($name) {
+        return !is_null($this->find($name));
+    }
+    
+    
+    /**
+     *
+     * @param string $name
+     * @return \SimplyTestable\ApiBundle\Entity\State
+     */
+    public function create($name) {
+        $state = new State();
+        $state->setName($name);
+        
+        $this->persistAndFlush($name);
+        return $state;
+    }
+}
