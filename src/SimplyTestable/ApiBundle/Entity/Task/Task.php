6d291f24c8ecaff99f9fbf790a798c9b91dd93e7
Jon Cram <jon@webignition.net>
2012-07-24 17:43:10 +0100

    Generated getters/setters for Task

--- src/SimplyTestable/ApiBundle/Entity/Task/Task.php
+++ src/SimplyTestable/ApiBundle/Entity/Task/Task.php
@@ -69,4 +69,124 @@ class Task
      * @ORM\JoinColumn(name="tasktype_id", referencedColumnName="id", nullable=false)
      */
     protected $type;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param text $url
+     * @return Task
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return text 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set job
+     *
+     * @param SimplyTestable\ApiBundle\Entity\Job\Job $job
+     * @return Task
+     */
+    public function setJob(\SimplyTestable\ApiBundle\Entity\Job\Job $job)
+    {
+        $this->job = $job;
+        return $this;
+    }
+
+    /**
+     * Get job
+     *
+     * @return SimplyTestable\ApiBundle\Entity\Job\Job 
+     */
+    public function getJob()
+    {
+        return $this->job;
+    }
+
+    /**
+     * Set state
+     *
+     * @param SimplyTestable\ApiBundle\Entity\State $state
+     * @return Task
+     */
+    public function setState(\SimplyTestable\ApiBundle\Entity\State $state)
+    {
+        $this->state = $state;
+        return $this;
+    }
+
+    /**
+     * Get state
+     *
+     * @return SimplyTestable\ApiBundle\Entity\State 
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * Set worker
+     *
+     * @param SimplyTestable\ApiBundle\Entity\Worker $worker
+     * @return Task
+     */
+    public function setWorker(\SimplyTestable\ApiBundle\Entity\Worker $worker = null)
+    {
+        $this->worker = $worker;
+        return $this;
+    }
+
+    /**
+     * Get worker
+     *
+     * @return SimplyTestable\ApiBundle\Entity\Worker 
+     */
+    public function getWorker()
+    {
+        return $this->worker;
+    }
+
+    /**
+     * Set type
+     *
+     * @param SimplyTestable\ApiBundle\Entity\Task\Type\Type $type
+     * @return Task
+     */
+    public function setType(\SimplyTestable\ApiBundle\Entity\Task\Type\Type $type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return SimplyTestable\ApiBundle\Entity\Task\Type\Type 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 }
