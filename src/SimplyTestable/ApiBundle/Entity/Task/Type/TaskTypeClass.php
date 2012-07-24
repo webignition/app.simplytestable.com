30dab9947da2e3b94e296809d4ad038fd51cceba
Jon Cram <jon@webignition.net>
2012-07-24 13:28:15 +0100

    TaskTypeClass - make description nullable

--- src/SimplyTestable/ApiBundle/Entity/Task/Type/TaskTypeClass.php
+++ src/SimplyTestable/ApiBundle/Entity/Task/Type/TaskTypeClass.php
@@ -32,7 +32,7 @@ class TaskTypeClass
     /**
      *
      * @var string
-     * @ORM\Column(type="text", nullable=false)
+     * @ORM\Column(type="text", name="description", nullable=true)
      */
     protected $description;
     
