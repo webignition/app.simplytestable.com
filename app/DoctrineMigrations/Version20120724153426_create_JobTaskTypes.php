de3de12a8ed4c52b0aa11426b45acb7da0c02eef
Jon Cram <jon@webignition.net>
2012-07-27 21:29:31 +0100

    Moved primary key alteration to where table is created

--- app/DoctrineMigrations/Version20120724153426_create_JobTaskTypes.php
+++ app/DoctrineMigrations/Version20120724153426_create_JobTaskTypes.php
@@ -13,7 +13,12 @@ class Version20120724153426_create_JobTaskTypes extends BaseMigration
     public function up(Schema $schema)
     {
         $this->statements['mysql'] = array(
-            "CREATE TABLE JobTaskTypes (tasktype_id INT NOT NULL, job_id INT NOT NULL, INDEX IDX_6520FB147D6EFC3 (tasktype_id), INDEX IDX_6520FB14BE04EA9 (job_id), PRIMARY KEY(tasktype_id, job_id)) ENGINE = InnoDB",
+            "CREATE TABLE JobTaskTypes (
+                tasktype_id INT NOT NULL,
+                job_id INT NOT NULL,
+                INDEX IDX_6520FB147D6EFC3 (tasktype_id),
+                INDEX IDX_6520FB14BE04EA9 (job_id),
+                PRIMARY KEY(job_id, tasktype_id)) ENGINE = InnoDB",
             "ALTER TABLE JobTaskTypes ADD CONSTRAINT FK_6520FB147D6EFC3 FOREIGN KEY (tasktype_id) REFERENCES TaskType (id)",
             "ALTER TABLE JobTaskTypes ADD CONSTRAINT FK_6520FB14BE04EA9 FOREIGN KEY (job_id) REFERENCES Job (id)"
         );
@@ -22,7 +27,7 @@ class Version20120724153426_create_JobTaskTypes extends BaseMigration
             "CREATE TABLE JobTaskTypes (
                 tasktype_id INT NOT NULL,
                 job_id INT NOT NULL,
-                PRIMARY KEY (tasktype_id, job_id),
+                PRIMARY KEY (job_id, tasktype_id),
                 FOREIGN KEY(tasktype_id) REFERENCES TaskType (id)
                 FOREIGN KEY(job_id) REFERENCES Job (id))",
             "CREATE INDEX IDX_6520FB147D6EFC3 ON JobTaskTypes (tasktype_id)",
