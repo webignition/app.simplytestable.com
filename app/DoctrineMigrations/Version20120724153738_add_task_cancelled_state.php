ced74287687caace873e4625abf0d194e4a87ef4
Jon Cram <jon@webignition.net>
2012-07-27 12:35:44 +0100

    Added 'task-cancelled' state

---
+++ app/DoctrineMigrations/Version20120724153738_add_task_cancelled_state.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use SimplyTestable\ApiBundle\Migration\EntityModificationMigration,
+    Doctrine\DBAL\Schema\Schema,
+    SimplyTestable\ApiBundle\Entity\State;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20120724153736_add_job_cancelled_state extends EntityModificationMigration
+{
+    public function postUp(Schema $schema)
+    {        
+        $state_completed = new State();
+        $state_completed->setName('job-cancelled');        
+        $this->getEntityManager()->persist($state_completed);
+        $this->getEntityManager()->flush();      
+    }
+
+    public function postDown(Schema $schema)
+    {
+        $stateNames = array(
+            'job-cancelled'
+        );
+        
+        foreach ($stateNames as $stateName) {
+            $state = $this->getEntityManager()->getRepository('SimplyTestable\ApiBundle\Entity\State')->findOneByName($stateName);
+            $this->getEntityManager()->remove($state);
+            $this->getEntityManager()->flush();
+        }
+    }
+}
