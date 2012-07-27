062c6490d7d796100da8881355a06571b8a67ec9
Jon Cram <jon@webignition.net>
2012-07-27 13:30:50 +0100

    Inject StateService into TaskSevice

--- src/SimplyTestable/ApiBundle/Services/TaskService.php
+++ src/SimplyTestable/ApiBundle/Services/TaskService.php
@@ -2,6 +2,8 @@
 namespace SimplyTestable\ApiBundle\Services;
 
 use Doctrine\ORM\EntityManager;
+use SimplyTestable\ApiBundle\Entity\Task\Task;
+use SimplyTestable\ApiBundle\Services\StateService;
 
 class TaskService extends EntityService {
     
@@ -11,9 +13,35 @@ class TaskService extends EntityService {
     
     /**
      *
+     * @var \SimplyTestable\ApiBundle\Services\StateService
+     */
+    private $stateService;
+    
+    
+    /**
+     *
+     * @param EntityManager $entityManager
+     * @param \SimplyTestable\ApiBundle\Services\StateService $stateService 
+     */
+    public function __construct(
+            EntityManager $entityManager,
+            \SimplyTestable\ApiBundle\Services\StateService $stateService)
+    {
+        parent::__construct($entityManager);        
+        $this->stateService = $stateService;
+    }    
+    
+    
+    /**
+     *
      * @return string
      */
     protected function getEntityName() {
         return self::ENTITY_NAME;
+    }
+    
+    
+    public function cancel(Task $task) {
+        $task->setState();
     }
 }
