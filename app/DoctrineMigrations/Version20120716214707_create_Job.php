e70a879212f8059e5dadccec0383d8dcd4647146
Jon Cram <jon@webignition.net>
2012-07-27 20:53:54 +0100

    Moved create Job to own migration

---
+++ app/DoctrineMigrations/Version20120716214707_create_Job.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Application\Migrations;
+
+use SimplyTestable\ApiBundle\Migration\BaseMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20120716214707_create_Job extends BaseMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->statements['mysql'] = array(
+            "CREATE TABLE Job (
+                id INT AUTO_INCREMENT NOT NULL,
+                user_id INT NOT NULL,
+                website_id INT NOT NULL,
+                state_id INT NOT NULL,
+                startDateTime DATETIME NOT NULL,
+                INDEX IDX_C395A618A76ED395 (user_id),
+                INDEX IDX_C395A61818F45C82 (website_id),
+                INDEX IDX_C395A6185D83CC1 (state_id),
+                PRIMARY KEY(id)) ENGINE = InnoDB",
+            "ALTER TABLE Job ADD CONSTRAINT FK_C395A618A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)",
+            "ALTER TABLE Job ADD CONSTRAINT FK_C395A61818F45C82 FOREIGN KEY (website_id) REFERENCES WebSite (id)",
+            "ALTER TABLE Job ADD CONSTRAINT FK_C395A6185D83CC1 FOREIGN KEY (state_id) REFERENCES State (id)"
+        );
+        
+        $this->statements['sqlite'] = array(
+            "CREATE TABLE Job (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                user_id INT NOT NULL,
+                website_id INT NOT NULL,
+                state_id INT NOT NULL,
+                startDateTime DATETIME NOT NULL,
+                FOREIGN KEY(user_id) REFERENCES fos_user (id),
+                FOREIGN KEY(website_id) REFERENCES WebSite (id),
+                FOREIGN KEY(state_id) REFERENCES State (id))", 
+            "CREATE INDEX IDX_C395A618A76ED395 ON Job (user_id)",
+            "CREATE INDEX IDX_C395A61818F45C82 ON Job (website_id)",
+            "CREATE INDEX IDX_C395A6185D83CC1 ON Job (state_id)"
+        ); 
+        
+        parent::up($schema);
+    }
+   
+
+    public function down(Schema $schema)
+    {   
+        $this->addCommonStatement("DROP TABLE Job");      
+        
+        parent::down($schema);
+    }    
+}
--- app/DoctrineMigrations/Version20120716214726_create_TaskType_Task.php
+++ app/DoctrineMigrations/Version20120716214726_create_TaskType_Task.php
@@ -8,21 +8,11 @@ use SimplyTestable\ApiBundle\Migration\BaseMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20120716214726_create_Job_TaskType_Task extends BaseMigration
+class Version20120716214726_create_TaskType_Task extends BaseMigration
 {
     public function up(Schema $schema)
     {
         $this->statements['mysql'] = array(
-            "CREATE TABLE Job (
-                id INT AUTO_INCREMENT NOT NULL,
-                user_id INT NOT NULL,
-                website_id INT NOT NULL,
-                state_id INT NOT NULL,
-                startDateTime DATETIME NOT NULL,
-                INDEX IDX_C395A618A76ED395 (user_id),
-                INDEX IDX_C395A61818F45C82 (website_id),
-                INDEX IDX_C395A6185D83CC1 (state_id),
-                PRIMARY KEY(id)) ENGINE = InnoDB",
             "CREATE TABLE TaskType (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_F7737B3C5E237E06 (name), PRIMARY KEY(id)) ENGINE = InnoDB",
             "CREATE TABLE Task (
                 id INT AUTO_INCREMENT NOT NULL,
@@ -36,9 +26,6 @@ class Version20120716214726_create_Job_TaskType_Task extends BaseMigration
                 INDEX IDX_F24C741B6B20BA36 (worker_id),
                 INDEX IDX_F24C741B7D6EFC3 (tasktype_id),
                 PRIMARY KEY(id)) ENGINE = InnoDB",
-            "ALTER TABLE Job ADD CONSTRAINT FK_C395A618A76ED395 FOREIGN KEY (user_id) REFERENCES fos_user (id)",
-            "ALTER TABLE Job ADD CONSTRAINT FK_C395A61818F45C82 FOREIGN KEY (website_id) REFERENCES WebSite (id)",
-            "ALTER TABLE Job ADD CONSTRAINT FK_C395A6185D83CC1 FOREIGN KEY (state_id) REFERENCES State (id)",
             "ALTER TABLE Task ADD CONSTRAINT FK_F24C741BBE04EA9 FOREIGN KEY (job_id) REFERENCES Job (id)",
             "ALTER TABLE Task ADD CONSTRAINT FK_F24C741B5D83CC1 FOREIGN KEY (state_id) REFERENCES State (id)",
             "ALTER TABLE Task ADD CONSTRAINT FK_F24C741B6B20BA36 FOREIGN KEY (worker_id) REFERENCES Worker (id)",
@@ -46,18 +33,6 @@ class Version20120716214726_create_Job_TaskType_Task extends BaseMigration
         );
         
         $this->statements['sqlite'] = array(
-            "CREATE TABLE Job (
-                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                user_id INT NOT NULL,
-                website_id INT NOT NULL,
-                state_id INT NOT NULL,
-                startDateTime DATETIME NOT NULL,
-                FOREIGN KEY(user_id) REFERENCES fos_user (id),
-                FOREIGN KEY(website_id) REFERENCES WebSite (id),
-                FOREIGN KEY(state_id) REFERENCES State (id))", 
-            "CREATE INDEX IDX_C395A618A76ED395 ON Job (user_id)",
-            "CREATE INDEX IDX_C395A61818F45C82 ON Job (website_id)",
-            "CREATE INDEX IDX_C395A6185D83CC1 ON Job (state_id)",
             "CREATE TABLE TaskType (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)",
             "CREATE UNIQUE INDEX UNIQ_F7737B3C5E237E06 ON TaskType (name)",
             "CREATE TABLE Task (
@@ -84,13 +59,11 @@ class Version20120716214726_create_Job_TaskType_Task extends BaseMigration
             "ALTER TABLE Task DROP FOREIGN KEY FK_F24C741B6B20BA36",
             "ALTER TABLE Task DROP FOREIGN KEY FK_F24C741BBE04EA9",
             "ALTER TABLE Task DROP FOREIGN KEY FK_F24C741B7D6EFC3",
-            "DROP TABLE Job",
             "DROP TABLE TaskType",
             "DROP TABLE Task"
         );
         
         $this->statements['sqlite'] = array(
-            "DROP TABLE Job",
             "DROP TABLE TaskType",
             "DROP TABLE Task"
         );      
