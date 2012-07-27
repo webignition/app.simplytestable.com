dfc047871cd40dbaa0f6f13dd12d60c28b71d764
Jon Cram <jon@webignition.net>
2012-07-27 21:17:11 +0100

    Moved altering TaskTypeClass to where table is creted

--- app/DoctrineMigrations/Version20120716214711_create_TaskTypeClass.php
+++ app/DoctrineMigrations/Version20120716214711_create_TaskTypeClass.php
@@ -13,11 +13,19 @@ class Version20120716214711_create_TaskTypeClass extends BaseMigration
     public function up(Schema $schema)
     {
         $this->statements['mysql'] = array(
-            "CREATE TABLE TaskTypeClass (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL, UNIQUE INDEX UNIQ_F92FE5F25E237E06 (name), PRIMARY KEY(id)) ENGINE = InnoDB"
+            "CREATE TABLE TaskTypeClass (
+                id INT AUTO_INCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description LONGTEXT DEFAULT NULL,
+                UNIQUE INDEX UNIQ_F92FE5F25E237E06 (name),
+                PRIMARY KEY(id)) ENGINE = InnoDB"
         );
         
         $this->statements['sqlite'] = array(
-            "CREATE TABLE TaskTypeClass (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, description LONGTEXT NOT NULL)",
+            "CREATE TABLE TaskTypeClass (
+                id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                name VARCHAR(255) NOT NULL,
+                description LONGTEXT DEFAULT NULL)",
             "CREATE INDEX UNIQ_F92FE5F25E237E06 ON TaskTypeClass (name)"
         );
         
