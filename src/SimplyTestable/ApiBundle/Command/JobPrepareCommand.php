00686dd146c8bed05b14b7677eb82e178de5cdde
Jon Cram <jon@webignition.net>
2012-07-31 09:30:48 +0100

    Let JobPrepareCommand accept path to http fixture data, allows tests to use mock http requests

--- src/SimplyTestable/ApiBundle/Command/JobPrepareCommand.php
+++ src/SimplyTestable/ApiBundle/Command/JobPrepareCommand.php
@@ -13,12 +13,25 @@ use SimplyTestable\ApiBundle\Entity\Task\Type\Type as TaskType;
 
 class JobPrepareCommand extends ContainerAwareCommand
 {
+    /**
+     *
+     * @var SimplyTestable\ApiBundle\Services\WebSiteService
+     */
+    private $websiteService;
+    
+    /**
+     *
+     * @var string
+     */
+    private $httpFixturePath;
+    
     protected function configure()
     {
         $this
             ->setName('simplytestable:job:prepare')
             ->setDescription('Prepare a set of tasks for a given job')
             ->addArgument('id', InputArgument::REQUIRED, 'id of job to prepare')
+            ->addArgument('http-fixture-path', InputArgument::OPTIONAL, 'path to HTTP fixture data when testing')
         ;
     }
 
@@ -30,33 +43,36 @@ class JobPrepareCommand extends ContainerAwareCommand
         /* @var $websiteService \SimplyTestable\ApiBundle\Services\WebsiteService */
         /* @var $entityManager \Doctrine\ORM\EntityManager */
         
+        if ($input->hasArgument('http-fixture-path')) {
+            $this->httpFixturePath = $input->getArgument('http-fixture-path');
+        }
+        
         $id = (int)$input->getArgument('id');
         $job = $this->getContainer()->get('simplytestable.services.jobservice')->getById($id);
 
         if ($job->getState()->getName() == JobService::STARTING_STATE) {
-            $websiteService = $this->getContainer()->get('simplytestable.services.websiteservice');
-            $urls = $websiteService->getUrls($job->getWebsite());
+            $urls = $this->getWebsiteService()->getUrls($job->getWebsite());
             $requestedTaskTypes = $job->getRequestedTaskTypes();
             $newTaskState = $this->getNewTaskState();
 
-            $entityManager = $this->getContainer()->get('doctrine')->getEntityManager();
+            $entityManager = $this->getContainer()->get('doctrine')->getEntityManager();          
 
-            foreach ($urls as $url) {
+            foreach ($urls as $url) {                
                 foreach ($requestedTaskTypes as $taskType) {
                     $task = new Task();
                     $task->setJob($job);
                     $task->setType($taskType);
                     $task->setUrl($url);
                     $task->setState($newTaskState);
-
+                    
                     $entityManager->persist($task);
-                }
+               }
             }
 
             $job->setNextState();
             $entityManager->persist($job);
 
-            $entityManager->flush();            
+            $entityManager->flush();          
         }
     }
     
@@ -68,4 +84,21 @@ class JobPrepareCommand extends ContainerAwareCommand
     private function getNewTaskState() {
         return $this->getContainer()->get('simplytestable.services.stateservice')->find('task-queued');
     }
+    
+    
+    /**
+     *
+     * @return SimplyTestable\ApiBundle\Services\WebSiteService
+     */
+    private function getWebsiteService() {
+        if (is_null($this->websiteService)) {
+            $this->websiteService = $this->getContainer()->get('simplytestable.services.websiteservice');
+            
+            if (!is_null($this->httpFixturePath)) {                
+                $this->websiteService->getHttpClient()->setMockResponsesPath($this->httpFixturePath);
+            }
+        }
+        
+        return $this->websiteService;
+    }   
 }
