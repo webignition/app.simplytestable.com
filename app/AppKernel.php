c9ceeb0f3892d5e3fb34ead537f02c09e149b55a
Jon Cram <jon@webignition.net>
2012-08-08 22:38:42 +0100

    Added glit ResqueBundle dep

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle($this),
+            new Glit\ResqueBundle\GlitResqueBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
