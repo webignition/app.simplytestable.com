d9b9e5103e3ba5ac8ad68fae0296369f61be031f
Jon Cram <jon@webignition.net>
2012-08-09 10:00:44 +0100

    Allow access to app_dev.php from 192.168.0.2

--- web/app_dev.php
+++ web/app_dev.php
@@ -13,6 +13,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
+        '192.168.0.2'
     ))
 ) {
     header('HTTP/1.0 403 Forbidden');
