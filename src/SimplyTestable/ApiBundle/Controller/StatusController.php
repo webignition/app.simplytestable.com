d8954a63ff010a71f20bacc975c329e0e8b2d70a
Jon Cram <jon@webignition.net>
2012-08-30 13:01:01 +0100

    Added /status/ listing app status and worker list

---
+++ src/SimplyTestable/ApiBundle/Controller/StatusController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use SimplyTestable\ApiBundle\Services\WorkerService;
+use SimplyTestable\ApiBundle\Services\WorkerRequestActivationService;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputDefinition;
+use SimplyTestable\ApiBundle\Entity\Worker;
+use SimplyTestable\ApiBundle\Entity\WorkerActivationRequest;
+
+
+class StatusController extends ApiController
+{  
+    
+    
+    public function indexAction()
+    {      
+        $responseData = array(
+            'status' => 'active',
+            'workers' => $this->getWorkerService()->getEntityRepository()->findAll()
+        );
+        
+        return $this->sendResponse($responseData);
+    }
+    
+    
+    /**
+     *
+     * @return WorkerService
+     */
+    private function getWorkerService() {
+        return $this->container->get('simplytestable.services.workerservice');
+    }
+    
+    
+
+
+}
