290e0e50b790da53570305ccae35cfeefbb330c5
Jon Cram <jon@webignition.net>
2012-09-03 15:36:41 +0100

    Fix resque job removal, correctly set key prefix

--- src/SimplyTestable/ApiBundle/Services/ResqueQueueService.php
+++ src/SimplyTestable/ApiBundle/Services/ResqueQueueService.php
@@ -29,17 +29,38 @@ class ResqueQueueService {
     private $logger;
     
     
+    /**
+     *
+     * @var \SimplyTestable\ApiBundle\Services\ResqueJobFactoryService 
+     */
     private $resqueJobFactoryService;
+    
+    
+    /**
+     *
+     * @var \Resque\Redis
+     */
+    private $resqueRedis;
+    
+    
+    /**
+     *
+     * @var string
+     */
+    private $resquePrefix;
     
     
     public function __construct(
             \Glit\ResqueBundle\Resque\Queue $queueManager,
             \Symfony\Component\HttpKernel\Log\LoggerInterface $logger,
-            \SimplyTestable\ApiBundle\Services\ResqueJobFactoryService $resqueJobFactoryService
+            \SimplyTestable\ApiBundle\Services\ResqueJobFactoryService $resqueJobFactoryService,
+            $resquePrefix
+            
     ) {
         $this->queueManager = $queueManager;
         $this->logger = $logger;
         $this->resqueJobFactoryService = $resqueJobFactoryService;
+        $this->resquePrefix = $resquePrefix;
     }
     
     
@@ -61,14 +82,13 @@ class ResqueQueueService {
     
     /**
      *
-     * @param string $job_name
      * @param string $queue_name
      * @param array $args
      * @return boolean 
      */
-    public function remove($job_name, $queue_name, $args = null) {
+    public function remove($queue_name, $args = null) {        
         try {
-            return \Resque\Resque::redis()->lrem('queue:task-assign', 1, $this->findRedisValue($job_name, $queue_name, $args)) == 1;           
+            return \Resque\Resque::redis()->lrem('queue:' . $queue_name, 1, $this->findRedisValue($queue_name, $args)) == 1;           
         } catch (\Exception $exception) {
             $this->logger->warn('ResqueQueueService::add: Redis error ['.$exception->getMessage().']');
             return false;
@@ -79,18 +99,17 @@ class ResqueQueueService {
     
     /**
      *
-     * @param string $job_name
      * @param string $queue_name
      * @param array $args
      * @return string
      */
-    private function findRedisValue($job_name, $queue_name, $args) {                
-        $queueLength = $this->getQueueLength($queue_name);        
+    private function findRedisValue($queue_name, $args) {                        
+        $queueLength = $this->getQueueLength($queue_name);
         
         for ($queueIndex = 0; $queueIndex < $queueLength; $queueIndex++) {            
-            $jobDetails = json_decode(\Resque\Resque::redis()->lindex('queue:task-assign', $queueIndex));
+            $jobDetails = json_decode(\Resque\Resque::redis()->lindex('queue:' . $queue_name, $queueIndex));
             
-            if ($this->match($jobDetails, $job_name, $args)) {
+            if ($this->match($jobDetails, $queue_name, $args)) {
                 return json_encode($jobDetails);
             }
         }
@@ -102,11 +121,13 @@ class ResqueQueueService {
     /**
      *
      * @param string $jobDetails
-     * @param string $job_name
+     * @param string $queue_name
      * @param array $args
      * @return boolean 
      */
-    private function match($jobDetails, $job_name, $args) {        
+    private function match($jobDetails, $queue_name, $args) {        
+        $job_name = $this->resqueJobFactoryService->getJobName($queue_name);
+        
         if (!isset($jobDetails->class)) {
             return false;
         }
@@ -143,6 +164,25 @@ class ResqueQueueService {
      * @return int
      */
     private function getQueueLength($queue_name) {
-        return \Resque\Resque::redis()->llen('queue:' . $queue_name);
+        return $this->getResqueRedis()->llen('queue:' . $queue_name);
+    }
+    
+    
+    
+    /**
+     *
+     * @return \Resque\Redis
+     */
+    private function getResqueRedis() {
+        var_dump($this->resquePrefix, json_encode($this->resquePrefix));
+        
+        if (is_null($this->resqueRedis)) {
+            $this->resqueRedis = \Resque\Resque::redis();
+            $this->resqueRedis->prefix($this->resquePrefix . ':resque:');
+        }
+        
+        return $this->resqueRedis;
     }    
+    
+    
 }
