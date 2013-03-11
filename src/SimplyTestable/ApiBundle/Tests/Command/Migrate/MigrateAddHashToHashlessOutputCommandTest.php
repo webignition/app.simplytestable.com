2e0000a567ca111102e0e21bfbd52c52c34c2cf6
Jon Cram <jon@webignition.net>
2013-03-11 14:07:09 +0000

    Set correct system state for migrate commands that expect to be in maintenance-read-only state

--- src/SimplyTestable/ApiBundle/Tests/Command/Migrate/MigrateAddHashToHashlessOutputCommandTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Command/Migrate/MigrateAddHashToHashlessOutputCommandTest.php
@@ -4,9 +4,10 @@ namespace SimplyTestable\ApiBundle\Tests\Command\Migrate;
 
 use SimplyTestable\ApiBundle\Tests\BaseSimplyTestableTestCase;
 
-class MigrateAddHashToHashlessOutputCommandTest extends BaseSimplyTestableTestCase {        
+class MigrateAddHashToHashlessOutputCommandTest extends BaseSimplyTestableTestCase {
     
     public function testRunCommandInMaintenanceReadOnlyModeReturnsStatusCode1() {
+        $this->assertEquals(0, $this->runConsole('simplytestable:maintenance:enable-read-only'));
         $this->assertEquals(1, $this->runConsole('simplytestable:add-hash-to-hashless-output'));
     }
 
--- src/SimplyTestable/ApiBundle/Tests/Command/Migrate/MigrateCanonicaliseTaskOutputCommandTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Command/Migrate/MigrateCanonicaliseTaskOutputCommandTest.php
@@ -7,6 +7,7 @@ use SimplyTestable\ApiBundle\Tests\BaseSimplyTestableTestCase;
 class MigrateCanonicaliseTaskOutputCommandTest extends BaseSimplyTestableTestCase {        
     
     public function testRunCommandInMaintenanceReadOnlyModeReturnsStatusCode1() {
+        $this->assertEquals(0, $this->runConsole('simplytestable:maintenance:enable-read-only'));
         $this->assertEquals(1, $this->runConsole('simplytestable:migrate:canonicalise-task-output'));
     }
 
