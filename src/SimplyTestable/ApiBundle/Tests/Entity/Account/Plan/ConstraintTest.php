999419af4aee0db5c144158ae8d78012e7a48b92
Jon Cram <jon@webignition.net>
2013-06-07 11:56:46 +0100

    Remove unique constraint from account plan constraint name

--- src/SimplyTestable/ApiBundle/Tests/Entity/Account/Plan/ConstraintTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Entity/Account/Plan/ConstraintTest.php
@@ -19,30 +19,5 @@ class ConstraintTest extends BaseSimplyTestableTestCase {
         
         $this->assertNotNull($constraint->getId());
     }
-    
-    
-    public function testNameUniqueness() {
-        $plan = $this->createAccountPlan();
-        
-        $constraint1 = new Constraint();
-        $constraint1->setName('bar');
-        $constraint1->setPlan($plan);
-        
-        $this->getEntityManager()->persist($constraint1);
-        $this->getEntityManager()->flush();        
-        
-        $constraint2 = new Constraint();
-        $constraint2->setName('bar');
-        $constraint2->setPlan($plan);
-        
-        $this->getEntityManager()->persist($constraint2);
-        
-        try {
-            $this->getEntityManager()->flush();
-            $this->fail('\Doctrine\DBAL\DBALException not raised for non-unique name');
-        } catch (\Doctrine\DBAL\DBALException $doctrineDbalException) {
-            $this->assertEquals(23000, $doctrineDbalException->getPrevious()->getCode());
-        }        
-    }
 
 }
