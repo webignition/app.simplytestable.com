3b6a67a2a6026460ac548d2b71debba5c6ad3b0b
Jon Cram <jon@webignition.net>
2013-06-07 13:34:01 +0100

    Move system user creation from migrations to fixtures

--- app/DoctrineMigrations/Version20120906125906_add_admin_user.php
+++ app/DoctrineMigrations/Version20120906125906_add_admin_user.php
@@ -8,16 +8,5 @@ use SimplyTestable\BaseMigrationsBundle\Migration\EntityModificationMigration,
 class Version20120906125906_add_admin_user extends EntityModificationMigration
 {    
     public function postUp(Schema $schema) {
-        $user = new User();
-        $user->setEmail($this->container->getParameter('admin_user_email'));
-        $user->setPlainPassword($this->container->getParameter('admin_user_password'));
-        $user->setUsername('admin'); 
-        $user->addRole('role_admin');
-        
-        $userManager = $this->container->get('fos_user.user_manager');        
-        $userManager->updateUser($user);
-        
-        $manipulator = $this->container->get('fos_user.util.user_manipulator');
-        $manipulator->activate($user->getUsername());
     }
 }
