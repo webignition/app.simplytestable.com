770e43d6cf3b29a3323d735de74cd2613e07d363
Jon Cram <jon@webignition.net>
2013-06-07 14:40:11 +0100

    Initial task types to fixture

---
+++ src/SimplyTestable/ApiBundle/DataFixtures/ORM/LoadTaskTypes.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use SimplyTestable\ApiBundle\Entity\Task\Type\Type as TaskType;
+
+class LoadTaskTypes extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+    
+    private $taskTypes = array(
+        'HTML validation' => array(
+            'description' => 'Validates the HTML markup for a given URL',
+            'class' => 'verification'
+        )
+    );
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        $taskTypeClassRepository = $manager->getRepository('SimplyTestable\ApiBundle\Entity\Task\Type\TaskTypeClass');
+        
+        foreach ($this->taskTypes as $name => $properties) {
+            $taskTypeClass = $taskTypeClassRepository->findOneByName($properties['class']);
+
+            $taskType = new TaskType();
+            $taskType->setClass($taskTypeClass);
+            $taskType->setDescription($properties['description']);
+            $taskType->setName($name);
+            
+            $manager->persist($taskType);
+            $manager->flush();            
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 4; // the order in which fixtures will be loaded
+    }
+}
