b29b2e67081ed618fa48990332bf490ee22dcbac
Jon Cram <jon@webignition.net>
2013-06-10 14:59:07 +0100

    Move setting public user account plan to fixture

--- app/DoctrineMigrations/Version20130426095749_add_public_user_to_public_plan.php
+++ app/DoctrineMigrations/Version20130426095749_add_public_user_to_public_plan.php
@@ -12,44 +12,4 @@ use SimplyTestable\BaseMigrationsBundle\Migration\EntityModificationMigration,
  */
 class Version20130426095749_add_public_user_to_public_plan extends EntityModificationMigration
 {
-
-    
-    public function postUp(Schema $schema)
-    {
-        $user = $this->getUserService()->getPublicUser();
-        $plan = $this->getAccountPlanService()->find('public');
-        
-        $this->getUserAccountPlanService()->create($user, $plan);
-    }
-    
-    public function postDown(Schema $schema)
-    {      
-    }
-    
-    
-    /**
-     * 
-     * @return \SimplyTestable\ApiBundle\Services\UserAccountPlanService
-     */
-    private function getUserAccountPlanService() {
-        return $this->container->get('simplytestable.services.useraccountplanservice');
-    }     
-    
-    
-    /**
-     * 
-     * @return \SimplyTestable\ApiBundle\Services\AccountPlanService
-     */
-    private function getAccountPlanService() {
-        return $this->container->get('simplytestable.services.accountplanservice');
-    }    
-    
-    
-    /**
-     * 
-     * @return \SimplyTestable\ApiBundle\Services\UserService
-     */
-    private function getUserService() {
-        return $this->container->get('simplytestable.services.userservice');
-    }
 }
