34581a2a9ecca359f6f26d36b698e4cc7436e7a1
Jon Cram <jon@webignition.net>
2013-06-11 10:24:30 +0100

    Load fixtures after rebuilding test dbs

--- tools/resetdb-test.sh
+++ tools/resetdb-test.sh
@@ -1,3 +1,4 @@
 php app/console doctrine:database:drop -e test --force
 php app/console doctrine:database:create -e test
 php app/console doctrine:migrations:migrate -e test --no-interaction
+php app/console doctrine:fixtures:load -e test --append
