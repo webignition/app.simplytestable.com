c68c8d30d6342045a27493d582621951a6e0dd14
Jon Cram <jon@webignition.net>
2013-08-12 15:37:35 +0100

    Remove all jenkins-specific autoloading, was a composer issue probably

--- app/autoload.php
+++ app/autoload.php
@@ -13,24 +13,4 @@ if (!function_exists('intl_get_error_code')) {
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
-/**
- * Explicitly include some dependencies that otherwise result in 'class not found'
- * errors when running on Jenkins.
- * 
- * Works on local dev environment. Works on live. Works on Travis. Doesn't work on Jenkins.
- */
-//$jenkinsNeedsThese = array(
-//    'Stripe' => '/stripe/stripe-php/lib/Stripe.php',
-//    'ExpressiveDate' => '/jasonlewis/expressive-date/src/ExpressiveDate.php'
-//);    
-//
-//foreach ($jenkinsNeedsThese as $class => $path) {
-//    if (!class_exists($class)) {
-//        var_dump("cp01");
-//        exit();
-//        
-//        require_once(realpath(__DIR__ . '/../vendor' . $path));
-//    }
-//}  
-
 return $loader;
