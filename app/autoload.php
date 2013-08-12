5e1bc8588acff15065e3318eb8c52716ca83f273
Jon Cram <jon@webignition.net>
2013-08-12 15:23:05 +0100

    Try again to work around Jenkins oddity

--- app/autoload.php
+++ app/autoload.php
@@ -2,25 +2,6 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-/**
- * Explicitly include some dependencies that otherwise result in 'class not found'
- * errors when running on Jenkins.
- * 
- * Works on local dev environment. Works on live. Works on Travis. Doesn't work on Jenkins.
- */
-if (getenv('IS_JENKINS') === 'true') {
-    $jenkinsNeedsThese = array(
-        'Stripe' => '/stripe/stripe-php/lib/Stripe.php',
-        'ExpressiveDate' => '/jasonlewis/expressive-date/src/ExpressiveDate.php'
-    );    
-    
-    foreach ($jenkinsNeedsThese as $class => $path) {
-        if (!class_exists($class)) {
-            require_once(realpath(__DIR__ . '/../vendor' . $path));
-        }
-    }    
-}
-
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 // intl
@@ -31,5 +12,22 @@ if (!function_exists('intl_get_error_code')) {
 }
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
+
+/**
+ * Explicitly include some dependencies that otherwise result in 'class not found'
+ * errors when running on Jenkins.
+ * 
+ * Works on local dev environment. Works on live. Works on Travis. Doesn't work on Jenkins.
+ */
+$jenkinsNeedsThese = array(
+    'Stripe' => '/stripe/stripe-php/lib/Stripe.php',
+    'ExpressiveDate' => '/jasonlewis/expressive-date/src/ExpressiveDate.php'
+);    
+
+foreach ($jenkinsNeedsThese as $class => $path) {
+    if (!class_exists($class)) {
+        require_once(realpath(__DIR__ . '/../vendor' . $path));
+    }
+}  
 
 return $loader;
