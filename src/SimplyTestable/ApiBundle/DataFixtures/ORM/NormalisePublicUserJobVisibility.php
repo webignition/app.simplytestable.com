ce6a2393c311bd34202f003b82e52fdd5bbae35f
Jon Cram <jon@webignition.net>
2013-10-17 08:47:15 +0100

    Add fixture to set public job isPublic=true for historical jobs

---
+++ src/SimplyTestable/ApiBundle/DataFixtures/ORM/NormalisePublicUserJobVisibility.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class NormalisePublicUserJobVisibility extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }     
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {   
+        $jobRepository = $manager->getRepository('SimplyTestable\ApiBundle\Entity\Job\Job');        
+        $publicUserPrivateJobs = $jobRepository->findBy(array(
+            'user' => $this->container->get('simplytestable.services.userservice')->getPublicUser(),
+            'isPublic' => false
+        ));
+        
+        if (count($publicUserPrivateJobs) === 0) {
+            return true;
+        }
+        
+        foreach ($publicUserPrivateJobs as $job) {
+            $job->setIsPublic(true);
+            $manager->persist($job);
+        }
+        
+        $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 9; // the order in which fixtures will be loaded
+    }
+}
