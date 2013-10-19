37100e84db8a49f2c27d1d181fb40974f088dac5
Jon Cram <jon@webignition.net>
2013-10-19 08:54:48 +0100

    Test that public user can access public job crawl status owned by private user

--- src/SimplyTestable/ApiBundle/Tests/Controller/Job/CrawlStatusTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Controller/Job/CrawlStatusTest.php
@@ -84,6 +84,25 @@ class CrawlStatusTest extends BaseControllerJsonTestCase {
         $this->assertNotNull($jobObject->crawl->id);        
     }
     
+    public function testGetForPublicJobOwnedByNonPublicUserByPublicUser() {
+        $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__). '/HttpResponses'));
+        
+        $user = $this->createAndActivateUser('user@example.com', 'password');
+        
+        $canonicalUrl = 'http://example.com/';
+        $job = $this->getJobService()->getById($this->createAndPrepareJob($canonicalUrl, $user->getEmail()));
+        
+        $this->getJobController('setPublicAction', array(
+            'user' => $user->getEmail()
+        ))->setPublicAction($canonicalUrl, $job->getId()); 
+        
+        $this->assertTrue($job->getIsPublic());       
+        
+        $jobObject = json_decode($this->getJobController('statusAction')->statusAction((string)$job->getWebsite(), $job->getId())->getContent());
+        
+        $this->assertTrue(isset($jobObject->crawl));       
+    }    
+    
     public function testGetForPublicJobOwnedByNonPublicUserByNonPublicUser() {
         $this->setHttpFixtures($this->getHttpFixtures($this->getFixturesDataPath(__FUNCTION__). '/HttpResponses'));
         
@@ -165,7 +184,10 @@ class CrawlStatusTest extends BaseControllerJsonTestCase {
         $this->assertEquals(403, $this->getJobController('statusAction', array(
             'user' => $user2->getEmail()
         ))->statusAction((string)$job->getWebsite(), $job->getId())->getStatusCode());        
-    }    
+    }   
+    
+    
+    //public function 
     
 }
 
