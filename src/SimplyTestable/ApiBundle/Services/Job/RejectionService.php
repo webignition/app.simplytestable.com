b39ec7f0758bf401b43a34bd8ce16bfa8bd12a42
Jon Cram <jon@webignition.net>
2014-02-12 10:43:49 +0000

    Add job rejection service

---
+++ src/SimplyTestable/ApiBundle/Services/Job/RejectionService.php
@@ -0,0 +1,43 @@
+<?php
+namespace SimplyTestable\ApiBundle\Services\Job;
+
+use SimplyTestable\ApiBundle\Entity\Job\Job;
+use SimplyTestable\ApiBundle\Entity\Account\Plan\Constraint as AccountPlanConstraint;
+use SimplyTestable\ApiBundle\Entity\Job\RejectionReason as JobRejectionReason;
+
+class RejectionService { 
+    
+    /**
+     *
+     * @var \SimplyTestable\ApiBundle\Services\JobService 
+     */
+    private $jobService;
+    
+    /**
+     * 
+     * @param \SimplyTestable\ApiBundle\Services\JobService $jobService
+     */
+    public function __construct(\SimplyTestable\ApiBundle\Services\JobService $jobService) {
+        $this->jobService = $jobService;
+    }
+    
+    
+    /**
+     * 
+     * @param \SimplyTestable\ApiBundle\Entity\Job\Job $job
+     * @param string $reason
+     * @param \SimplyTestable\ApiBundle\Entity\Account\Plan\Constraint $constraint
+     */
+    public function reject(Job $job, $reason, AccountPlanConstraint $constraint = null) {
+        $this->jobService->reject($job);
+
+        $rejectionReason = new JobRejectionReason();
+        $rejectionReason->setConstraint($constraint);
+        $rejectionReason->setJob($job);
+        $rejectionReason->setReason($reason);
+
+        $this->jobService->getEntityManager()->persist($rejectionReason);
+        $this->jobService->getEntityManager()->flush();        
+    }
+  
+}
