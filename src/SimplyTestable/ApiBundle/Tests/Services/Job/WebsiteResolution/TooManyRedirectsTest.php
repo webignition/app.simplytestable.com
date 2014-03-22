184a6323965f5be228070d3cfe064106fdb31528
Jon Cram <jon@webignition.net>
2014-03-22 16:32:57 +0000

    Add test to verify previous change

---
+++ src/SimplyTestable/ApiBundle/Tests/Services/Job/WebsiteResolution/TooManyRedirectsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Services\Job\WebsiteResolution;
+
+use SimplyTestable\ApiBundle\Tests\BaseSimplyTestableTestCase;
+
+class TooManyRedirectsTest extends BaseSimplyTestableTestCase {    
+    
+    const SOURCE_URL = 'http://example.com/';
+    const EFFECTIVE_URL = 'http://www.example.com/';
+    
+    public function setUp() {
+        parent::setUp();
+        
+        $this->queueHttpFixtures($this->buildHttpFixtureSet(array(
+            "HTTP/1.0 301\nLocation: " . self::SOURCE_URL,
+            "HTTP/1.0 301\nLocation: " . self::SOURCE_URL,
+            "HTTP/1.0 301\nLocation: " . self::SOURCE_URL,
+            "HTTP/1.0 301\nLocation: " . self::SOURCE_URL,            
+            "HTTP/1.0 301\nLocation: " . self::EFFECTIVE_URL,            
+            "HTTP/1.0 200 OK",
+        )));  
+    }
+    
+    public function testFullSiteTestResolvesToEffectiveUrl() {        
+        $job = $this->getJobService()->getById($this->createJobAndGetId(self::SOURCE_URL));
+        $this->resolveJob($job->getWebsite()->getCanonicalUrl(), $job->getId());
+        
+        $this->assertEquals(self::EFFECTIVE_URL, $job->getWebsite()->getCanonicalUrl());    
+    } 
+    
+    
+    public function testSingleUrlTestResolvesToEffectiveUrl() {        
+        $job = $this->getJobService()->getById($this->createJobAndGetId(self::SOURCE_URL, null, 'single url'));
+        $this->resolveJob($job->getWebsite()->getCanonicalUrl(), $job->getId());
+        
+        $this->assertEquals(self::EFFECTIVE_URL, $job->getWebsite()->getCanonicalUrl());    
+    }    
+
+}
