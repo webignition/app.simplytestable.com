aa9e2c46b2feab83efcbb943127d64a04dc5b614
Jon Cram <jon@webignition.net>
2014-04-07 08:45:12 +0100

    Renamespace job start controller retest action tests; add routing test

--- src/SimplyTestable/ApiBundle/Tests/Controller/JobStart/RetestAction/RetestTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Controller/JobStart/RetestAction/RetestTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SimplyTestable\ApiBundle\Tests\Controller\JobStart;
+namespace SimplyTestable\ApiBundle\Tests\Controller\JobStart\RetestAction;
 
 use SimplyTestable\ApiBundle\Tests\Controller\BaseControllerJsonTestCase;
 
