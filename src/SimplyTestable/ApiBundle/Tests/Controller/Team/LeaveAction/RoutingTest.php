c8414b94737e288c82325e0b55d138d4c693b6a0
Jon Cram <jon@webignition.net>
2014-07-11 15:25:24 +0100

    Fix test namespacing

--- src/SimplyTestable/ApiBundle/Tests/Controller/Team/LeaveAction/RoutingTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Controller/Team/LeaveAction/RoutingTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace SimplyTestable\ApiBundle\Tests\Controller\Team\RemoveAction;
+namespace SimplyTestable\ApiBundle\Tests\Controller\Team\LeaveAction;
 
 use SimplyTestable\ApiBundle\Tests\Controller\RoutingTest as BaseRoutingTest;
 
