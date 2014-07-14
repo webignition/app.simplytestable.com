bfdc34a31eeae64edf9b736d357d788e29354fe9
Jon Cram <jon@webignition.net>
2014-07-14 09:01:02 +0100

    Add common setUp

--- src/SimplyTestable/ApiBundle/Tests/Controller/Job/Job/Access/PublicUserAccessTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Controller/Job/Job/Access/PublicUserAccessTest.php
@@ -3,34 +3,42 @@
 namespace SimplyTestable\ApiBundle\Tests\Controller\Job\Job\Access;
 
 use SimplyTestable\ApiBundle\Tests\Controller\BaseControllerJsonTestCase;
+use SimplyTestable\ApiBundle\Entity\Job\Job;
 
 abstract class PublicUserAccessTest extends BaseControllerJsonTestCase {
     
     const CANONICAL_URL = 'http://www.example.com/';
     
     abstract protected function getActionName();
+
+    /**
+     * @var Job
+     */
+    private $job;
+
+    public function setUp() {
+        parent::setUp();
+        $this->job = $this->getJobService()->getById($this->createJobAndGetId(self::CANONICAL_URL));
+    }
     
     public function testGetForPublicJobOwnedByPublicUserByPublicUser() {
-        $job = $this->getJobService()->getById($this->createJobAndGetId(self::CANONICAL_URL));
-        
-        $this->assertTrue($job->getIsPublic());
-        $this->assertEquals($this->getUserService()->getPublicUser()->getId(), $job->getUser()->getId());
+        $this->assertTrue($this->job->getIsPublic());
+        $this->assertEquals($this->getUserService()->getPublicUser()->getId(), $this->job->getUser()->getId());
         
         $actionName = $this->getActionName();
-        $this->assertEquals(200, $this->getJobController($actionName)->$actionName(self::CANONICAL_URL, $job->getId())->getStatusCode());      
+        $this->assertEquals(200, $this->getJobController($actionName)->$actionName(self::CANONICAL_URL, $this->job->getId())->getStatusCode());
     }
     
     public function testGetForPublicJobOwnedByPublicUserByNonPublicUser() {
-        $job = $this->getJobService()->getById($this->createJobAndGetId(self::CANONICAL_URL));
-        $this->assertTrue($job->getIsPublic());
-        $this->assertEquals($this->getUserService()->getPublicUser()->getId(), $job->getUser()->getId()); 
+        $this->assertTrue($this->job->getIsPublic());
+        $this->assertEquals($this->getUserService()->getPublicUser()->getId(), $this->job->getUser()->getId());
         
         $user = $this->createAndActivateUser('user@example.com', 'password');
         $actionName = $this->getActionName();
 
         $this->assertEquals(200, $this->getJobController($actionName, array(
             'user' => $user->getEmail()            
-        ))->$actionName(self::CANONICAL_URL, $job->getId())->getStatusCode());              
+        ))->$actionName(self::CANONICAL_URL, $this->job->getId())->getStatusCode());
     } 
     
 
