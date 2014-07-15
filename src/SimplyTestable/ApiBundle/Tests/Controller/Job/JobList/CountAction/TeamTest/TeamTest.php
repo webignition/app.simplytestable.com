5f9003f6fffe81b493d1eaa0f1eb9b6dcc3fa30f
Jon Cram <jon@webignition.net>
2014-07-15 08:42:34 +0100

    Verify JobListController::countAction() for team

---
+++ src/SimplyTestable/ApiBundle/Tests/Controller/Job/JobList/CountAction/TeamTest/TeamTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Controller\Job\JobList\CountAction\TeamTest;
+
+use SimplyTestable\ApiBundle\Tests\Controller\BaseControllerJsonTestCase;
+use SimplyTestable\ApiBundle\Entity\User;
+use SimplyTestable\ApiBundle\Entity\Team;
+
+abstract class TeamTest extends BaseControllerJsonTestCase {
+
+    /**
+     * @var User
+     */
+    protected $leader;
+
+
+    /**
+     * @var User
+     */
+    protected $member1;
+
+
+    /**
+     * @var User
+     */
+    protected $member2;
+
+
+    /**
+     * @var array
+     */
+    private $jobIds = [];
+
+
+    /**
+     * @var int
+     */
+    private $count;
+
+    public function setUp() {
+        parent::setUp();
+
+        $this->leader = $this->createAndActivateUser('leader@example.com');
+        $this->member1 = $this->createAndActivateUser('member1@example.com');
+        $this->member2 = $this->createAndActivateUser('member2@example.com');
+
+        $team = $this->getTeamService()->create('Foo', $this->leader);
+
+        $this->getTeamMemberService()->add($team, $this->member1);
+        $this->getTeamMemberService()->add($team, $this->member2);
+
+        $this->jobIds[] = $this->createJobAndGetId('http://leader.example.com/', $this->leader->getEmail());
+        $this->jobIds[] = $this->createJobAndGetId('http://member1.example.com/', $this->member1->getEmail());
+        $this->jobIds[] = $this->createJobAndGetId('http://member2.example.com/', $this->member2->getEmail());
+
+        $countResponse = $this->getJobListController('countAction', [
+            'user' => $this->getRequester()->getEmail()
+        ])->countAction(count($this->jobIds));
+
+        $this->count = json_decode($countResponse->getContent());
+    }
+
+
+    /**
+     * @return User
+     */
+    abstract protected function getRequester();
+
+
+    public function testCount() {
+        $this->assertEquals(count($this->jobIds), $this->count);
+    }
+    
+}
