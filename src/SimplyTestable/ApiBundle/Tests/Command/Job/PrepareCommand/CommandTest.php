d86c68d4019e3fb6454e228b2bd56c594b951b2c
Jon Cram <jon@webignition.net>
2014-10-03 10:11:12 +0100

    Refactor job prepare command tests

--- src/SimplyTestable/ApiBundle/Tests/Command/Job/PrepareCommand/CommandTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Command/Job/PrepareCommand/CommandTest.php
@@ -4,5 +4,5 @@ namespace SimplyTestable\ApiBundle\Tests\Command\Job\PrepareCommand;
 
 use SimplyTestable\ApiBundle\Tests\Command\CommandTest as BaseCommandTest;
 
-class CommandTest extends BaseCommandTest {
+abstract class CommandTest extends BaseCommandTest {
 }
