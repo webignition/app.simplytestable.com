79f4f12828334d825ba6300eee07cf12101e2cb7
Jon Cram <jon@webignition.net>
2017-06-14 13:26:57 +0100

    PSR2 CS

--- src/SimplyTestable/ApiBundle/Controller/TaskController.php
+++ src/SimplyTestable/ApiBundle/Controller/TaskController.php
@@ -2,21 +2,26 @@
 
 namespace SimplyTestable\ApiBundle\Controller;
 
+use SimplyTestable\ApiBundle\Entity\State;
+use SimplyTestable\ApiBundle\Services\CrawlJobContainerService;
+use SimplyTestable\ApiBundle\Services\Resque\JobFactoryService;
+use SimplyTestable\ApiBundle\Services\Resque\QueueService;
+use SimplyTestable\ApiBundle\Services\TaskOutputJoiner\FactoryService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputDefinition;
 use SimplyTestable\ApiBundle\Entity\Task\Task;
 use SimplyTestable\ApiBundle\Entity\Task\Output;
-use SimplyTestable\ApiBundle\Services\WorkerService;
 use SimplyTestable\ApiBundle\Services\TaskService;
 use SimplyTestable\ApiBundle\Services\TaskTypeService;
 use SimplyTestable\ApiBundle\Services\StateService;
 use SimplyTestable\ApiBundle\Services\JobService;
 use SimplyTestable\ApiBundle\Entity\Task\Type\Type as TaskType;
 
-class TaskController extends ApiController {
-    
-    public function __construct() {
+class TaskController extends ApiController
+{
+    public function __construct()
+    {
         $this->setInputDefinitions(array(
             'completeAction' => new InputDefinition(array(
                 new InputArgument('end_date_time', InputArgument::REQUIRED, 'Task end date and time'),
@@ -25,17 +30,24 @@ class TaskController extends ApiController {
                 new InputArgument('state', InputArgument::REQUIRED, 'Task ending state'),
                 new InputArgument('errorCount', InputArgument::REQUIRED, 'Task error count'),
                 new InputArgument('warningCount', InputArgument::REQUIRED, 'Task warning count')
-            ))            
+            ))
         ));
-        
+
         $this->setRequestTypes(array(
             'completeAction' => \Guzzle\Http\Message\Request::POST,
             'completeByUrlAndTaskTypeAction' => \Guzzle\Http\Message\Request::POST
-        ));       
-    }  
-    
-    
-    public function completeAction($canonical_url, $task_type, $parameter_hash) {
+        ));
+    }
+
+    /**
+     * @param string $canonical_url
+     * @param string $task_type
+     * @param string $parameter_hash
+     *
+     * @return Response
+     */
+    public function completeAction($canonical_url, $task_type, $parameter_hash)
+    {
         if ($this->getApplicationStateService()->isInMaintenanceReadOnlyState()) {
             return $this->sendServiceUnavailableResponse();
         }
@@ -43,11 +55,16 @@ class TaskController extends ApiController {
         $task_type = urldecode($task_type);
         if (!$this->getTaskTypeService()->exists($task_type)) {
             return $this->sendFailureResponse();
-        }        
-        
+        }
+
         $taskType = $this->getTaskTypeService()->getByName($task_type);
 
-        $tasks = $this->getTaskService()->getEquivalentTasks($canonical_url, $taskType, $parameter_hash, $this->getTaskService()->getIncompleteStates());
+        $tasks = $this->getTaskService()->getEquivalentTasks(
+            $canonical_url,
+            $taskType,
+            $parameter_hash,
+            $this->getTaskService()->getIncompleteStates()
+        );
 
         if (count($tasks) === 0) {
             return $this->sendGoneResponse();
@@ -55,19 +72,23 @@ class TaskController extends ApiController {
 
         $endDateTime = new \DateTime($this->getArguments('completeByUrlAndTaskTypeAction')->get('end_date_time'));
         $rawOutput = $this->getArguments('completeByUrlAndTaskTypeAction')->get('output');
-        
+
         $mediaTypeParser = new \webignition\InternetMediaType\Parser\Parser();
-        $contentType = $mediaTypeParser->parse($this->getArguments('completeByUrlAndTaskTypeAction')->get('contentType'));
-        
+        $contentType = $mediaTypeParser->parse(
+            $this->getArguments('completeByUrlAndTaskTypeAction')->get('contentType')
+        );
+
         $output = new Output();
         $output->setOutput($rawOutput);
         $output->setContentType($contentType);
         $output->setErrorCount($this->getArguments('completeByUrlAndTaskTypeAction')->get('errorCount'));
         $output->setWarningCount($this->getArguments('completeByUrlAndTaskTypeAction')->get('warningCount'));
-                
+
         $state = $this->getTaskEndState($this->getArguments('completeByUrlAndTaskTypeAction')->get('state'));
 
         $urlDiscoveryTaskType = $this->getTaskTypeService()->getByName('URL discovery');
+
+        $crawlJobContainerService = $this->getCrawlJobContainerService();
 
         foreach ($tasks as $task) {
             /* @var $task Task */
@@ -82,7 +103,7 @@ class TaskController extends ApiController {
             $this->getTaskService()->complete($task, $endDateTime, $output, $state, false);
 
             if ($task->getType()->equals($urlDiscoveryTaskType)) {
-                $this->getCrawlJobContainerService()->processTaskResults($task);
+                $crawlJobContainerService->processTaskResults($task);
             }
 
             if (!$this->getJobService()->hasIncompleteTasks($task->getJob())) {
@@ -91,15 +112,25 @@ class TaskController extends ApiController {
 
             if ($task->getType()->equals($urlDiscoveryTaskType)) {
                 if ($this->getJobService()->isCompleted($task->getJob())) {
-                    if ($this->getCrawlJobContainerService()->getEntityRepository()->doesCrawlTaskParentStateMatchState($task, $this->getJobService()->getFailedNoSitemapState())) {
-                        $crawlJobContainer = $this->getCrawlJobContainerService()->getForJob($task->getJob());
+                    $failedNoSitemapState = $this->getJobService()->getFailedNoSitemapState();
+
+                    if ($crawlJobContainerService->getEntityRepository()->doesCrawlTaskParentStateMatchState(
+                        $task,
+                        $failedNoSitemapState
+                    )) {
+                        $crawlJobContainer = $crawlJobContainerService->getForJob($task->getJob());
 
                         foreach ($crawlJobContainer->getParentJob()->getRequestedTaskTypes() as $taskType) {
                             /* @var $taskType TaskType */
-                            $taskTypeParameterDomainsToIgnoreKey = strtolower(str_replace(' ', '-', $taskType->getName())) . '-domains-to-ignore';
+                            $taskTypeParameterDomainsToIgnoreKey = strtolower(
+                                str_replace(' ', '-', $taskType->getName())
+                            ) . '-domains-to-ignore';
 
                             if ($this->container->hasParameter($taskTypeParameterDomainsToIgnoreKey)) {
-                                $this->getJobPreparationService()->setPredefinedDomainsToIgnore($taskType, $this->container->getParameter($taskTypeParameterDomainsToIgnoreKey));
+                                $this->getJobPreparationService()->setPredefinedDomainsToIgnore(
+                                    $taskType,
+                                    $this->container->getParameter($taskTypeParameterDomainsToIgnoreKey)
+                                );
                             }
                         }
 
@@ -118,126 +149,125 @@ class TaskController extends ApiController {
         return $this->sendSuccessResponse();
     }
 
-    
     /**
-     *
      * @param string $stateFromRequest
-     * @return \SimplyTestable\ApiBundle\Entity\State
+     *
+     * @return State
      */
-    private function getTaskEndState($stateFromRequest) {        
+    private function getTaskEndState($stateFromRequest)
+    {
         if ($stateFromRequest == $this->getTaskService()->getFailedNoRetryAvailableState()->getName()) {
             return $this->getTaskService()->getFailedNoRetryAvailableState();
         }
-        
+
         if ($stateFromRequest == $this->getTaskService()->getFailedRetryAvailableState()->getName()) {
             return $this->getTaskService()->getFailedRetryAvailableState();
         }
-        
+
         if ($stateFromRequest == $this->getTaskService()->getFailedRetryLimitReachedState()->getName()) {
             return $this->getTaskService()->getFailedRetryLimitReachedState();
         }
-        
+
         if ($stateFromRequest == $this->getTaskService()->getSkippedState()->getName()) {
             return $this->getTaskService()->getSkippedState();
-        }        
-        
+        }
+
         return $this->getTaskService()->getCompletedState();
     }
-    
+
+    /**
+     * @param string $task_type
+     * @param string $state_name
+     *
+     * @return Response
+     */
     public function taskTypeCountAction($task_type, $state_name)
-    {       
+    {
         $taskStatePrefix = 'task-';
-        
+
         if (!$this->getTaskTypeService()->exists($task_type)) {
             return new Response('', 404);
         }
-        
+
         if (!$this->getStateService()->has($taskStatePrefix . $state_name)) {
             return new Response('', 404);
         }
-        
-        $taskType = $this->getTaskTypeService()->getByName($task_type);       
+
+        $taskType = $this->getTaskTypeService()->getByName($task_type);
         $state = $this->getStateService()->find($taskStatePrefix . $state_name);
-        
+
         return new Response(json_encode($this->getTaskService()->getCountByTaskTypeAndState($taskType, $state)), 200);
     }
 
-    
     /**
-     *
      * @return TaskService
      */
-    private function getTaskService() {
+    private function getTaskService()
+    {
         return $this->container->get('simplytestable.services.taskservice');
     }
-    
-    
+
     /**
-     *
      * @return JobService
      */
-    private function getJobService() {
+    private function getJobService()
+    {
         return $this->container->get('simplytestable.services.jobservice');
-    }     
-    
-    
+    }
+
     /**
-     *
      * @return \SimplyTestable\ApiBundle\Services\JobPreparationService
      */
-    private function getJobPreparationService() {
+    private function getJobPreparationService()
+    {
         return $this->container->get('simplytestable.services.jobpreparationservice');
-    }     
-    
-    
+    }
+
     /**
-     *
      * @return TaskTypeService
      */
-    private function getTaskTypeService() {
+    private function getTaskTypeService()
+    {
         return $this->container->get('simplytestable.services.tasktypeservice');
-    }     
-    
+    }
+
     /**
-     *
      * @return StateService
      */
-    private function getStateService() {
+    private function getStateService()
+    {
         return $this->container->get('simplytestable.services.stateservice');
-    }      
-    
+    }
+
     /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\CrawlJobContainerService
+     * @return CrawlJobContainerService
      */
-    private function getCrawlJobContainerService() {
+    private function getCrawlJobContainerService()
+    {
         return $this->container->get('simplytestable.services.crawljobcontainerservice');
     }
 
     /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\Resque\QueueService
+     * @return QueueService
      */
-    private function getResqueQueueService() {
+    private function getResqueQueueService()
+    {
         return $this->get('simplytestable.services.resque.queueService');
     }
 
-
     /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\Resque\JobFactoryService
+     * @return JobFactoryService
      */
-    private function getResqueJobFactoryService() {
+    private function getResqueJobFactoryService()
+    {
         return $this->get('simplytestable.services.resque.jobFactoryService');
     }
-    
+
     /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\TaskOutputJoiner\FactoryService
-     */    
-    private function getTaskOutputJoinerFactoryService() {
+     * @return FactoryService
+     */
+    private function getTaskOutputJoinerFactoryService()
+    {
         return $this->container->get('simplytestable.services.TaskOutputJoinerServiceFactory');
     }
-
 }
-
