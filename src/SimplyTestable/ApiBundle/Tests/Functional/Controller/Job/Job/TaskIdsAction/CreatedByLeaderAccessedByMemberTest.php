b9d21254082451a6fe9ea7e3ea581009c857966b
Jon Cram <jon@webignition.net>
2017-06-20 18:20:17 +0100

    Remove obsolete tests

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/Job/Job/TaskIdsAction/CreatedByLeaderAccessedByMemberTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Job\TaskIdsAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Job\Access\TeamAccess\CreatedByLeaderAccessedByMemberTest as BaseCreatedByLeaderAccessedByMemberTest;
-
-class CreatedByLeaderAccessedByMemberTest extends BaseCreatedByLeaderAccessedByMemberTest {}
--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/Job/Job/TasksAction/CreatedByLeaderAccessedByMemberTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Job\TasksAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Job\Access\TeamAccess\CreatedByLeaderAccessedByMemberTest as BaseCreatedByLeaderAccessedByMemberTest;
-
-class CreatedByLeaderAccessedByMemberTest extends BaseCreatedByLeaderAccessedByMemberTest {}
