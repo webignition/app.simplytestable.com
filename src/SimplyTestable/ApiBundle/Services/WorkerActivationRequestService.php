17a4962bb534c4f10fe5505a81c6727838d303cd
Jon Cram <jon@webignition.net>
2017-07-17 14:00:58 +0100

    Remove old Logger from WorkerActivationRequestService

--- src/SimplyTestable/ApiBundle/Services/WorkerActivationRequestService.php
+++ src/SimplyTestable/ApiBundle/Services/WorkerActivationRequestService.php
@@ -2,111 +2,125 @@
 namespace SimplyTestable\ApiBundle\Services;
 
 use Doctrine\ORM\EntityManager;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Exception\CurlException;
+use Psr\Log\LoggerInterface;
 use SimplyTestable\ApiBundle\Entity\Worker;
 use SimplyTestable\ApiBundle\Entity\WorkerActivationRequest;
 use SimplyTestable\ApiBundle\Entity\State;
-use Symfony\Component\HttpKernel\Log\LoggerInterface as Logger;
 
-
-class WorkerActivationRequestService extends EntityService {
-    
-    const ENTITY_NAME = 'SimplyTestable\ApiBundle\Entity\WorkerActivationRequest';   
+class WorkerActivationRequestService extends EntityService
+{
+    const ENTITY_NAME = 'SimplyTestable\ApiBundle\Entity\WorkerActivationRequest';
     const STARTING_STATE = 'worker-activation-request-awaiting-verification';
-    
+
     /**
-     *
-     * @var Logger
+     * @var LoggerInterface
      */
-    private $logger;    
-    
+    private $logger;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\StateService
+     * @var StateService
      */
-    private $stateService;    
-    
+    private $stateService;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\HttpClientService
+     * @var HttpClientService
      */
     private $httpClientService;
-    
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Service\UrlService $urlService
+     * @var UrlService $urlService
      */
-    private $urlService;    
-    
+    private $urlService;
+
     /**
-     *
      * @param EntityManager $entityManager
-     * @param Logger $logger
-     * @param \SimplyTestable\ApiBundle\Services\StateService $stateService 
-     * @param \SimplyTestable\ApiBundle\Services\HttpClientService $httpClientService 
+     * @param LoggerInterface $logger
+     * @param StateService $stateService
+     * @param HttpClientService $httpClientService
      * @param \SimplyTestable\ApiBundle\Services\UrlService $urlService
      */
     public function __construct(
-            EntityManager $entityManager,
-            Logger $logger,
-            \SimplyTestable\ApiBundle\Services\StateService $stateService,
-            \SimplyTestable\ApiBundle\Services\HttpClientService $httpClientService,
-            \SimplyTestable\ApiBundle\Services\UrlService $urlService)            
-    {
-        parent::__construct($entityManager);        
-        
+        EntityManager $entityManager,
+        LoggerInterface $logger,
+        StateService $stateService,
+        HttpClientService $httpClientService,
+        UrlService $urlService
+    ) {
+        parent::__construct($entityManager);
+
         $this->logger = $logger;
         $this->stateService = $stateService;
-        $this->httpClientService = $httpClientService; 
+        $this->httpClientService = $httpClientService;
         $this->urlService = $urlService;
-    }  
-    
-   
-    
-    
+    }
+
     /**
-     *
      * @return string
      */
-    protected function getEntityName() {
+    protected function getEntityName()
+    {
         return self::ENTITY_NAME;
     }
-    
-    
+
     /**
-     *
      * @param WorkerActivationRequest $activationRequest
+     *
      * @return boolean
      */
-    public function verify(WorkerActivationRequest $activationRequest) {
+    public function verify(WorkerActivationRequest $activationRequest)
+    {
         $this->logger->info("WorkerActivationRequestService::verify: Initialising");
-        
-        $requestUrl = $this->urlService->prepare('http://' . $activationRequest->getWorker()->getHostname() . '/verify/');        
-        
+
+        $requestUrl = $this->urlService->prepare(
+            'http://' . $activationRequest->getWorker()->getHostname() . '/verify/'
+        );
+
         $httpRequest = $this->httpClientService->postRequest($requestUrl, null, array(
             'hostname' => $activationRequest->getWorker()->getHostname(),
             'token' => $activationRequest->getToken()
         ));
 
         $this->logger->info("WorkerActivationRequestService::verify: Requesting verification with " . $requestUrl);
-        
+
         try {
             $response = $httpRequest->send();
-        } catch (\Guzzle\Http\Exception\CurlException $curlException) {
-            $this->logger->info("WorkerActivationRequestService::verify" . $requestUrl . ": " . $curlException->getErrorNo().' '.$curlException->getError());
+        } catch (CurlException $curlException) {
+            $this->logger->info(sprintf(
+                'WorkerActivationRequestService::verify %s: %s %s',
+                $requestUrl,
+                $curlException->getErrorNo(),
+                $curlException->getError()
+            ));
             return false;
-        } catch (\Guzzle\Http\Exception\BadResponseException $badResponseException) {            
-            $response = $badResponseException->getResponse();            
-            $this->logger->info("WorkerActivationRequestService::verify " . $requestUrl . ": " . $badResponseException->getResponse()->getStatusCode().' '.$badResponseException->getResponse()->getReasonPhrase());
-        } 
+        } catch (BadResponseException $badResponseException) {
+            $response = $badResponseException->getResponse();
 
-        $this->logger->info("WorkerActivationRequestService::verify: " . $requestUrl . ": " . $response->getStatusCode()." ".$response->getReasonPhrase());
+            $this->logger->info(sprintf(
+                'WorkerActivationRequestService::verify %s: %s %s',
+                $requestUrl,
+                $badResponseException->getResponse()->getStatusCode(),
+                $badResponseException->getResponse()->getReasonPhrase()
+            ));
+        }
 
-        if ($response->getStatusCode() !== 200) {                
+        $this->logger->info(sprintf(
+            "WorkerActivationRequestService::verify: %s: %s %s",
+            $requestUrl,
+            $response->getStatusCode(),
+            $response->getReasonPhrase()
+        ));
+
+        if ($response->getStatusCode() !== 200) {
             if ($response->getStatusCode() === 503) {
-                $this->logger->info("WorkerActivationRequestService::verify: Worker at ".$activationRequest->getWorker()->getHostname()." is in read-only mode");
+                $this->logger->info(sprintf(
+                    'WorkerActivationRequestService::verify: Worker at %s is in read-only mode',
+                    $activationRequest->getWorker()->getHostname()
+                ));
             }
 
-            $this->logger->err("WorkerActivationRequestService::verify: Activation request failed");
+            $this->logger->error("WorkerActivationRequestService::verify: Activation request failed");
 
             return $response->getStatusCode();
         }
@@ -121,66 +135,64 @@ class WorkerActivationRequestService extends EntityService {
         $this->getManager()->persist($worker);
         $this->getManager()->flush();
 
-        return 0;           
-  
+        return 0;
     }
-    
-    
+
     /**
-     *
      * @param Worker $worker
+     *
      * @return boolean
      */
-    public function has(Worker $worker) {
+    public function has(Worker $worker)
+    {
         return !is_null($this->fetch($worker));
     }
-    
-   
+
     /**
-     *
      * @param Worker $worker
-     * @return WorkerActivationRequest 
-     */
-    public function fetch(Worker $worker) {
-        return $this->getEntityRepository()->findOneBy(
-            array('worker' => $worker
-        ));
-    }
-    
-    
-    /**
      *
+     * @return WorkerActivationRequest
+     */
+    public function fetch(Worker $worker)
+    {
+        return $this->getEntityRepository()->findOneBy(
+            array('worker' => $worker)
+        );
+    }
+
+    /**
      * @param Worker $worker
      * @param string $token
-     * @return WorkerActivationRequest 
+     *
+     * @return WorkerActivationRequest
      */
-    public function create(Worker $worker, $token) {             
+    public function create(Worker $worker, $token)
+    {
         $activationRequest = new WorkerActivationRequest();
         $activationRequest->setState($this->getStartingState());
         $activationRequest->setWorker($worker);
         $activationRequest->setToken($token);
-        
-        return $this->persistAndFlush($activationRequest);
-    }    
 
-    
+        return $this->persistAndFlush($activationRequest);
+    }
+
     /**
-     *
      * @param WorkerActivationRequest $workerActivationRequest
+     *
      * @return WorkerActivationRequest
      */
-    public function persistAndFlush(WorkerActivationRequest $workerActivationRequest) {
+    public function persistAndFlush(WorkerActivationRequest $workerActivationRequest)
+    {
         $this->getManager()->persist($workerActivationRequest);
         $this->getManager()->flush();
         return $workerActivationRequest;
     }
-    
-    
+
     /**
-     *
      * @return State
      */
-    public function getStartingState() {
+    public function getStartingState()
+    {
         return $this->stateService->fetch(self::STARTING_STATE);
     }
 }
