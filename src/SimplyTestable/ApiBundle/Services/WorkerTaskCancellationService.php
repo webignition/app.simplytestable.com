5c17424ef524a8c982cd58e2d0f18cce65e0afdc
Jon Cram <jon@webignition.net>
2017-07-17 14:24:01 +0100

    Remove old Logger from WorkerTaskCancellationService

--- src/SimplyTestable/ApiBundle/Services/WorkerTaskCancellationService.php
+++ src/SimplyTestable/ApiBundle/Services/WorkerTaskCancellationService.php
@@ -1,84 +1,119 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services;
 
-use Doctrine\ORM\EntityManager;
-use SimplyTestable\ApiBundle\Entity\Worker;
-use SimplyTestable\ApiBundle\Entity\State;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Exception\CurlException;
 use SimplyTestable\ApiBundle\Entity\Task\Task;
-use Symfony\Component\HttpKernel\Log\LoggerInterface as Logger;
-use SimplyTestable\ApiBundle\Entity\TimePeriod;
-use webignition\Http\Client\CurlException;
 
-
-class WorkerTaskCancellationService extends WorkerTaskService {
-    
-    
+class WorkerTaskCancellationService extends WorkerTaskService
+{
     /**
-     * 
-     * @param \SimplyTestable\ApiBundle\Entity\Task\Task $task
+     * @param Task $task
+     *
      * @return int
      */
-    public function cancel(Task $task) {        
+    public function cancel(Task $task)
+    {
         $this->logger->info("WorkerTaskCancellationService::cancel: Initialising");
-        $this->logger->info("WorkerTaskCancellationService::cancel: Processing task [".$task->getId()."] [".$task->getRemoteId()."] [".$task->getType()."] [".$task->getUrl()."]");
-        
-        if (!$this->taskService->isCancellable($task)) {
-            $this->logger->err("WorkerTaskCancellationService::cancel: Task not in cancellable state [".$task->getState()->getName()."]");
-            return -1;            
-        }        
-        
-        $requestUrl = $this->urlService->prepare('http://' . $task->getWorker()->getHostname() . '/task/cancel/');        
-        $httpRequest = $this->httpClientService->postRequest($requestUrl, null, array(
-            'id' => $task->getRemoteId()    
+        $this->logger->info(sprintf(
+            'WorkerTaskCancellationService::cancel: Processing task [%s] [%s] [%s] [%s]',
+            $task->getId(),
+            $task->getRemoteId(),
+            $task->getType()->getName(),
+            $task->getUrl()
         ));
-        
+
+        if (!$this->taskService->isCancellable($task)) {
+            $this->logger->error(sprintf(
+                'WorkerTaskCancellationService::cancel: Task not in cancellable state [%s]',
+                $task->getState()->getName()
+            ));
+
+            return -1;
+        }
+
+        $requestUrl = $this->urlService->prepare('http://' . $task->getWorker()->getHostname() . '/task/cancel/');
+        $httpRequest = $this->httpClientService->postRequest($requestUrl, null, array(
+            'id' => $task->getRemoteId()
+        ));
+
         try {
             $response = $httpRequest->send();
-        } catch (\Guzzle\Http\Exception\CurlException $curlException) {
-            $this->logger->info("WorkerTaskCancellationService::cancel::CurlException " . $requestUrl . ": " . $curlException->getErrorNo().' '.$curlException->getError());
+        } catch (CurlException $curlException) {
+            $this->logger->info(sprintf(
+                'WorkerTaskCancellationService::cancel::CurlException %s: %s %s',
+                $requestUrl,
+                $curlException->getErrorNo(),
+                $curlException->getError()
+            ));
             return false;
-        } catch (\Guzzle\Http\Exception\BadResponseException $badResponseException) {            
-            $response = $badResponseException->getResponse();            
-            $this->logger->info("WorkerTaskCancellationService::cancel::BadResponseException " . $requestUrl . ": " . $badResponseException->getResponse()->getStatusCode().' '.$badResponseException->getResponse()->getReasonPhrase());
-        } 
-        
+        } catch (BadResponseException $badResponseException) {
+            $response = $badResponseException->getResponse();
+            $this->logger->info(sprintf(
+                'WorkerTaskCancellationService::cancel::BadResponseException %s: %s %s',
+                $requestUrl,
+                $response->getStatusCode(),
+                $response->getReasonPhrase()
+            ));
+        }
+
         $this->taskService->cancel($task);
         return $response->getStatusCode();
-    }  
-    
-    
-    public function cancelCollection($tasks) {        
+    }
+
+    /**
+     * @param Task[] $tasks
+     *
+     * @return bool
+     */
+    public function cancelCollection($tasks)
+    {
         $remoteTaskIds = array();
         foreach ($tasks as $task) {
             /* @var Task $task */
             $remoteTaskIds[] = $task->getRemoteId();
         }
-        
+
         $remoteTaskIdsString = implode(',', $remoteTaskIds);
-        
+
         $this->logger->info("WorkerTaskCancellationService::cancelCollection: Initialising");
-        $this->logger->info("WorkerTaskCancellationService::cancelCollection: Processing remote IDs [".$remoteTaskIdsString."]");        
-        
-        $requestUrl = $this->urlService->prepare('http://' . $tasks[0]->getWorker()->getHostname() . '/task/cancel/collection/');               
-        
-        $httpRequest = $this->httpClientService->postRequest($requestUrl, null, array(
-            'ids' => $remoteTaskIdsString         
+        $this->logger->info(sprintf(
+            'WorkerTaskCancellationService::cancelCollection: Processing remote IDs [%s]',
+            $remoteTaskIdsString
         ));
-        
+
+        $requestUrl = $this->urlService->prepare(
+            'http://' . $tasks[0]->getWorker()->getHostname() . '/task/cancel/collection/'
+        );
+
+        $httpRequest = $this->httpClientService->postRequest($requestUrl, null, array(
+            'ids' => $remoteTaskIdsString
+        ));
+
         foreach ($tasks as $task) {
             $this->taskService->cancel($task);
         }
-        
+
         try {
-            $response = $httpRequest->send();
-        } catch (\Guzzle\Http\Exception\CurlException $curlException) {
-            $this->logger->info("WorkerTaskCancellationService::cancelCollection::CurlException " . $requestUrl . ": " . $curlException->getErrorNo().' '.$curlException->getError());
+            $httpRequest->send();
+        } catch (CurlException $curlException) {
+            $this->logger->info(sprintf(
+                'WorkerTaskCancellationService::cancelCollection::CurlException %s: %s %s',
+                $requestUrl,
+                $curlException->getErrorNo(),
+                $curlException->getError()
+            ));
             return false;
-        } catch (\Guzzle\Http\Exception\BadResponseException $badResponseException) {            
-            $this->logger->info("WorkerTaskCancellationService::cancelCollection::BadResponseException " . $requestUrl . ": " . $badResponseException->getResponse()->getStatusCode().' '.$badResponseException->getResponse()->getReasonPhrase());
+        } catch (BadResponseException $badResponseException) {
+            $this->logger->info(sprintf(
+                'WorkerTaskCancellationService::cancelCollection::BadResponseException %s: %s %s',
+                $requestUrl,
+                $badResponseException->getResponse()->getStatusCode(),
+                $badResponseException->getResponse()->getReasonPhrase()
+            ));
             return false;
         }
-        
-        return true;        
+
+        return true;
     }
 }
