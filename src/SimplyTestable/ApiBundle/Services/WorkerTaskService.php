7eb0557ac4babfed5e03d3628b5c1455cc47c19f
Jon Cram <jon@webignition.net>
2017-07-17 16:00:47 +0100

    Remove old Logger from WorkerTaskService

--- src/SimplyTestable/ApiBundle/Services/WorkerTaskService.php
+++ src/SimplyTestable/ApiBundle/Services/WorkerTaskService.php
@@ -1,74 +1,61 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services;
 
-use Doctrine\ORM\EntityManager;
-use SimplyTestable\ApiBundle\Entity\Worker;
-use SimplyTestable\ApiBundle\Entity\State;
-use SimplyTestable\ApiBundle\Entity\Task\Task;
-use Symfony\Component\HttpKernel\Log\LoggerInterface as Logger;
-use SimplyTestable\ApiBundle\Entity\TimePeriod;
+use Psr\Log\LoggerInterface;
 
+abstract class WorkerTaskService
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
 
-abstract class WorkerTaskService {
-    
     /**
-     *
-     * @var Logger
+     * @var WorkerService
      */
-    protected $logger;  
-    
+    protected $workerService;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\WorkerService
-     */
-    protected $workerService;    
-    
-    /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\StateService
+     * @var StateService
      */
     protected $stateService;
-    
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\HttpClientService
+     * @var HttpClientService
      */
     protected $httpClientService;
-    
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\UrlService
+     * @var UrlService
      */
-    protected $urlService; 
-    
-    
+    protected $urlService;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\TaskService
-     */    
+     * @var TaskService
+     */
     protected $taskService;
-    
+
     /**
      *
-     * @param Logger $logger
-     * @param \SimplyTestable\ApiBundle\Services\WorkerService $workerService 
-     * @param \SimplyTestable\ApiBundle\Services\StateService $stateService 
-     * @param \SimplyTestable\ApiBundle\Services\HttpClientService $httpClientService
-     * @param \SimplyTestable\ApiBundle\Services\UrlService $urlService
-     * @param \SimplyTestable\ApiBundle\Services\TaskService $taskService
+     * @param LoggerInterface $logger
+     * @param WorkerService $workerService
+     * @param StateService $stateService
+     * @param HttpClientService $httpClientService
+     * @param UrlService $urlService
+     * @param TaskService $taskService
      */
     public function __construct(
-            Logger $logger,
-            \SimplyTestable\ApiBundle\Services\WorkerService $workerService,
-            \SimplyTestable\ApiBundle\Services\StateService $stateService,
-            \SimplyTestable\ApiBundle\Services\HttpClientService $httpClientService,
-            \SimplyTestable\ApiBundle\Services\UrlService $urlService,
-            \SimplyTestable\ApiBundle\Services\TaskService $taskService)            
-    {        
+        LoggerInterface $logger,
+        WorkerService $workerService,
+        StateService $stateService,
+        HttpClientService $httpClientService,
+        UrlService $urlService,
+        TaskService $taskService
+    ) {
         $this->logger = $logger;
         $this->workerService = $workerService;
         $this->stateService = $stateService;
-        $this->httpClientService = $httpClientService; 
+        $this->httpClientService = $httpClientService;
         $this->urlService = $urlService;
         $this->taskService = $taskService;
     }
