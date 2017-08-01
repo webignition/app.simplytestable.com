c4a39202ab0dccab660df6a370e576ccb52ced3b
Jon Cram <jon@webignition.net>
2017-08-01 15:54:41 +0100

    Factor out url resolution to own service

--- src/SimplyTestable/ApiBundle/Services/Job/WebsiteResolutionService.php
+++ src/SimplyTestable/ApiBundle/Services/Job/WebsiteResolutionService.php
@@ -1,127 +1,110 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services\Job;
 
+use Guzzle\Http\Exception\CurlException;
 use SimplyTestable\ApiBundle\Entity\Job\Job;
 use SimplyTestable\ApiBundle\Exception\Services\Job\WebsiteResolutionException;
+use SimplyTestable\ApiBundle\Services\HttpClientService;
+use SimplyTestable\ApiBundle\Services\JobService;
+use SimplyTestable\ApiBundle\Services\UrlResolver;
+use SimplyTestable\ApiBundle\Services\WebSiteService;
+use webignition\Url\Url;
 
-class WebsiteResolutionService {
-    
-    const RETRUN_CODE_OK = 0;
-    
+class WebsiteResolutionService
+{
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\JobService
+     * @var JobService
      */
-    private $jobService;    
-    
-    
+    private $jobService;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\HttpClientService
+     * @var HttpClientService
      */
-    private $httpClientService;      
-  
+    private $httpClientService;
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\WebSiteService
+     * @var WebSiteService
      */
     private $websiteService;
-    
-  
+
     /**
-     *
-     * @var \SimplyTestable\ApiBundle\Services\Job\RejectionService
+     * @var RejectionService
      */
-    private $jobRejectionService;    
-    
-    
+    private $jobRejectionService;
+
     /**
-     *
-     * @var \webignition\Url\Resolver\Resolver
+     * @var UrlResolver
      */
     private $urlResolver = null;
-    
-    
+
+    /**
+     * @param JobService $jobService
+     * @param HttpClientService $httpClientService
+     * @param WebSiteService $websiteService
+     * @param RejectionService $jobRejectionService
+     * @param UrlResolver $urlResolver
+     */
     public function __construct(
-        \SimplyTestable\ApiBundle\Services\JobService $jobService,
-        \SimplyTestable\ApiBundle\Services\HttpClientService $httpClientService,
-        \SimplyTestable\ApiBundle\Services\WebSiteService $websiteService,
-        \SimplyTestable\ApiBundle\Services\Job\RejectionService $jobRejectionService
+        JobService $jobService,
+        HttpClientService $httpClientService,
+        WebSiteService $websiteService,
+        RejectionService $jobRejectionService,
+        UrlResolver $urlResolver
     ) {
         $this->jobService = $jobService;
         $this->httpClientService = $httpClientService;
         $this->websiteService = $websiteService;
         $this->jobRejectionService = $jobRejectionService;
+        $this->urlResolver = $urlResolver;
     }
-    
-    
-    public function resolve(Job $job) {        
+
+    /**
+     * @param Job $job
+     * @throws WebsiteResolutionException
+     */
+    public function resolve(Job $job)
+    {
         if (!$this->jobService->isNew($job)) {
             throw new WebsiteResolutionException(
                 'Job is in wrong state, currently "'.$job->getState()->getName().'"',
                 WebsiteResolutionException::CODE_JOB_IN_WRONG_STATE_CODE
             );
         }
-        
+
         $job->setState($this->jobService->getResolvingState());
         $this->jobService->persistAndFlush($job);
-        
+
+        $this->urlResolver->configureForJob($job);
+
         try {
-            $resolvedUrl = $this->getUrlResolver($job)->resolve($job->getWebsite()->getCanonicalUrl());
+            $resolvedUrl = $this->urlResolver->resolve($job->getWebsite()->getCanonicalUrl());
 
             if ($job->getType()->getName() == 'Full site') {
                 $resolvedUrl = $this->trimToRootUrl($resolvedUrl);
-            }        
+            }
 
-            if ($job->getWebsite()->getCanonicalUrl() != $resolvedUrl) {            
+            if ($job->getWebsite()->getCanonicalUrl() != $resolvedUrl) {
                 $job->setWebsite($this->websiteService->fetch($resolvedUrl));
             }
 
-            $job->setState($this->jobService->getResolvedState());            
-        } catch (\Guzzle\Http\Exception\CurlException $curlException) {                        
+            $job->setState($this->jobService->getResolvedState());
+        } catch (CurlException $curlException) {
             $this->jobRejectionService->reject($job, 'curl-' . $curlException->getErrorNo());
         }
-        
 
-        $this->jobService->persistAndFlush($job);        
+
+        $this->jobService->persistAndFlush($job);
     }
-    
-    
+
     /**
-     * 
      * @param string $url
+     *
      * @return string
      */
-    private function trimToRootUrl($url) {
-        $urlObject = new \webignition\Url\Url($url);
+    private function trimToRootUrl($url)
+    {
+        $urlObject = new Url($url);
+
         return $urlObject->getScheme() . '://' . $urlObject->getHost() . '/';
-    }
-    
-    
-    /**
-     * @return \webignition\Url\Resolver\Resolver
-     */
-    public function getUrlResolver(Job $job) {        
-        if (is_null($this->urlResolver)) {
-            $this->urlResolver = new \webignition\Url\Resolver\Resolver();
-            
-            $baseRequest = $this->httpClientService->getRequest();            
-            $baseRequest->getCurlOptions()->set(CURLOPT_TIMEOUT_MS, 10000);
-            $baseRequest->setHeader('user-agent', 'ST url resolver (http://bit.ly/RlhKCL)');
-            
-            $this->urlResolver->getConfiguration()->enableFollowMetaRedirects();
-            $this->urlResolver->getConfiguration()->enableRetryWithUrlEncodingDisabled();
-            $this->urlResolver->getConfiguration()->setBaseRequest($baseRequest);
-            
-            if ($job->hasParameter('cookies')) {
-                $this->urlResolver->getConfiguration()->setCookies($job->getParameter('cookies'));
-            }
-        }
-        
-        if ($job->hasParameter('http-auth-username') || $job->hasParameter('http-auth-password')) {            
-            $this->httpClientService->prepareRequest($this->urlResolver->getConfiguration()->getBaseRequest(), $job->getParametersArray());
-        }
-        
-        return $this->urlResolver;
     }
 }
--- src/SimplyTestable/ApiBundle/Services/TestHttpClientService.php
+++ src/SimplyTestable/ApiBundle/Services/TestHttpClientService.php
@@ -2,90 +2,67 @@
 namespace SimplyTestable\ApiBundle\Services;
 
 use Guzzle\Http\Client as HttpClient;
+use Guzzle\Http\Exception\CurlException;
+use Guzzle\Plugin\History\HistoryPlugin;
+use Guzzle\Plugin\Mock\MockPlugin;
 
-class TestHttpClientService extends HttpClientService {     
-    
-    public function get() {
-        if (is_null($this->httpClient)) {            
+class TestHttpClientService extends HttpClientService
+{
+    /**
+     * @var HistoryPlugin
+     */
+    private $historyPlugin;
+
+    /**
+     * @var MockPlugin
+     */
+    private $mockPlugin;
+
+    public function get()
+    {
+        if (is_null($this->httpClient)) {
+            $this->historyPlugin = new HistoryPlugin();
+            $this->mockPlugin = new MockPlugin();
+
             $this->httpClient = new HttpClient();
-            $this->httpClient->addSubscriber(new \Guzzle\Plugin\History\HistoryPlugin());            
+            $this->httpClient->addSubscriber($this->historyPlugin);
+            $this->httpClient->addSubscriber($this->mockPlugin);
         }
-        
+
         return $this->httpClient;
     }
-    
-    
-    public function queueFixtures($fixtures) {
+
+    public function queueFixtures($fixtures)
+    {
         foreach ($fixtures as $fixture) {
             $this->queueFixture($fixture);
         }
     }
-    
-    
-    public function queueFixture($fixture) {
-        if ($fixture instanceof \Exception) {             
-            $this->getMockPlugin()->addException($fixture);
+
+    public function queueFixture($fixture)
+    {
+        $this->get();
+
+        if ($fixture instanceof CurlException) {
+            $this->mockPlugin->addException($fixture);
         } else {
-            $this->getMockPlugin()->addResponse($fixture);
+            $this->mockPlugin->addResponse($fixture);
         }
     }
-    
-    
-    
+
     /**
-     * 
-     * @return \Guzzle\Plugin\Mock\MockPlugin
+     * @return MockPlugin
      */
-    public function getMockPlugin() {
-        if (!$this->hasMockPlugin()) {
-            $this->get()->addSubscriber(new \Guzzle\Plugin\Mock\MockPlugin()); 
-        }
-        
-        $beforeSendListeners = $this->get()->getEventDispatcher()->getListeners('request.before_send');
-        
-        foreach ($beforeSendListeners as $beforeSendListener) {
-            if (get_class($beforeSendListener[0]) == 'Guzzle\Plugin\Mock\MockPlugin') {
-                return $beforeSendListener[0];
-            }
-        }
+    public function getMockPlugin()
+    {
+        return $this->mockPlugin;
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return HistoryPlugin
      */
-    private function hasMockPlugin() {
-        $beforeSendListeners = $this->get()->getEventDispatcher()->getListeners('request.before_send');
-        
-        foreach ($beforeSendListeners as $beforeSendListener) {
-            if (get_class($beforeSendListener[0]) == 'Guzzle\Plugin\Mock\MockPlugin') {
-                return true;
-            }
-        }
-        
-        return false;     
+    public function getHistoryPlugin()
+    {
+        return $this->historyPlugin;
     }
-    
-    
-    /**
-     * 
-     * @return \Guzzle\Plugin\History\HistoryPlugin
-     */
-    public function getHistoryPlugin() {
-        return $this->getPluginByClassAndEvent('Guzzle\Plugin\History\HistoryPlugin', 'request.sent');
-    }
-    
-    
-    
-    private function getPluginByClassAndEvent($class, $event) {
-        $listeners = $this->get()->getEventDispatcher()->getListeners($event);
-        
-        foreach ($listeners as $listener) {            
-            if (get_class($listener[0]) == $class) {
-                return $listener[0];
-            }
-        }        
-    }
-    
 }
