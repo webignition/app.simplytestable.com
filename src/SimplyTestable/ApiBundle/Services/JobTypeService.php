0edba1fcf9fb65debfa0ab436488422bb1bc2441
Jon Cram <jon@webignition.net>
2017-08-04 17:11:33 +0100

    PSR2 CS JobTypeService

--- src/SimplyTestable/ApiBundle/Services/JobTypeService.php
+++ src/SimplyTestable/ApiBundle/Services/JobTypeService.php
@@ -1,80 +1,81 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services;
 
-use Doctrine\ORM\EntityManager;
 use SimplyTestable\ApiBundle\Entity\Job\Type;
 
-class JobTypeService extends EntityService {
-    
-    const ENTITY_NAME = 'SimplyTestable\ApiBundle\Entity\Job\Type';   
+class JobTypeService extends EntityService
+{
     const DEFAULT_TYPE_ID = 1;
     const FULL_SITE_NAME = 'full site';
     const SINGLE_URL_NAME = 'single url';
     const CRAWL_NAME = 'crawl';
-    
+
     /**
+     * {@inheritdoc}
+     */
+    protected function getEntityName()
+    {
+        return Type::class;
+    }
+
+    /**
+     * @return Type
+     */
+    public function getDefaultType()
+    {
+        /* @var Type $type */
+        $type = $this->getEntityRepository()->find(self::DEFAULT_TYPE_ID);
+
+        return $type;
+    }
+
+
+    /**
+     * @param string $name
      *
-     * @return string
-     */
-    protected function getEntityName() {
-        return self::ENTITY_NAME;
-    }
-    
-    
-    /**
-     * 
      * @return Type
      */
-    public function getDefaultType() {
-        return $this->getEntityRepository()->find(self::DEFAULT_TYPE_ID);
-    }
-    
-    
-    /**
-     * 
-     * @param string $name
-     * @return Type
-     */
-    public function getByName($name) {
-        return $this->getEntityRepository()->findOneBy(array(
+    public function getByName($name)
+    {
+        /* @var Type $type */
+        $type = $this->getEntityRepository()->findOneBy([
             'name' => $name
-        ));
+        ]);
+
+        return $type;
     }
-    
-    
+
     /**
-     * 
      * @param string $name
-     * @return boolean
+     *
+     * @return bool
      */
-    public function has($name) {
+    public function has($name)
+    {
         return !is_null($this->getByName($name));
     }
-    
-    
+
     /**
-     * 
      * @return Type
      */
-    public function getFullSiteType() {
+    public function getFullSiteType()
+    {
         return $this->getByName(self::FULL_SITE_NAME);
     }
-    
+
     /**
-     * 
      * @return Type
      */
-    public function getSingleUrlType() {
+    public function getSingleUrlType()
+    {
         return $this->getByName(self::SINGLE_URL_NAME);
-    }      
-    
-    
+    }
+
     /**
-     * 
      * @return Type
      */
-    public function getCrawlType() {
+    public function getCrawlType()
+    {
         return $this->getByName(self::CRAWL_NAME);
-    }    
-    
+    }
 }
