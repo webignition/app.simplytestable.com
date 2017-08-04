a673ffc1c8adc1cc63033beed9ea80bdb37bc63e
Jon Cram <webignition@gmail.com>
2017-08-04 08:28:47 +0100

    Reimplement Task/QueueService tests (#29)

--- src/SimplyTestable/ApiBundle/Services/Task/QueueService.php
+++ src/SimplyTestable/ApiBundle/Services/Task/QueueService.php
@@ -3,106 +3,65 @@ namespace SimplyTestable\ApiBundle\Services\Task;
 
 use SimplyTestable\ApiBundle\Services\JobService;
 use SimplyTestable\ApiBundle\Services\TaskService;
-use SimplyTestable\ApiBundle\Entity\Job\Job;
 
-class QueueService {
-
+class QueueService
+{
     /**
-     *
      * @var JobService
      */
     private $jobService;
 
-
     /**
-     *
      * @var TaskService
      */
     private $taskService;
-
 
     /**
      * @var int
      */
     private $limit = 1;
 
-
-    /**
-     * @var Job
-     */
-    private $job = null;
-
-
     /**
      * @param JobService $jobService
      * @param TaskService $taskService
      */
-    public function __construct(JobService $jobService, TaskService $taskService) {
+    public function __construct(JobService $jobService, TaskService $taskService)
+    {
         $this->jobService = $jobService;
         $this->taskService = $taskService;
     }
 
-
     /**
      * @param int $limit
-     * @return $this
      */
-    public function setLimit($limit) {
+    public function setLimit($limit)
+    {
         $this->limit = $limit;
-        return $this;
     }
-
-
-    /**
-     * @return int
-     */
-    public function getLimit() {
-        return $this->limit;
-    }
-
-
-    /**
-     * @param Job $job
-     * @return $this
-     */
-    public function setJob(Job $job) {
-        $this->job = $job;
-        return $this;
-    }
-
-
-    /**
-     * @return Job
-     */
-    public function getJob() {
-        return $this->job;
-    }
-
-
-    /**
-     * @return $this
-     */
-    public function clearJob() {
-        $this->job = null;
-        return $this;
-    }
-
-
 
     /**
      * @return int[]
      */
-    public function getNext() {
-        $incompleteJobs = $this->getIncompleteJobSet();
-        if (count($incompleteJobs) === 0) {
+    public function getNext()
+    {
+        $incompleteJobsWithQueuedTasks = $this->jobService->getEntityRepository()->getByStatesAndTaskStates(
+            $this->jobService->getIncompleteStates(),
+            [
+                $this->taskService->getQueuedState()
+            ]
+        );
+
+        if (empty($incompleteJobsWithQueuedTasks)) {
             return [];
         }
 
         $jobTaskIds = [];
-        foreach ($incompleteJobs as $job) {
-            $taskIdsForJob = $this->taskService->getEntityRepository()->getIdsByJobAndTaskStates($job, [
-                $this->taskService->getQueuedState()
-            ], $this->getLimit());
+        foreach ($incompleteJobsWithQueuedTasks as $job) {
+            $taskIdsForJob = $this->taskService->getEntityRepository()->getIdsByJobAndTaskStates(
+                $job,
+                [$this->taskService->getQueuedState()],
+                $this->limit
+            );
 
             if (count($taskIdsForJob)) {
                 $jobTaskIds[$job->getId()] =  $taskIdsForJob;
@@ -110,7 +69,7 @@ class QueueService {
         }
 
         $taskIds = [];
-        while (count($taskIds) < ($this->getLimit()) && count($jobTaskIds) > 0) {
+        while (count($taskIds) < ($this->limit) && count($jobTaskIds) > 0) {
             foreach ($jobTaskIds as $jobId => $taskIdSet) {
                 $taskIds[] = array_shift($taskIdSet);
 
@@ -122,31 +81,10 @@ class QueueService {
             }
         }
 
-        if (count($taskIds) > $this->getLimit()) {
-            $taskIds = array_slice($taskIds, 0, $this->getLimit());
+        if (count($taskIds) > $this->limit) {
+            $taskIds = array_slice($taskIds, 0, $this->limit);
         }
 
         return $taskIds;
     }
-
-
-    private function getIncompleteJobSet() {
-        $incompleteJobs =  $this->jobService->getEntityRepository()->getByStatesAndTaskStates(
-            $this->jobService->getIncompleteStates(),
-            [
-                $this->taskService->getQueuedState()
-            ]
-        );
-
-        if (!is_null($this->job)) {
-            foreach ($incompleteJobs as $jobIndex => $job) {
-                if ($job->getId() !== $this->job->getId()) {
-                    unset($incompleteJobs[$jobIndex]);
-                }
-            }
-        }
-
-        return $incompleteJobs;
-    }
-
 }
--- src/SimplyTestable/ApiBundle/Tests/Functional/Services/Task/QueueService/GetNext/WithJobs/Job1Task1Test.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Services\Task\QueueService\GetNext\WithJobs;
-
-class Job1Task1test extends WithJobsTest {}
