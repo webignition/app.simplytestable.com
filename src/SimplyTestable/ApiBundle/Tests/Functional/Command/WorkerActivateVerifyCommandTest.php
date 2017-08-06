f9dfe3ca18e5d4f2461b649703b388de658440a0
Jon Cram <jon@webignition.net>
2017-08-06 07:42:19 +0100

    Reimplement WorkerActivateVerifyCommandTest

--- src/SimplyTestable/ApiBundle/Tests/Functional/Command/WorkerActivateVerifyCommandTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Command/WorkerActivateVerifyCommandTest.php
@@ -2,142 +2,146 @@
 
 namespace SimplyTestable\ApiBundle\Tests\Functional\Command;
 
+use SimplyTestable\ApiBundle\Command\WorkerActivateVerifyCommand;
+use SimplyTestable\ApiBundle\Controller\MaintenanceController;
 use SimplyTestable\ApiBundle\Tests\Factory\HttpFixtureFactory;
-use SimplyTestable\ApiBundle\Tests\Functional\ConsoleCommandTestCase;
-use SimplyTestable\ApiBundle\Entity\Worker;
-use SimplyTestable\ApiBundle\Entity\WorkerActivationRequest;
+use SimplyTestable\ApiBundle\Tests\Factory\WorkerFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Guzzle\Http\Message\Response as GuzzleResponse;
 
-class WorkerActivateVerifyCommandTest extends ConsoleCommandTestCase {
+class WorkerActivateVerifyCommandTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @var WorkerActivateVerifyCommand
+     */
+    private $command;
 
     /**
-     *
-     * @return string
+     * @var WorkerFactory
      */
-    protected function getCommandName() {
-        return 'simplytestable:worker:activate:verify';
+    private $workerFactory;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->command = new WorkerActivateVerifyCommand();
+        $this->command->setContainer($this->container);
+
+        $this->workerFactory = new WorkerFactory($this->container);
     }
 
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        $maintenanceController = new MaintenanceController();
+        $maintenanceController->setContainer($this->container);
+        $maintenanceController->enableReadOnlyAction();
 
-    /**
-     *
-     * @return \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand[]
-     */
-    protected function getAdditionalCommands() {
-        return array(
-            new \SimplyTestable\ApiBundle\Command\WorkerActivateVerifyCommand()
+        $returnCode = $this->command->run(new ArrayInput([
+            'id' => 1,
+        ]), new BufferedOutput());
+
+        $this->assertEquals(
+            WorkerActivateVerifyCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $returnCode
         );
     }
 
-    public function testSuccessfulActivateVerifyWorker() {
+    /**
+     * @dataProvider runHttpErrorDataProvider
+     *
+     * @param array $httpFixtures
+     * @param int $expectedReturnCode
+     */
+    public function testRunHttpError($httpFixtures, $expectedReturnCode)
+    {
+        $workerActivationRequestService = $this->container->get(
+            'simplytestable.services.workeractivationrequestservice'
+        );
+
+        $this->queueHttpFixtures($httpFixtures);
+
+        $token = 'token';
+        $worker = $this->workerFactory->create();
+
+        $request = $workerActivationRequestService->create($worker, $token);
+
+        $this->assertTrue($request->getWorker()->equals($worker));
+        $this->assertEquals($workerActivationRequestService->getStartingState(), $request->getState());
+
+        $returnCode = $this->command->run(new ArrayInput([
+            'id' => $worker->getId(),
+        ]), new BufferedOutput());
+
+        $this->assertEquals(
+            $expectedReturnCode,
+            $returnCode
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function runHttpErrorDataProvider()
+    {
+        return [
+            'Bad Request' => [
+                'httpFixtures' => [
+                    GuzzleResponse::fromMessage('HTTP/1.1 400 Bad Request'),
+                ],
+                'expectedReturnCode' => 400,
+            ],
+            'Not Found' => [
+                'httpFixtures' => [
+                    GuzzleResponse::fromMessage('HTTP/1.1 404 Not Found'),
+                ],
+                'expectedReturnCode' => 404,
+            ],
+            'Internal Server Error' => [
+                'httpFixtures' => [
+                    GuzzleResponse::fromMessage('HTTP/1.1 500 Internal Server Error'),
+                ],
+                'expectedReturnCode' => 500,
+            ],
+            'Service Unavailable' => [
+                'httpFixtures' => [
+                    GuzzleResponse::fromMessage('HTTP/1.1 503 Service Unavailable'),
+                ],
+                'expectedReturnCode' => 503,
+            ],
+        ];
+    }
+
+    public function testRunSuccess()
+    {
+        $workerActivationRequestService = $this->container->get(
+            'simplytestable.services.workeractivationrequestservice'
+        );
+
         $this->queueHttpFixtures([
             HttpFixtureFactory::createSuccessResponse(),
         ]);
 
-        $activationRequestToken = 'token';
-        $worker = $this->createWorker();
+        $token = 'token';
+        $worker = $this->workerFactory->create();
 
-        $activationRequest = $this->createActivationRequest($worker, $activationRequestToken);
+        $request = $workerActivationRequestService->create($worker, $token);
 
-        $this->assertTrue($activationRequest->getWorker()->equals($worker));
-        $this->assertTrue($activationRequest->getState()->equals($this->getWorkerActivationRequestService()->getStartingState()));
+        $this->assertTrue($request->getWorker()->equals($worker));
+        $this->assertEquals($workerActivationRequestService->getStartingState(), $request->getState());
 
-        $this->assertReturnCode(0, array(
-            'id' => $worker->getId()
-        ));
+        $returnCode = $this->command->run(new ArrayInput([
+            'id' => $worker->getId(),
+        ]), new BufferedOutput());
+
+        $this->assertEquals(
+            0,
+            $returnCode
+        );
     }
-
-
-    public function testFailedActivateVerifyWorker() {
-        $this->queueHttpFixtures([
-            HttpFixtureFactory::createBadRequestResponse(),
-        ]);
-
-        $activationRequestToken = 'invalid-token';
-        $worker = $this->createWorker();
-
-        $activationRequest = $this->createActivationRequest($worker, $activationRequestToken);
-
-        $this->assertTrue($activationRequest->getWorker()->equals($worker));
-        $this->assertTrue($activationRequest->getState()->equals($this->getWorkerActivationRequestService()->getStartingState()));
-
-        $this->assertReturnCode(400, array(
-            'id' => $worker->getId()
-        ));
-    }
-
-
-    public function testActivateVerifyWhenWorkerIsInMaintenanceReadyOnlyMode() {
-        $this->queueHttpFixtures([
-            HttpFixtureFactory::createServiceUnavailableResponse(),
-        ]);
-
-        $activationRequestToken = 'token';
-        $worker = $this->createWorker();
-
-        $activationRequest = $this->createActivationRequest($worker, $activationRequestToken);
-
-        $this->assertTrue($activationRequest->getWorker()->equals($worker));
-        $this->assertTrue($activationRequest->getState()->equals($this->getWorkerActivationRequestService()->getStartingState()));
-
-        $this->assertReturnCode(503, array(
-            'id' => $worker->getId()
-        ));
-    }
-
-    public function testActivateVerifyInIsInMaintenanceReadyOnlyModeReturnsCode1() {
-        $this->executeCommand('simplytestable:maintenance:enable-read-only');
-        $this->assertReturnCode(1, array(
-            'id' => 1
-        ));
-    }
-
-
-    public function testActivateVerifyRaisesHttpClientError() {
-        $this->queueHttpFixtures([
-            HttpFixtureFactory::createNotFoundResponse(),
-        ]);
-
-        $activationRequestToken = 'token';
-        $worker = $this->createWorker();
-
-        $activationRequest = $this->createActivationRequest($worker, $activationRequestToken);
-
-        $this->assertTrue($activationRequest->getWorker()->equals($worker));
-        $this->assertTrue($activationRequest->getState()->equals($this->getWorkerActivationRequestService()->getStartingState()));
-
-        $this->assertReturnCode(404, array(
-            'id' => $worker->getId()
-        ));
-    }
-
-
-    public function testActivateVerifyRaisesHttpServerError() {
-        $this->queueHttpFixtures([
-            HttpFixtureFactory::createInternalServerErrorResponse(),
-        ]);
-
-        $activationRequestToken = 'token';
-        $worker = $this->createWorker();
-
-        $activationRequest = $this->createActivationRequest($worker, $activationRequestToken);
-
-        $this->assertTrue($activationRequest->getWorker()->equals($worker));
-        $this->assertTrue($activationRequest->getState()->equals($this->getWorkerActivationRequestService()->getStartingState()));
-
-        $this->assertReturnCode(500, array(
-            'id' => $worker->getId()
-        ));
-    }
-
-
-    /**
-     *
-     * @param Worker $worker
-     * @param string $token
-     * @return \SimplyTestable\ApiBundle\Entity\WorkerActivationRequest
-     */
-    private function createActivationRequest(Worker $worker, $token) {
-        return $this->getWorkerActivationRequestService()->create($worker, $token);
-    }
-
 }
