3cc2adf2b02519fb1588c507bec3ad5a83d312eb
Jon Cram <jon@webignition.net>
2017-08-15 17:49:35 +0100

    PSR2 JobUserAccountPlanEnforcementService

--- src/SimplyTestable/ApiBundle/Services/JobUserAccountPlanEnforcementService.php
+++ src/SimplyTestable/ApiBundle/Services/JobUserAccountPlanEnforcementService.php
@@ -1,63 +1,48 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services;
 
-use SimplyTestable\ApiBundle\Services\UserAccountPlanService;
-use SimplyTestable\ApiBundle\Services\JobService;
-use SimplyTestable\ApiBundle\Services\TaskService;
 use SimplyTestable\ApiBundle\Services\Team\Service as TeamService;
 use SimplyTestable\ApiBundle\Entity\User;
 use SimplyTestable\ApiBundle\Entity\Job\Type as JobType;
 use SimplyTestable\ApiBundle\Entity\WebSite;
 use SimplyTestable\ApiBundle\Entity\Account\Plan\Constraint as AccountPlanConstraint;
 
-class JobUserAccountPlanEnforcementService {
-    
+class JobUserAccountPlanEnforcementService
+{
     const FULL_SITE_JOBS_PER_SITE_CONSTRAINT_NAME = 'full_site_jobs_per_site';
     const SINGLE_URL_JOBS_PER_URL_CONSTRAINT_NAME = 'single_url_jobs_per_url';
     const URLS_PER_JOB_CONSTRAINT_NAME = 'urls_per_job';
     const CREDITS_PER_MONTH_CONSTRAINT_NAME = 'credits_per_month';
-    
-    
+
     /**
-     *
      * @var UserAccountPlanService
      */
     private $userAccountPlanService;
-    
-    
+
     /**
-     *
      * @var JobService
      */
     private $jobService;
-    
-    
+
     /**
-     *
      * @var TaskService
      */
-    private $taskService;    
-    
-    
+    private $taskService;
+
     /**
-     *
      * @var User
      */
     private $user;
-    
-    
+
     /**
-     *
      * @var JobType
      */
     private $jobType;
-
 
     /**
      * @var TeamService
      */
     private $teamService;
-
 
     /**
      * @param UserAccountPlanService $userAccountPlanService
@@ -66,101 +51,124 @@ class JobUserAccountPlanEnforcementService {
      * @param TeamService $teamService
      */
     public function __construct(
-            UserAccountPlanService $userAccountPlanService,
-            JobService $jobService,
-            TaskService $taskService,
-            TeamService $teamService) {
+        UserAccountPlanService $userAccountPlanService,
+        JobService $jobService,
+        TaskService $taskService,
+        TeamService $teamService
+    ) {
         $this->userAccountPlanService = $userAccountPlanService;
         $this->jobService = $jobService;
         $this->taskService = $taskService;
         $this->teamService = $teamService;
     }
-    
-    
+
     /**
-     * 
      * @return UserAccountPlanService
      */
-    public function getUserAccountPlanService() {
+    public function getUserAccountPlanService()
+    {
         return $this->userAccountPlanService;
     }
-    
-    
+
     /**
-     * 
      * @param User $user
      */
-    public function setUser(User $user) {
+    public function setUser(User $user)
+    {
         $this->user = $user;
     }
-    
-    
+
     /**
-     * 
      * @param JobType $jobType
      */
-    public function setJobType(JobType $jobType) {
+    public function setJobType(JobType $jobType)
+    {
         $this->jobType = $jobType;
     }
-    
-    
+
     /**
-     * 
      * @param WebSite $website
-     * @return boolean
+     *
+     * @return bool
      */
-    public function isFullSiteJobLimitReachedForWebSite(WebSite $website) {
+    public function isFullSiteJobLimitReachedForWebSite(WebSite $website)
+    {
         $userAccountPlan = $this->userAccountPlanService->getForUser($this->user);
 
         if (!$userAccountPlan->getPlan()->hasConstraintNamed(self::FULL_SITE_JOBS_PER_SITE_CONSTRAINT_NAME)) {
             return false;
         }
-        
-        $currentCount = $this->jobService->getEntityRepository()->getJobCountByUserAndJobTypeAndWebsiteForCurrentMonth($this->user, $this->jobType, $website);        
+
+        $jobRepository = $this->jobService->getEntityRepository();
+
+        $currentCount = $jobRepository->getJobCountByUserAndJobTypeAndWebsiteForCurrentMonth(
+            $this->user,
+            $this->jobType,
+            $website
+        );
+
         if ($currentCount === 0) {
             return false;
         }
-        
-        return $currentCount >= $userAccountPlan->getPlan()->getConstraintNamed(self::FULL_SITE_JOBS_PER_SITE_CONSTRAINT_NAME)->getLimit();
+
+        return $currentCount >= $this->getFullSiteJobLimitConstraint()->getLimit();
     }
-    
-    
-    public function isSingleUrlLimitReachedForWebsite(WebSite $website) {
+
+    /**
+     * @param WebSite $website
+     *
+     * @return bool
+     */
+    public function isSingleUrlLimitReachedForWebsite(WebSite $website)
+    {
         $userAccountPlan = $this->userAccountPlanService->getForUser($this->user);
 
         if (!$userAccountPlan->getPlan()->hasConstraintNamed(self::SINGLE_URL_JOBS_PER_URL_CONSTRAINT_NAME)) {
             return false;
         }
-        
-        $currentCount = $this->jobService->getEntityRepository()->getJobCountByUserAndJobTypeAndWebsiteForCurrentMonth($this->user, $this->jobType, $website);
+
+        $jobRepository = $this->jobService->getEntityRepository();
+
+        $currentCount = $jobRepository->getJobCountByUserAndJobTypeAndWebsiteForCurrentMonth(
+            $this->user,
+            $this->jobType,
+            $website
+        );
+
         if ($currentCount === 0) {
             return false;
         }
-        
-        return $currentCount >= $userAccountPlan->getPlan()->getConstraintNamed(self::SINGLE_URL_JOBS_PER_URL_CONSTRAINT_NAME)->getLimit();
+
+        return $currentCount >= $this->getSingleUrlJobLimitConstraint()->getLimit();
     }
-    
-    
-    public function isJobUrlLimitReached($urlCount) {
+
+
+    /**
+     * @param $urlCount
+     *
+     * @return bool
+     */
+    public function isJobUrlLimitReached($urlCount)
+    {
         if ($urlCount === 0) {
             return false;
         }
-        
+
         $userAccountPlan = $this->userAccountPlanService->getForUser($this->user);
 
         if (!$userAccountPlan->getPlan()->hasConstraintNamed(self::URLS_PER_JOB_CONSTRAINT_NAME)) {
             return false;
         }
-        
-        return $urlCount > $userAccountPlan->getPlan()->getConstraintNamed(self::URLS_PER_JOB_CONSTRAINT_NAME)->getLimit();        
+
+        return $urlCount > $this->getJobUrlLimitConstraint()->getLimit();
     }
-    
-    
+
+
     /**
-     * 
      * @return int
      */
-    public function getCreditsUsedThisMonth() {
+    public function getCreditsUsedThisMonth()
+    {
         return $this->taskService->getEntityRepository()->getCountByUsersAndStatesForCurrentMonth(
             $this->teamService->getPeopleForUser($this->user),
             array(
@@ -172,57 +180,59 @@ class JobUserAccountPlanEnforcementService {
             )
         );
     }
-    
 
     /**
-     * 
+     *
      * @return boolean
      */
-    public function isUserCreditLimitReached() {        
+    public function isUserCreditLimitReached()
+    {
         $userAccountPlan = $this->userAccountPlanService->getForUser($this->user);
 
         if (!$userAccountPlan->getPlan()->hasConstraintNamed(self::CREDITS_PER_MONTH_CONSTRAINT_NAME)) {
             return false;
         }
-        
-        return $this->getCreditsUsedThisMonth() >= $userAccountPlan->getPlan()->getConstraintNamed(self::CREDITS_PER_MONTH_CONSTRAINT_NAME)->getLimit();               
+
+        return $this->getCreditsUsedThisMonth() >= $this->getCreditsPerMonthConstraint()->getLimit();
     }
-    
 
     /**
-     * 
      * @return AccountPlanConstraint
      */
-    public function getFullSiteJobLimitConstraint() {
-        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(self::FULL_SITE_JOBS_PER_SITE_CONSTRAINT_NAME);
-    }    
-    
-    
-    /**
-     * 
-     * @return AccountPlanConstraint
-     */
-    public function getSingleUrlJobLimitConstraint() {
-        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(self::SINGLE_URL_JOBS_PER_URL_CONSTRAINT_NAME);
+    public function getFullSiteJobLimitConstraint()
+    {
+        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(
+            self::FULL_SITE_JOBS_PER_SITE_CONSTRAINT_NAME
+        );
     }
-    
 
     /**
-     * 
      * @return AccountPlanConstraint
      */
-    public function getJobUrlLimitConstraint() {
-        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(self::URLS_PER_JOB_CONSTRAINT_NAME);
-    }      
-    
-    
-    /**
-     * 
-     * @return AccountPlanConstraint
-     */
-    public function getCreditsPerMonthConstraint() {
-        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(self::CREDITS_PER_MONTH_CONSTRAINT_NAME);
-    }    
-    
+    public function getSingleUrlJobLimitConstraint()
+    {
+        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(
+            self::SINGLE_URL_JOBS_PER_URL_CONSTRAINT_NAME
+        );
+    }
 
+    /**
+     * @return AccountPlanConstraint
+     */
+    public function getJobUrlLimitConstraint()
+    {
+        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(
+            self::URLS_PER_JOB_CONSTRAINT_NAME
+        );
+    }
+
+    /**
+     * @return AccountPlanConstraint
+     */
+    public function getCreditsPerMonthConstraint()
+    {
+        return $this->userAccountPlanService->getForUser($this->user)->getPlan()->getConstraintNamed(
+            self::CREDITS_PER_MONTH_CONSTRAINT_NAME
+        );
+    }
 }
