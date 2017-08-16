5c20ca76db4cff59ab937e8708f4909f11e9a816
Jon Cram <jon@webignition.net>
2017-08-16 13:48:28 +0100

    Remove old tests

--- src/SimplyTestable/ApiBundle/Tests/Functional/Services/JobUserAccountPlanEnforcement/ServiceTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Services\JobUserAccountPlanEnforcement;
-
-use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
-
-abstract class ServiceTest extends BaseSimplyTestableTestCase {
-}
