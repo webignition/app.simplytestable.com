93efdce4ad1fa6c5860c4ec5250db44568326b7a
Jon Cram <jon@webignition.net>
2017-08-18 14:33:18 +0100

    Remove call to TaskRepository::getIdsByJob() from ResolveWebsiteCommand

--- src/SimplyTestable/ApiBundle/Tests/Functional/Command/Job/ResolveWebsiteCommand/SingleUrlJobTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Command/Job/ResolveWebsiteCommand/SingleUrlJobTest.php
@@ -4,6 +4,7 @@ namespace SimplyTestable\ApiBundle\Tests\Functional\Command\Job\ResolveWebsiteCo
 
 use SimplyTestable\ApiBundle\Services\JobService;
 use SimplyTestable\ApiBundle\Services\JobTypeService;
+use SimplyTestable\ApiBundle\Services\TaskTypeService;
 use SimplyTestable\ApiBundle\Tests\Factory\HttpFixtureFactory;
 use SimplyTestable\ApiBundle\Tests\Factory\JobFactory;
 
@@ -19,11 +20,14 @@ class SingleUrlJobTest extends CommandTest
 
         $job = $jobFactory->create([
             JobFactory::KEY_TYPE => JobTypeService::SINGLE_URL_NAME,
-            JobFactory::KEY_TEST_TYPES => ['CSS Validation'],
+            JobFactory::KEY_TEST_TYPES => [
+                TaskTypeService::HTML_VALIDATION_TYPE,
+                TaskTypeService::CSS_VALIDATION_TYPE,
+            ],
             JobFactory::KEY_TEST_TYPE_OPTIONS => [
-                'CSS validation' => array(
+                TaskTypeService::CSS_VALIDATION_TYPE => [
                     'ignore-common-cdns' => 1,
-                )
+                ]
             ],
         ]);
 
@@ -33,17 +37,24 @@ class SingleUrlJobTest extends CommandTest
             'id' => $job->getId()
         ));
 
+        $taskIds = [];
         $tasks = $job->getTasks();
-        $task = $tasks->first();
+
+        foreach ($tasks as $task) {
+            $taskIds[] = $task->getId();
+        }
+
+        $htmlValidationTask = $tasks->get(0);
+        $cssValidationTask = $tasks->get(1);
 
         $this->assertEquals(0, $returnCode);
         $this->assertEquals(JobService::QUEUED_STATE, $job->getState()->getName());
-        $this->assertEquals(1, $tasks->count());
-        $this->assertEquals($this->getTaskService()->getQueuedState(), $task->getState());
-        $this->assertTrue(is_array($task->getParameter('domains-to-ignore')));
+        $this->assertEquals(2, $tasks->count());
+        $this->assertEquals($this->getTaskService()->getQueuedState(), $htmlValidationTask->getState());
+        $this->assertTrue(is_array($cssValidationTask->getParameter('domains-to-ignore')));
         $this->assertTrue($this->getResqueQueueService()->contains(
             'task-assign-collection',
-            ['ids' => implode(',', $this->getTaskService()->getEntityRepository()->getIdsByJob($job))]
+            ['ids' => implode(',', $taskIds)]
         ));
     }
 }
