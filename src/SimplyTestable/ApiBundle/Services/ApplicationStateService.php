b00f148b07a7dddad88e8b910a9b0bca6409823a
Jon Cram <jon@webignition.net>
2017-08-22 19:10:21 +0100

    Update ApplicationStateService

--- src/SimplyTestable/ApiBundle/Services/ApplicationStateService.php
+++ src/SimplyTestable/ApiBundle/Services/ApplicationStateService.php
@@ -1,116 +1,138 @@
 <?php
+
 namespace SimplyTestable\ApiBundle\Services;
 
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class ApplicationStateService {
-    
+class ApplicationStateService
+{
+    const RESOURCE_PATH = '@SimplyTestableApiBundle/Resources/config/state/%s';
+
     const STATE_ACTIVE = 'active';
     const STATE_MAINTENANCE_READ_ONLY = 'maintenance-read-only';
     const STATE_MAINTENANCE_BACKUP_READ_ONLY = 'maintenance-backup-read-only';
     const DEFAULT_STATE = self::STATE_ACTIVE;
-    
-    private $allowedStates = array(
+
+    /**
+     * @var string[]
+     */
+    private $allowedStates = [
         self::STATE_ACTIVE,
         self::STATE_MAINTENANCE_READ_ONLY,
         self::STATE_MAINTENANCE_BACKUP_READ_ONLY
-    );
-    
+    ];
+
     /**
-     *
      * @var string
      */
     private $stateResourcePath;
-    
-    
+
     /**
-     *
      * @var string
      */
     private $state;
-    
-    
-    /**
-     * 
-     * @return boolean
-     */
-    public function isInActiveState() {
-        return $this->getState() == self::STATE_ACTIVE;
-    }
-    
-    
-    /**
-     * 
-     * @return boolean
-     */
-    public function isInMaintenanceReadOnlyState() {
-        return $this->getState() == self::STATE_MAINTENANCE_READ_ONLY;
-    }
-    
 
     /**
-     * 
-     * @return boolean
+     * @var ResourceLocator
      */
-    public function isInMaintenanceBackupReadOnlyState() {
-        return $this->getState() == self::STATE_MAINTENANCE_BACKUP_READ_ONLY;
-    }    
-    
-    
+    private $resourceLocator;
+
     /**
-     * 
-     * @param string $state
-     * @return boolean
+     * @var KernelInterface
      */
-    public function setState($state) {
+    private $kernel;
+
+    /**
+     * @param ResourceLocator $resourceLocator
+     * @param KernelInterface $kernel
+     */
+    public function __construct(ResourceLocator $resourceLocator, KernelInterface $kernel)
+    {
+        $this->resourceLocator = $resourceLocator;
+        $this->kernel = $kernel;
+
+        $this->stateResourcePath = $resourceLocator->locate(sprintf(
+            self::RESOURCE_PATH,
+            $kernel->getEnvironment()
+        ));
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInActiveState()
+    {
+        return $this->getState() == self::STATE_ACTIVE;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInMaintenanceReadOnlyState()
+    {
+        return $this->getState() == self::STATE_MAINTENANCE_READ_ONLY;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isInMaintenanceBackupReadOnlyState()
+    {
+        return $this->getState() == self::STATE_MAINTENANCE_BACKUP_READ_ONLY;
+    }
+
+    /**
+     * @param string $state
+     *
+     * @return bool
+     */
+    public function setState($state)
+    {
         if (!$this->isAllowedState($state)) {
             return false;
         }
-        
-        if (file_put_contents($this->stateResourcePath, $state) > 0) {
+
+        if (file_put_contents($this->stateResourcePath, $state) !== false) {
             $this->state = $state;
+
             return true;
         }
-        
+
         return false;
     }
-    
-    
+
     /**
-     * 
      * @return string
      */
-    public function getState() {       
-        if (is_null($this->state)) { 
+    public function getState()
+    {
+        if (is_null($this->state)) {
             if (file_exists($this->stateResourcePath)) {
                 $this->state = trim(file_get_contents($this->stateResourcePath));
             }
-            
+
             if (!$this->isAllowedState($this->state)) {
                 $this->state = self::DEFAULT_STATE;
             }
         }
-        
-        return $this->state;        
+
+        return $this->state;
     }
-    
-    
+
     /**
-     * 
      * @param string $stateResourcePath
      */
-    public function setStateResourcePath($stateResourcePath) {
+    public function setStateResourcePath($stateResourcePath)
+    {
         $this->stateResourcePath = $stateResourcePath;
     }
-    
-    
+
     /**
-     * 
      * @param string $state
      * @return boolean
      */
-    private function isAllowedState($state) {
+    private function isAllowedState($state)
+    {
         return in_array($state, $this->allowedStates);
     }
-    
-    
 }
