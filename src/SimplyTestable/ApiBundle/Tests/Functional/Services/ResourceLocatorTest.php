51ff4f61d6faeacbf0962b58c8573758a620c802
Jon Cram <jon@webignition.net>
2017-08-22 17:25:45 +0100

    Implement ResourceLocator service

---
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Services/ResourceLocatorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Functional\Services;
+
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+
+class ResourceLocatorTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @dataProvider locateDataProvider
+     *
+     * @param string $name
+     * @param string $expectedRelativeResourcePath
+     */
+    public function testLocate($name, $expectedRelativeResourcePath)
+    {
+        $resourceLocator = $this->container->get('simplytestable.services.resourcelocator');
+
+        $expectedAbsoluteResourcePath = preg_replace(
+            '/app$/',
+            '',
+            $this->container->get('kernel')->getRootDir()
+        ) . $expectedRelativeResourcePath;
+
+        $this->assertEquals($expectedAbsoluteResourcePath, $resourceLocator->locate($name));
+    }
+
+    /**
+     * @return array
+     */
+    public function locateDataProvider()
+    {
+        return [
+            'application state' => [
+                'name' => '@SimplyTestableApiBundle/Resources/config/state/test',
+                'expectedRelativeResourcePath' =>
+                    'src/SimplyTestable/ApiBundle/Resources/config/state/test',
+            ],
+        ];
+    }
+}
