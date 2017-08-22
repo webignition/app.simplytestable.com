cb43f404dac64566436c881c8c2082a4e7962500
Jon Cram <webignition@gmail.com>
2017-08-22 15:14:15 +0100

    Define Cron/RunCommand as service (#53)

--- src/SimplyTestable/ApiBundle/Command/Cron/RunCommand.php
+++ src/SimplyTestable/ApiBundle/Command/Cron/RunCommand.php
@@ -5,8 +5,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Cron\CronBundle\Command\CronRunCommand;
 
-class RunCommand extends CronRunCommand {
-
+class RunCommand extends CronRunCommand
+{
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this->setName('simplytestable:cron:run')
@@ -14,5 +17,4 @@ class RunCommand extends CronRunCommand {
             ->addArgument('job', InputArgument::OPTIONAL, 'Run only this job (if enabled)')
             ->addOption('force', null, InputOption::VALUE_NONE, 'Force the current job.');
     }
-
 }
