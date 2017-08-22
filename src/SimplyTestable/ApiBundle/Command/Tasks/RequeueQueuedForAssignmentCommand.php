61ba30516636a84df064f7e682b9c68f69f02a9b
Jon Cram <jon@webignition.net>
2017-08-22 11:08:42 +0100

    Create Tasks/RequeueQueuedForAssignmentCommand

---
+++ src/SimplyTestable/ApiBundle/Command/Tasks/RequeueQueuedForAssignmentCommand.php
@@ -0,0 +1,70 @@
+<?php
+namespace SimplyTestable\ApiBundle\Command\Tasks;
+
+use SimplyTestable\ApiBundle\Command\BaseCommand;
+
+use SimplyTestable\ApiBundle\Entity\Task\Task;
+use SimplyTestable\ApiBundle\Services\TaskService;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RequeueQueuedForAssignmentCommand extends BaseCommand
+{
+    const RETURN_CODE_OK = 0;
+    const RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE = 1;
+
+    const NAME = 'simplytestable:tasks:requeue-queued-for-assignment';
+    const DESCRIPTION = 'Change the state of all "queued-for-assignment" tasks to "queued"';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription(self::DESCRIPTION)
+            ->setHelp(self::DESCRIPTION);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $isInMaintenanceReadOnlyState = $this->getApplicationStateService()->isInMaintenanceReadOnlyState();
+        $isInMaintenanceBackupReadOnlyState = $this->getApplicationStateService()->isInMaintenanceBackupReadOnlyState();
+
+        $isInReadOnlyState = $isInMaintenanceReadOnlyState || $isInMaintenanceBackupReadOnlyState;
+
+        if ($isInReadOnlyState) {
+            return self::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE;
+        }
+
+        $stateService = $this->getContainer()->get('simplytestable.services.stateservice');
+        $taskService = $this->getContainer()->get('simplytestable.services.taskservice');
+        $entityManager = $this->getContainer()->get('doctrine.orm.entity_manager');
+
+        $taskQueuedForAssignmentState = $stateService->fetch(TaskService::QUEUED_FOR_ASSIGNMENT_STATE);
+
+        /* @var Task[] $tasks */
+        $tasks = $taskService->getEntityRepository()->findBy([
+            'state' => $taskQueuedForAssignmentState,
+        ]);
+
+        if (empty($tasks)) {
+            return self::RETURN_CODE_OK;
+        }
+
+        $taskQueuedState = $stateService->fetch(TaskService::QUEUED_STATE);
+
+        foreach ($tasks as $task) {
+            $task->setState($taskQueuedState);
+            $entityManager->persist($task);
+        }
+
+        $entityManager->flush();
+
+        return self::RETURN_CODE_OK;
+    }
+}
---
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Command/Tasks/RequeueQueuedForAssignmentCommandTest.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Functional\Command\Tasks;
+
+use SimplyTestable\ApiBundle\Command\Tasks\RequeueQueuedForAssignmentCommand;
+use SimplyTestable\ApiBundle\Controller\MaintenanceController;
+use SimplyTestable\ApiBundle\Entity\Task\Task;
+use SimplyTestable\ApiBundle\Services\TaskService;
+use SimplyTestable\ApiBundle\Tests\Factory\JobFactory;
+use SimplyTestable\ApiBundle\Tests\Factory\UserFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class RequeueQueuedForAssignmentCommandTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @var RequeueQueuedForAssignmentCommand
+     */
+    private $command;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->command = new RequeueQueuedForAssignmentCommand();
+        $this->command->setContainer($this->container);
+    }
+
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        $maintenanceController = new MaintenanceController();
+        $maintenanceController->setContainer($this->container);
+        $maintenanceController->enableReadOnlyAction();
+
+        $returnCode = $this->command->run(new ArrayInput([]), new BufferedOutput());
+
+        $this->assertEquals(
+            RequeueQueuedForAssignmentCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $returnCode
+        );
+    }
+
+    public function testRunInMaintenanceBackupReadOnlyMode()
+    {
+        $maintenanceController = new MaintenanceController();
+        $maintenanceController->setContainer($this->container);
+        $maintenanceController->enableBackupReadOnlyAction();
+
+        $returnCode = $this->command->run(new ArrayInput([]), new BufferedOutput());
+
+        $this->assertEquals(
+            RequeueQueuedForAssignmentCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $returnCode
+        );
+    }
+
+    /**
+     * @dataProvider runSuccessDataProvider
+     *
+     * @param array $jobValuesCollection
+     * @param string $expectedTaskStateNames
+     */
+    public function testRunSuccess($jobValuesCollection, $expectedTaskStateNames)
+    {
+        $userFactory = new UserFactory($this->container);
+        $users = $userFactory->createPublicAndPrivateUserSet();
+
+        foreach ($jobValuesCollection as $jobValuesIndex => $jobValues) {
+            if (isset($jobValues[JobFactory::KEY_USER])) {
+                $jobValues[JobFactory::KEY_USER] = $users[$jobValues[JobFactory::KEY_USER]];
+                $jobValuesCollection[$jobValuesIndex] = $jobValues;
+            }
+        }
+
+        $jobFactory = new JobFactory($this->container);
+        $jobs = $jobFactory->createResolveAndPrepareCollection($jobValuesCollection);
+
+        /* @var Task[] $tasks */
+        $tasks = [];
+
+        foreach ($jobs as $job) {
+            $tasks = array_merge($tasks, $job->getTasks()->toArray());
+        }
+
+        $returnCode = $this->command->run(new ArrayInput([]), new BufferedOutput());
+
+        $this->assertEquals(
+            RequeueQueuedForAssignmentCommand::RETURN_CODE_OK,
+            $returnCode
+        );
+
+        foreach ($tasks as $taskIndex => $task) {
+            $expectedStateName = $expectedTaskStateNames[$taskIndex];
+
+            $this->assertEquals($expectedStateName, $task->getState()->getName());
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function runSuccessDataProvider()
+    {
+        return [
+            'no tasks' => [
+                'jobValuesCollection' => [],
+                'expectedTaskStateNames' => [],
+            ],
+            'all tasks already queued' => [
+                'jobValuesCollection' => [
+                    [],
+                ],
+                'expectedTaskStateNames' => [
+                    TaskService::QUEUED_STATE,
+                    TaskService::QUEUED_STATE,
+                    TaskService::QUEUED_STATE,
+                ],
+            ],
+            'mixed states' => [
+                'jobValuesCollection' => [
+                    [
+                        JobFactory::KEY_USER => 'public',
+                        JobFactory::KEY_TASK_STATES => [
+                            TaskService::COMPLETED_STATE,
+                            TaskService::QUEUED_STATE,
+                            TaskService::QUEUED_FOR_ASSIGNMENT_STATE,
+                        ],
+                    ],
+                    [
+                        JobFactory::KEY_USER => 'private',
+                        JobFactory::KEY_TASK_STATES => [
+                            TaskService::CANCELLED_STATE,
+                            TaskService::QUEUED_FOR_ASSIGNMENT_STATE,
+                            TaskService::TASK_FAILED_RETRY_AVAILABLE_STATE,
+                        ],
+                    ],
+                ],
+                'expectedTaskStateNames' => [
+                    TaskService::COMPLETED_STATE,
+                    TaskService::QUEUED_STATE,
+                    TaskService::QUEUED_STATE,
+                    TaskService::CANCELLED_STATE,
+                    TaskService::QUEUED_STATE,
+                    TaskService::TASK_FAILED_RETRY_AVAILABLE_STATE,
+                ],
+            ],
+        ];
+    }
+}
