86ab42b6ca4ed2591839d5bde9217835ab8fddda
Jon Cram <jon@webignition.net>
2017-08-23 14:59:40 +0100

    Tidy up Resque/Job/Job/ResolveJob

---
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Resque/Job/Job/ResolveJobTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Functional\Resque\Job\Job;
+
+use SimplyTestable\ApiBundle\Command\Job\PrepareCommand;
+use SimplyTestable\ApiBundle\Controller\MaintenanceController;
+use SimplyTestable\ApiBundle\Resque\Job\Job\ResolveJob;
+use SimplyTestable\ApiBundle\Tests\Factory\JobFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+
+class ResolveJobTest extends BaseSimplyTestableTestCase
+{
+    const QUEUE = 'job-resolve';
+
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        $maintenanceController = new MaintenanceController();
+        $maintenanceController->setContainer($this->container);
+
+        $maintenanceController->enableReadOnlyAction();
+
+        $jobFactory = new JobFactory($this->container);
+        $job = $jobFactory->create();
+
+        $jobPrepareJob = $this->createJobResolveJob($job->getId());
+
+        $returnCode = $jobPrepareJob->run([]);
+
+        $maintenanceController->disableReadOnlyAction();
+
+        $this->assertEquals(PrepareCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE, $returnCode);
+    }
+
+    /**
+     * @param int $jobId
+     *
+     * @return ResolveJob
+     */
+    private function createJobResolveJob($jobId)
+    {
+        $resqueJobFactory = $this->container->get('simplytestable.services.resque.jobfactory');
+
+        $jobPrepareJob = $resqueJobFactory->create(
+            self::QUEUE,
+            [
+                'id' =>  $jobId,
+            ]
+        );
+
+        $jobPrepareJob->setKernelOptions([
+            'kernel.root_dir' => $this->container->getParameter('kernel.root_dir'),
+            'kernel.environment' => $this->container->getParameter('kernel.environment'),
+        ]);
+
+        return $jobPrepareJob;
+    }
+}
