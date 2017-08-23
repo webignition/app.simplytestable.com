9570e35759f8324dd2ac902d84fcc068965fa9ca
Jon Cram <jon@webignition.net>
2017-08-23 15:54:18 +0100

    Tidy up Resque/Task/CancelJob

--- src/SimplyTestable/ApiBundle/Resque/Job/Task/CancelJob.php
+++ src/SimplyTestable/ApiBundle/Resque/Job/Task/CancelJob.php
@@ -5,25 +5,41 @@ namespace SimplyTestable\ApiBundle\Resque\Job\Task;
 use SimplyTestable\ApiBundle\Command\Task\Cancel\Command;
 use SimplyTestable\ApiBundle\Resque\Job\CommandJob;
 
-class CancelJob extends CommandJob {
-
+class CancelJob extends CommandJob
+{
     const QUEUE_NAME = 'task-cancel';
 
-    protected function getQueueName() {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getQueueName()
+    {
         return self::QUEUE_NAME;
     }
 
-    public function getCommand() {
+    /**
+     * {@inheritdoc}
+     */
+    public function getCommand()
+    {
         return new Command();
     }
 
-    protected function getCommandArgs() {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getCommandArgs()
+    {
         return [
             'id' => $this->args['id']
         ];
     }
 
-    protected function getIdentifier() {
+    /**
+     * {@inheritdoc}
+     */
+    protected function getIdentifier()
+    {
         return $this->args['id'];
     }
 }
