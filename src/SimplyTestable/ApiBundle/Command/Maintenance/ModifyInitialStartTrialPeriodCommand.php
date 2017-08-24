c870986f73b25b79e499c5a5bd985417b357b1f8
Jon Cram <jon@webignition.net>
2017-08-24 16:54:11 +0100

    Define Maintenance/ModifyInitialStartTrialPeriodCommand a service

--- src/SimplyTestable/ApiBundle/Command/Maintenance/ModifyInitialStartTrialPeriodCommand.php
+++ src/SimplyTestable/ApiBundle/Command/Maintenance/ModifyInitialStartTrialPeriodCommand.php
@@ -1,151 +1,175 @@
 <?php
 namespace SimplyTestable\ApiBundle\Command\Maintenance;
 
-use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\ORM\EntityManager;
+use SimplyTestable\ApiBundle\Services\AccountPlanService;
+use SimplyTestable\ApiBundle\Services\UserAccountPlanService;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use SimplyTestable\ApiBundle\Command\BaseCommand;
 use SimplyTestable\ApiBundle\Entity\UserAccountPlan;
 
-class ModifyInitialStartTrialPeriodCommand extends BaseCommand
+class ModifyInitialStartTrialPeriodCommand extends Command
 {
     const RETURN_CODE_OK = 0;
-    const RETURN_CODE_MISSING_REQUIRED_OPTION = 2;   
-    
+    const RETURN_CODE_MISSING_REQUIRED_OPTION = 2;
+
     /**
-     *
+     * @var AccountPlanService
+     */
+    private $accountPlanService;
+
+    /**
+     * @var UserAccountPlanService
+     */
+    private $userAccountPlanService;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param AccountPlanService $accountPlanService
+     * @param UserAccountPlanService $userAccountPlanService
+     * @param EntityManager $entityManager
+     * @param null $name
+     */
+    public function __construct(
+        AccountPlanService $accountPlanService,
+        UserAccountPlanService $userAccountPlanService,
+        EntityManager $entityManager,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->accountPlanService = $accountPlanService;
+        $this->userAccountPlanService = $userAccountPlanService;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
      * @var InputInterface
      */
     private $input;
-    
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('simplytestable:maintenance:modify-initial-start-trial-period')
             ->setDescription('Modify the intial start trial period for all users on the basic plan')
-            ->addOption('dry-run', null, InputOption::VALUE_OPTIONAL, 'Run through the process without writing any data')
-            ->addOption('current', null, InputOption::VALUE_REQUIRED, 'Current trial period to modify from')
-            ->addOption('new', null, InputOption::VALUE_REQUIRED, 'New trial period to modify to')
-            ->setHelp(<<<EOF
-Modify the intial start trial period for all users on the basic plan.
-EOF
-        );     
+            ->addOption(
+                'dry-run',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Run through the process without writing any data'
+            )
+            ->addOption(
+                'current',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Current trial period to modify from'
+            )
+            ->addOption(
+                'new',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'New trial period to modify to'
+            );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
-    {        
+    {
         $this->input = $input;
-        
-        if ($this->isDryRun()) {
+
+        $isDryRun = filter_var($input->getOption('dry-run'), FILTER_VALIDATE_BOOLEAN);
+
+        if ($isDryRun) {
             $output->writeln('<comment>This is a DRY RUN, no data will be written</comment>');
         }
-        
-        $current = $this->getCurrent();
-        
+
+        $current = $this->getNonZeroIntegerOption($input, 'current');
+
         if (is_null($current)) {
             $output->writeln('<info>Current trial period: NULL</info>');
             $output->writeln('<error>Current trial period not specified. Use --current=<int></error>');
+
             return self::RETURN_CODE_MISSING_REQUIRED_OPTION;
         } else {
             $output->writeln('<info>Current trial period: '.$current.'</info>');
         }
-        
-        $new = $this->getNew();        
-        
-        if (is_null($current)) {
+
+        $new = $this->getNonZeroIntegerOption($input, 'new');
+
+        if (is_null($new)) {
             $output->writeln('<info>New trial period: NULL</info>');
             $output->writeln('<error>New trial period not specified. Use --new=<int></error>');
+
             return self::RETURN_CODE_MISSING_REQUIRED_OPTION;
         } else {
             $output->writeln('<info>New trial period: '.$new.'</info>');
         }
-        
-        $basicPlan = $this->getAccountPlanService()->find('basic');
-        
-        $output->write('<info>Finding users on basic plan:</info>'); 
-        
-        $userAccountPlans = $this->getUserAccountPlanService()->findAllByPlan($basicPlan);
-        
+
+        $basicPlan = $this->accountPlanService->find('basic');
+
+        $output->write('<info>Finding users on basic plan:</info>');
+
+        $userAccountPlans = $this->userAccountPlanService->findAllByPlan($basicPlan);
+
         $output->writeln(' ' . count($userAccountPlans));
-        
+
         foreach ($userAccountPlans as $userAccountPlan) {
             /* @var $userAccountPlan UserAccountPlan */
-            if (is_null($userAccountPlan->getIsActive())) {
-                $userAccountPlan->setIsActive(true);
-            }
-            
-            if (is_null($userAccountPlan->getStartTrialPeriod()) || $userAccountPlan->getStartTrialPeriod() === $current) {
+            $currentUserStartTrialPeriod = $userAccountPlan->getStartTrialPeriod();
+
+            $hasNoStartTrialPeriod = empty($currentUserStartTrialPeriod);
+            $hasMatchingStartTrialPeriod = $currentUserStartTrialPeriod === $current;
+            $shouldUpdateStartTrialPeriod = $hasNoStartTrialPeriod || $hasMatchingStartTrialPeriod;
+
+            if ($shouldUpdateStartTrialPeriod) {
                 $output->write('Updating for ' . $userAccountPlan->getUser()->getUsername() . ' ... ');
-                $output->writeln('going from ' .(is_null($userAccountPlan->getStartTrialPeriod()) ? 'NULL' : $current). ' to ' . $new);
-                
-                $userAccountPlan->setStartTrialPeriod($new);
+                $output->writeln('going from ' . ($hasNoStartTrialPeriod ? 'NULL' : $current). ' to ' . $new);
             }
-            
-            if (!$this->isDryRun()) {
-                $this->getUserAccountPlanService()->getManager()->persist($userAccountPlan);
-                $this->getUserAccountPlanService()->getManager()->flush($userAccountPlan);
+
+            if (!$isDryRun && $shouldUpdateStartTrialPeriod) {
+                if (is_null($userAccountPlan->getIsActive())) {
+                    $userAccountPlan->setIsActive(true);
+                }
+
+                $userAccountPlan->setStartTrialPeriod($new);
+                $this->entityManager->persist($userAccountPlan);
+                $this->entityManager->flush($userAccountPlan);
             }
         }
-        
-        
+
         return self::RETURN_CODE_OK;
     }
 
-    private function getCurrent() {
-        return $this->getNonZeroIntegerOption('current');
-    }    
-    
-    
-    private function getNew() {
-        return $this->getNonZeroIntegerOption('new');
-    }
-    
-    
-    
     /**
-     * 
+     * @param InputInterface $input
      * @param string $name
+     *
      * @return int
      */
-    private function getNonZeroIntegerOption($name) {
-        $value = $this->input->getOption($name);
-        if (!ctype_digit($value)) {
+    private function getNonZeroIntegerOption(InputInterface $input, $name)
+    {
+        $value = $input->getOption($name);
+
+        if (!(ctype_digit($value) || is_int($value))) {
             return null;
         }
-        
+
         if ($value < 0) {
             return null;
         }
-        
-        return (int)$value;        
-    }
-    
-    
-    /**
-     * 
-     * @return boolean
-     */
-    private function isDryRun() {        
-        return $this->input->getOption('dry-run') == 'true';
-    }
-    
-    
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\AccountPlanService
-     */    
-    private function getAccountPlanService() {
-        return $this->getContainer()->get('simplytestable.services.accountplanservice');
-    }     
-    
-    
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\UserAccountPlanService
-     */    
-    private function getUserAccountPlanService() {
-        return $this->getContainer()->get('simplytestable.services.useraccountplanservice');
+
+        return (int)$value;
     }
 }
