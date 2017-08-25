26959349189f0865333e10288df67315c11a9dac
Jon Cram <webignition@gmail.com>
2017-08-25 12:42:44 +0100

    Define Reporting/GetOutputIdsForErrorCommand as a service (#69)

--- src/SimplyTestable/ApiBundle/Command/Reporting/GetOutputIdsForErrorCommand.php
+++ src/SimplyTestable/ApiBundle/Command/Reporting/GetOutputIdsForErrorCommand.php
@@ -1,268 +1,268 @@
 <?php
 namespace SimplyTestable\ApiBundle\Command\Reporting;
 
-use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\ORM\EntityManager;
+use SimplyTestable\ApiBundle\Entity\Task\Output;
+use SimplyTestable\ApiBundle\Services\TaskTypeService;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
-use SimplyTestable\ApiBundle\Command\BaseCommand;
 use SimplyTestable\ApiBundle\Entity\Task\Output as TaskOutput;
 use SimplyTestable\ApiBundle\Entity\Task\Type\Type as TaskType;
 
-use webignition\HtmlValidationErrorNormaliser\HtmlValidationErrorNormaliser;
-
-
-class GetOutputIdsForErrorCommand extends BaseCommand
+class GetOutputIdsForErrorCommand extends Command
 {
     const RETURN_CODE_OK = 0;
-    const RETURN_CODE_INVALID_TASK_TYPE = 2;    
-    const RETURN_CODE_INVALID_FRAGMENTS = 3;    
-    
+    const RETURN_CODE_MISSING_TASK_TYPE = 1;
+    const RETURN_CODE_INVALID_TASK_TYPE = 2;
+    const RETURN_CODE_MISSING_FRAGMENTS = 3;
+
     /**
-     *
-     * @var \Symfony\Component\Console\Input\InputInterface
+     * @var TaskTypeService
      */
-    private $input;
-    
-    
+    private $taskTypeService;
+
     /**
-     *
-     * @var TaskType
-     */
-    private $taskType;
-    
-    
-    /**
-     *
-     * @var \Doctrine\ORM\EntityManager
+     * @var EntityManager
      */
     private $entityManager;
-    
+
     /**
-     *
-     * @var \Doctrine\ORM\EntityRepository
+     * @param TaskTypeService $taskTypeService
+     * @param EntityManager $entityManager
+     * @param string|null $name
      */
-    private $taskOutputRepository;
-    
-    
-    private $messages = array();    
-    
+    public function __construct(
+        TaskTypeService $taskTypeService,
+        EntityManager $entityManager,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->taskTypeService = $taskTypeService;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('simplytestable:reporting:get-output-ids-for-error')
             ->setDescription('Get list of task output ids matching certain error messages')
-            ->addOption('task-type', null, InputOption::VALUE_REQUIRED, 'Name of task type for which to generate report')
-            ->addOption('task-output-limit', null, InputOption::VALUE_OPTIONAL, 'Limit the number of task outputs processed')
-            ->addOption('task-output-offset', null, InputOption::VALUE_OPTIONAL, 'Offset for task output list')
-            ->addOption('fragments', null, InputOption::VALUE_OPTIONAL, 'Fragments of errors to match against')
-            ->setHelp(<<<EOF
-Get list of task output ids matching certain error messages
-EOF
-        );     
+            ->addOption(
+                'task-type',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Name of task type for which to generate report'
+            )
+            ->addOption(
+                'task-output-limit',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Limit the number of task outputs processed'
+            )
+            ->addOption(
+                'task-output-offset',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Offset for task output list'
+            )
+            ->addOption(
+                'fragments',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Fragments of errors to match against'
+            )
+            ->addOption(
+                'output-only-ids',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Output only a comma-separated list of matching task output ids'
+            );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
-    {        
-        ini_set('xdebug.var_display_max_depth', '10'); 
-        
-        $this->input = $input;
-        
-        $output->write('<info>Requested task type: '.$input->getOption('task-type').' ... </info>');        
-        $this->taskType = $this->getTaskTypeService()->getByName($input->getOption('task-type'));
-        
-        if (is_null($this->taskType)) {
-            $output->writeln('invalid task type');
+    {
+        $outputOnlyIds = filter_var($input->getOption('output-only-ids'), FILTER_VALIDATE_BOOLEAN);
+
+        $taskTypeName = $input->getOption('task-type');
+        $taskType = null;
+
+        if (empty($taskTypeName)) {
+            if (!$outputOnlyIds) {
+                $output->writeln('<error>Required option "task-type" missing</error>');
+            }
+
+            return self::RETURN_CODE_MISSING_TASK_TYPE;
+        }
+
+        if (!$outputOnlyIds) {
+            $output->write('<info>Requested task type: "' . $taskTypeName . '" ... </info>');
+        }
+
+        $taskType = $this->taskTypeService->getByName($taskTypeName);
+
+        if (empty($taskType)) {
+            if (!$outputOnlyIds) {
+                $output->writeln('<error>invalid</error>');
+            }
+
             return self::RETURN_CODE_INVALID_TASK_TYPE;
         }
-        
-        $output->writeln('ok');
-        
-        $output->write('<info>Fragments to match: ... </info>'); 
-        $fragments = $this->getFragments();
-        
-        if (count($fragments) === 0) {
-            $output->write('none specified, stopping'); 
-            return self::RETURN_CODE_INVALID_FRAGMENTS;
-        }
-        
-        $output->writeln(implode(', ', $fragments));                
-        
-        $limit = $this->getTaskOutputLimit();
-        $output->write('<info>Requested limit: ');
 
-        if (is_null($limit)) {
-            $output->writeln('NONE');
-        } else {
-            $output->writeln($limit);
-        }        
-        
-        $offset = $this->getTaskOutputOffset();
-        $output->write('<info>Requested offset: ');
-        
-        if (is_null($offset)) {
-            $output->writeln('NONE');
-        } else {
-            $output->writeln($offset);
+        if (!$outputOnlyIds) {
+            $output->writeln('<comment>ok</comment>');
+            $output->write('<info>Fragments to match: ... </info>');
         }
-        
-        $output->writeln('');
-        
-        $output->write('Finding task output for ['.$this->taskType->getName().'] tasks ... ');
-        
-        $taskOutputIds = $this->getTaskOutputRepository()->findIdsByTaskType($this->taskType, $limit, $offset);
-        $taskOutputCount = count($taskOutputIds);
-        
-        $output->writeln('['.$taskOutputCount.'] task outputs found');
-        
+
+        $fragments = $this->getFragments($input);
+
+        if (empty($fragments)) {
+            if (!$outputOnlyIds) {
+                $output->writeln('<comment>none specified, stopping</comment>');
+            }
+
+            return self::RETURN_CODE_MISSING_FRAGMENTS;
+        }
+
+        $limit = (int)$input->getOption('task-output-limit');
+        $limit = $limit < 0 ? null : $limit;
+
+        $offset = (int)$input->getOption('task-output-offset');
+        $offset = $offset < 0 ? null : $offset;
+
+        if (!$outputOnlyIds) {
+            $output->writeln('<comment>' . implode(', ', $fragments) . '</comment>');
+            $output->write('<info>Requested limit: ');
+            $output->writeln('<comment>' . (empty($limit) ? 'NONE' : $limit) . '</comment>');
+            $output->write('<info>Requested offset: ');
+            $output->writeln('<comment>' . (empty($offset) ? 'NONE' : $offset) . '</comment>');
+            $output->writeln('');
+            $output->write('Finding task output for [' . $taskTypeName . '] tasks ... ');
+        }
+
+        $taskOutputRepository = $this->entityManager->getRepository(Output::class);
+        $taskOutputIds = $taskOutputRepository->findIdsByTaskType($taskType, $limit, $offset);
+
+        if (!$outputOnlyIds) {
+            $output->writeln('[' . count($taskOutputIds) . '] task outputs found');
+        }
+
         $processedTaskOutputCount = 0;
-        
-        $normaliser = new HtmlValidationErrorNormaliser();
-        
-        $messageCount = 0;
-        $outputIds = array();
-        
-        foreach ($taskOutputIds as $taskOutputId) {            
+        $outputIds = [];
+
+        foreach ($taskOutputIds as $taskOutputId) {
             $processedTaskOutputCount++;
-            $output->write('.');
-            
-            $taskOutput = $this->getTaskOutputRepository()->find($taskOutputId);
-            
-            $messages = $this->getMessagesForTaskOutput($taskOutput);
-            
+
+            if (!$outputOnlyIds) {
+                $output->write('.');
+            }
+
+            $taskOutput = $taskOutputRepository->find($taskOutputId);
+            $messages = $this->getMessagesForTaskOutput($taskOutput, $taskType);
+
             foreach ($messages as $message) {
                 if ($this->isFragmentMatch($message, $fragments)) {
-                    $outputIds[] = $taskOutputId;
+                    if (!in_array($taskOutputId, $outputIds)) {
+                        $outputIds[] = $taskOutputId;
+                    }
                 }
             }
-            
-            $this->getManager()->detach($taskOutput);
+
+            $this->entityManager->detach($taskOutput);
         }
-        
-        $output->writeln('');
-        $output->writeln('<info>============================================</info>');
-        $output->writeln('');
-        $output->writeln('Outputs found: ' . count($outputIds));
-        $output->writeln('');
-        $output->writeln(implode(',',$outputIds));
-        $output->writeln('');  
-        
+
+        if ($outputOnlyIds) {
+            $output->writeln(implode(',', $outputIds));
+        } else {
+            $output->writeln('');
+            $output->writeln('<info>============================================</info>');
+            $output->writeln('');
+            $output->writeln('Outputs found: ' . count($outputIds));
+            $output->writeln('');
+            $output->writeln(implode(',', $outputIds));
+            $output->writeln('');
+        }
+
         return self::RETURN_CODE_OK;
     }
-    
-    
+
     /**
-     * 
      * @param string $message
      * @param array $fragments
-     * @return boolean
+     *
+     * @return bool
      */
-    private function isFragmentMatch($message, $fragments) {        
-        $isMatch = true;
+    private function isFragmentMatch($message, $fragments)
+    {
         $message = strtolower($message);
-        
-        foreach ($fragments as $fragment) {            
-            $fragment = strtolower(trim($fragment));
+
+        foreach ($fragments as $fragment) {
             if (!substr_count($message, $fragment)) {
-                $isMatch = false;
+                return false;
             }
         }
-        
-        return $isMatch;
+
+        return true;
     }
-  
-    
+
     /**
-     * 
-     * @return int
+     * @param InputInterface $input
+     *
+     * @return string[]
      */
-    private function getTaskOutputLimit() {
-        $limit = (int)$this->input->getOption('task-output-limit');        
-        return ($limit > 0) ? $limit : null;
-    }
-    
-    
-    /**
-     * 
-     * @return array
-     */
-    private function getFragments() {
-        $fragmentsString = $this->input->getOption('fragments');        
-        if (is_null($fragmentsString)) {
-            return array();
+    private function getFragments(InputInterface $input)
+    {
+        $fragmentsString = $input->getOption('fragments');
+
+        if (empty($fragmentsString)) {
+            return [];
         }
-        
-        return explode(',', $fragmentsString);
+
+        $fragments = explode(',', $fragmentsString);
+
+        array_walk($fragments, function (&$value) {
+            $value = trim(strtolower($value));
+        });
+
+        return $fragments;
     }
-        
-    
+
     /**
-     * 
-     * @return int
+     * @param TaskOutput $taskOutput
+     * @param TaskType $taskType
+     *
+     * @return string[]
      */
-    private function getTaskOutputOffset() {
-        $offset = (int)$this->input->getOption('task-output-offset');        
-        return ($offset > 0) ? $offset : null;
-    }
-    
-    
-    private function getMessagesForTaskOutput(TaskOutput $taskOutput) {
-        $messages = array();
-        
+    private function getMessagesForTaskOutput(TaskOutput $taskOutput, TaskType $taskType)
+    {
+        $messages = [];
+
         if ($taskOutput->getErrorCount() === 0) {
             return $messages;
         }
-        
-        switch ($this->taskType->getName()) {
-            case 'HTML validation':
-                $decodedOutput = json_decode($taskOutput->getOutput());
-                
-                if ($decodedOutput instanceof \stdClass) {
-                    foreach ($decodedOutput->messages as $message) {
-                        if ($message->type == 'error') {
-                            $messages[] = $message->message;   
-                        }                                   
-                    }                    
+
+        switch ($taskType->getName()) {
+            case TaskTypeService::HTML_VALIDATION_TYPE:
+                $decodedOutput = json_decode($taskOutput->getOutput(), true);
+
+                if (is_array($decodedOutput)) {
+                    foreach ($decodedOutput['messages'] as $message) {
+                        if ($message['type'] === 'error') {
+                            $messages[] = $message['message'];
+                        }
+                    }
                 }
-                
+
                 break;
         }
-        
-        return $messages;      
+
+        return $messages;
     }
-    
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\TaskTypeService
-     */    
-    private function getTaskTypeService() {
-        return $this->getContainer()->get('simplytestable.services.tasktypeservice');
-    }
-    
-    
-    /**
-     * 
-     * @return \Doctrine\ORM\EntityManager
-     */
-    private function getManager() {
-        if (is_null($this->entityManager)) {
-            $this->entityManager = $this->getContainer()->get('doctrine')->getManager();
-        }
-        
-        return  $this->entityManager;
-    }
-    
-    
-    /**
-     * 
-     * @return \SimplyTestable\ApiBundle\Repository\TaskOutputRepository
-     */
-    private function getTaskOutputRepository() {
-        if (is_null($this->taskOutputRepository)) {
-            $this->taskOutputRepository = $this->getManager()->getRepository('SimplyTestable\ApiBundle\Entity\Task\Output');
-        }
-        
-        return $this->taskOutputRepository;
-    }  
 }
