6ed945d36e46f75cf5b63440e8a68b09f5631316
Jon Cram <webignition@gmail.com>
2017-08-26 06:48:48 +0100

    Define ScheduledJob/EnqueueCommand as a service (#71)

--- src/SimplyTestable/ApiBundle/Command/ScheduledJob/EnqueueCommand.php
+++ src/SimplyTestable/ApiBundle/Command/ScheduledJob/EnqueueCommand.php
@@ -1,48 +1,95 @@
 <?php
 namespace SimplyTestable\ApiBundle\Command\ScheduledJob;
 
-use SimplyTestable\ApiBundle\Command\BaseCommand;
+use Psr\Log\LoggerInterface;
+use SimplyTestable\ApiBundle\Services\Resque\JobFactory as ResqueJobFactory;
+use SimplyTestable\ApiBundle\Services\Resque\QueueService as ResqueQueueService;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class EnqueueCommand extends BaseCommand {
+class EnqueueCommand extends Command
+{
+    /**
+     * @var ResqueQueueService
+     */
+    private $resqueQueueService;
 
+    /**
+     * @var ResqueJobFactory
+     */
+    private $resqueJobFactory;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param ResqueQueueService $resqueQueueService
+     * @param ResqueJobFactory $resqueJobFactory
+     * @param LoggerInterface $logger
+     * @param string|null $name
+     */
+    public function __construct(
+        ResqueQueueService $resqueQueueService,
+        ResqueJobFactory $resqueJobFactory,
+        LoggerInterface $logger,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->resqueQueueService = $resqueQueueService;
+        $this->resqueJobFactory = $resqueJobFactory;
+        $this->logger = $logger;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
             ->setName('simplytestable:scheduledjob:enqueue')
             ->setDescription('Start a new job from a scheduled job')
-            ->addArgument('id', InputArgument::REQUIRED, 'id of scheduled job to execute')
-        ;
+            ->addArgument(
+                'id',
+                InputArgument::REQUIRED,
+                'id of scheduled job to execute'
+            );
     }
 
+    /**
+     * {@inheritdoc}
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $resqueQueueService = $this->getContainer()->get('simplytestable.services.resque.queueservice');
-        $resqueJobFactory = $this->getContainer()->get('simplytestable.services.resque.jobfactory');
+        $scheduleJobId = (int)$input->getArgument('id');
 
-        $startNotice = 'simplytestable:scheduledjob:enqueue [' . $input->getArgument('id') . '] start';
+        $startNotice = 'simplytestable:scheduledjob:enqueue [' . $scheduleJobId . '] start';
 
         $output->write($startNotice);
-        $this->getLogger()->notice($startNotice);
+        $this->logger->notice($startNotice);
 
-        if ($resqueQueueService->contains('scheduledjob-execute', ['id' => (int)$input->getArgument('id')])) {
-            $this->getLogger()->notice('simplytestable:scheduledjob:enqueue [' . $input->getArgument('id') . '] already in execute queue');
+        if ($this->resqueQueueService->contains('scheduledjob-execute', ['id' => $scheduleJobId])) {
+            $this->logger->notice(
+                'simplytestable:scheduledjob:enqueue [' . $scheduleJobId. '] already in execute queue'
+            );
         } else {
-            $resqueQueueService->enqueue(
-                $resqueJobFactory->create(
+            $this->resqueQueueService->enqueue(
+                $this->resqueJobFactory->create(
                     'scheduledjob-execute',
-                    ['id' => (int)$input->getArgument('id')]
+                    ['id' => $scheduleJobId]
                 )
             );
 
-            $this->getLogger()->notice('simplytestable:scheduledjob:enqueue [' . $input->getArgument('id') . '] enqueuing');
+            $this->logger->notice('simplytestable:scheduledjob:enqueue [' . $scheduleJobId . '] enqueuing');
         }
 
-        $endNotice = 'simplytestable:scheduledjob:enqueue [' . $input->getArgument('id') . '] done';
+        $endNotice = 'simplytestable:scheduledjob:enqueue [' . $scheduleJobId . '] done';
 
         $output->writeln($endNotice);
-        $this->getLogger()->notice($endNotice);
+        $this->logger->notice($endNotice);
     }
 }
