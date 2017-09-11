d643174dd49ae4252cbcbf4875a027cc059c7c48
Jon Cram <jon@webignition.net>
2017-09-11 18:00:06 +0100

    Test CustomerSubscriptionDeletedListener

--- src/SimplyTestable/ApiBundle/EventListener/Stripe/CustomerSubscriptionDeletedListener.php
+++ src/SimplyTestable/ApiBundle/EventListener/Stripe/CustomerSubscriptionDeletedListener.php
@@ -2,96 +2,115 @@
 
 namespace SimplyTestable\ApiBundle\EventListener\Stripe;
 
+use SimplyTestable\ApiBundle\Event\Stripe\DispatchableEvent;
+use webignition\Model\Stripe\Event\CustomerSubscriptionUpdated;
+use webignition\Model\Stripe\Event\Event;
+use webignition\Model\Stripe\Invoice\Invoice;
+use webignition\Model\Stripe\Subscription;
+
 class CustomerSubscriptionDeletedListener extends CustomerSubscriptionListener
 {
+    /**
+     * @param DispatchableEvent $event
+     */
+    public function onCustomerSubscriptionDeleted(DispatchableEvent $event)
+    {
+        $this->setEvent($event);
 
-    public function onCustomerSubscriptionDeleted(\SimplyTestable\ApiBundle\Event\Stripe\DispatchableEvent $event) {
-        $this->setEvent($event);       
-        
         $stripeSubscription = $this->getStripeSubscription();
-        
+
         if ($stripeSubscription->wasCancelledDuringTrial()) {
-            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(           
+            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(
                 'plan_name' => $stripeSubscription->getPlan()->getName(),
                 'actioned_by' => 'user',
                 'is_during_trial' => 1,
                 'trial_days_remaining' => $this->getUserAccountPlanFromEvent()->getStartTrialPeriod()
             )));
-            
+
             $this->markEntityProcessed();
+
             return;
         }
-        
+
         if ($this->hasInvoicePaymentFailedEventForSubscription($stripeSubscription)) {
             // System has cancelled following payment failure
-            $this->downgradeToBasicPlan();            
-            
-            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(           
+            $this->downgradeToBasicPlan();
+
+            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(
                 'plan_name' => $stripeSubscription->getPlan()->getName(),
                 'actioned_by' => 'system'
-            )));        
-            $this->markEntityProcessed();             
-            return;            
+            )));
+            $this->markEntityProcessed();
+
+            return;
         }
-        
+
         if (!$this->hasTrialToActiveStatusChangeEvent()) {
-            // User has canceled after trial            
-            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(           
+            // User has canceled after trial
+            $this->issueWebClientEvent(array_merge($this->getDefaultWebClientData(), array(
                 'plan_name' => $stripeSubscription->getPlan()->getName(),
                 'actioned_by' => 'user',
                 'is_during_trial' => 0
-            )));        
-            $this->markEntityProcessed();              
-            return;            
+            )));
+            $this->markEntityProcessed();
+
+            return;
         }
-      
+
         $this->markEntityProcessed();
     }
-    
-    
-    private function hasInvoicePaymentFailedEventForSubscription(\webignition\Model\Stripe\Subscription $subscription) {
+
+    /**
+     * @param Subscription $subscription
+     *
+     * @return bool
+     */
+    private function hasInvoicePaymentFailedEventForSubscription(Subscription $subscription)
+    {
         $paymentFailedEvents = $this->getStripeEventService()->getForUserAndType(
             $this->getEventEntity()->getUser(),
             'invoice.payment_failed'
         );
 
         foreach ($paymentFailedEvents as $paymentFailedEvent) {
-            if ($paymentFailedEvent->getStripeEventObject()->getDataObject()->getObject()->isForSubscription($subscription)) {
+            /* @var Invoice $invoice */
+            $invoice = $paymentFailedEvent->getStripeEventObject()->getDataObject()->getObject();
+
+            if ($invoice->isForSubscription($subscription)) {
                 return true;
             }
         }
-        
+
         return false;
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return bool
      */
-    private function hasTrialToActiveStatusChangeEvent() {
+    private function hasTrialToActiveStatusChangeEvent()
+    {
         return !is_null($this->getMostRecentTrialToActiveStatusChangeEvent());
     }
-    
-    
-    
+
     /**
-     * 
-     * @return \webignition\Model\Stripe\Event\Event|
+     * @return Event
      */
-    private function getMostRecentTrialToActiveStatusChangeEvent() {
+    private function getMostRecentTrialToActiveStatusChangeEvent()
+    {
         $customerSubscriptionUpdatedEvents = $this->getStripeEventService()->getForUserAndType(
             $this->getEventEntity()->getUser(),
             'customer.subscription.updated'
         );
-        
-        foreach ($customerSubscriptionUpdatedEvents as $customerSubscriptionUpdatedEvent) {            
-            if ($customerSubscriptionUpdatedEvent->getStripeEventObject()->hasStatusChange('trialing:active')) {
+
+        foreach ($customerSubscriptionUpdatedEvents as $customerSubscriptionUpdatedEvent) {
+            /* @var CustomerSubscriptionUpdated $stripeCustomerSubscriptionUpdatedEvent */
+            $stripeCustomerSubscriptionUpdatedEvent = $customerSubscriptionUpdatedEvent->getStripeEventObject();
+
+            if ($stripeCustomerSubscriptionUpdatedEvent->hasStatusChange('trialing:active')) {
                 return $customerSubscriptionUpdatedEvent->getStripeEventObject();
             }
-        }        
-        
+        }
+
         return null;
     }
-   
 }
---
+++ src/SimplyTestable/ApiBundle/Tests/Functional/EventListener/Stripe/CustomerSubscriptionDeletedListenerTest.php
@@ -0,0 +1,258 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Functional\EventListener\Stripe;
+
+use Guzzle\Http\Message\EntityEnclosingRequestInterface;
+use SimplyTestable\ApiBundle\Event\Stripe\DispatchableEvent;
+use SimplyTestable\ApiBundle\Tests\Factory\HttpFixtureFactory;
+use SimplyTestable\ApiBundle\Tests\Factory\StripeEventFixtureFactory;
+use SimplyTestable\ApiBundle\Tests\Factory\UserFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+
+class CustomerSubscriptionDeletedListenerTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @dataProvider onCustomerSubscriptionDeletedDataProvider
+     *
+     * @param array $stripeEventFixtures
+     * @param string $userName
+     * @param array $stripeServiceResponses
+     * @param array $expectedWebClientRequestData
+     * @param string $expectedPlanName
+     */
+    public function testOnCustomerSubscriptionDeleted(
+        $stripeEventFixtures,
+        $userName,
+        $stripeServiceResponses,
+        $expectedWebClientRequestData,
+        $expectedPlanName
+    ) {
+        $eventDispatcher = $this->container->get('event_dispatcher');
+        $stripeEventService = $this->container->get('simplytestable.services.stripeeventservice');
+        $httpClientService = $this->container->get('simplytestable.services.httpclientservice');
+        $stripeService = $this->container->get('simplytestable.services.stripeservice');
+        $userAccountPlanService = $this->container->get('simplytestable.services.useraccountplanservice');
+
+        foreach ($stripeServiceResponses as $methodName => $responseData) {
+            $stripeService->addResponseData($methodName, $responseData);
+        }
+
+        $this->queueHttpFixtures([
+            HttpFixtureFactory::createSuccessResponse(),
+        ]);
+
+        $userFactory = new UserFactory($this->container);
+        $users = $userFactory->createPublicAndPrivateUserSet();
+        $user = $users[$userName];
+
+        $stripeEvents = [];
+
+        foreach ($stripeEventFixtures as $fixtureName => $modifiers) {
+            $stripeEventFixture = StripeEventFixtureFactory::load($fixtureName, $modifiers);
+
+            $stripeEvents[] = $stripeEventService->create(
+                $stripeEventFixture['id'],
+                $stripeEventFixture['type'],
+                $stripeEventFixture['livemode'],
+                json_encode($stripeEventFixture),
+                $user
+            );
+        }
+
+        $stripeEvent = array_pop($stripeEvents);
+
+        $eventDispatcher->dispatch(
+            'stripe_process.' . $stripeEvent->getType(),
+            new DispatchableEvent($stripeEvent)
+        );
+
+        $this->assertTrue($stripeEvent->getIsProcessed());
+
+        /* @var EntityEnclosingRequestInterface $lastHttpRequest */
+        $lastHttpRequest = $httpClientService->getHistoryPlugin()->getLastRequest();
+
+        if (null === $expectedWebClientRequestData) {
+            $this->assertNull($lastHttpRequest);
+        } else {
+            $this->assertEquals(
+                $expectedWebClientRequestData,
+                $lastHttpRequest->getPostFields()->getAll()
+            );
+        }
+
+        $userAccountPlan = $userAccountPlanService->getForUser($user);
+
+        $this->assertEquals(
+            $expectedPlanName,
+            $userAccountPlan->getPlan()->getName()
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function onCustomerSubscriptionDeletedDataProvider()
+    {
+        return [
+            'customer.subscription.deleted; cancelled during trial' => [
+                'stripeEventFixtures' => [
+                    'customer.subscription.deleted' => [
+                        'data' => [
+                            'object' => [
+                                'plan' => [
+                                    'name' => 'Foo Plan Name',
+                                    'amount' => 10000,
+                                    'currency' => 'eur'
+                                ],
+                                'trial_start' => 1,
+                                'trial_end' => 4,
+                                'canceled_at' => 3,
+                            ],
+                        ],
+                    ]
+                ],
+                'user' => 'public',
+                'stripeServiceResponses' => [],
+                'expectedWebClientRequestData' => [
+                    'event' =>  'customer.subscription.deleted',
+                    'user' => 'public@simplytestable.com',
+                    'plan_name' => 'Foo Plan Name',
+                    'actioned_by' => 'user',
+                    'is_during_trial' => 1,
+                    'trial_days_remaining' => 30,
+                ],
+                'expectedPlanName' => 'public',
+            ],
+            'customer.subscription.deleted; cancelled during trial with non-relevant invoice payment failed' => [
+                'stripeEventFixtures' => [
+                    'invoice.payment_failed' => [
+                        'data' => [
+                            'object' => [
+                                'lines' => [
+                                    'data' => [
+                                        [
+                                            'id' => 'non-relevant subscription id',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                    'customer.subscription.deleted' => [
+                        'data' => [
+                            'object' => [
+                                'plan' => [
+                                    'name' => 'Foo Plan Name',
+                                    'amount' => 10000,
+                                    'currency' => 'eur'
+                                ],
+                                'trial_start' => 1,
+                                'trial_end' => 4,
+                                'canceled_at' => 3,
+                            ],
+                        ],
+                    ],
+                ],
+                'user' => 'public',
+                'stripeServiceResponses' => [],
+                'expectedWebClientRequestData' => [
+                    'event' =>  'customer.subscription.deleted',
+                    'user' => 'public@simplytestable.com',
+                    'plan_name' => 'Foo Plan Name',
+                    'actioned_by' => 'user',
+                    'is_during_trial' => 1,
+                    'trial_days_remaining' => 30,
+                ],
+                'expectedPlanName' => 'public',
+            ],
+            'customer.subscription.deleted; invoice payment failed' => [
+                'stripeEventFixtures' => [
+                    'invoice.payment_failed' => [],
+                    'customer.subscription.deleted' => [
+                        'data' => [
+                            'object' => [
+                                'plan' => [
+                                    'name' => 'Foo Plan Name',
+                                    'amount' => 10000,
+                                    'currency' => 'eur'
+                                ],
+                                'trial_start' => 1,
+                                'trial_end' => 4,
+                                'canceled_at' => 5,
+                            ],
+                        ],
+                    ],
+                ],
+                'user' => 'public',
+                'stripeServiceResponses' => [],
+                'expectedWebClientRequestData' => [
+                    'event' =>  'customer.subscription.deleted',
+                    'user' => 'public@simplytestable.com',
+                    'plan_name' => 'Foo Plan Name',
+                    'actioned_by' => 'system',
+                ],
+                'expectedPlanName' => 'basic',
+            ],
+            'customer.subscription.deleted; user cancelled after trial' => [
+                'stripeEventFixtures' => [
+                    'customer.subscription.deleted' => [
+                        'data' => [
+                            'object' => [
+                                'plan' => [
+                                    'name' => 'Foo Plan Name',
+                                    'amount' => 10000,
+                                    'currency' => 'eur'
+                                ],
+                                'trial_start' => 1,
+                                'trial_end' => 4,
+                                'canceled_at' => 5,
+                            ],
+                        ],
+                    ],
+                ],
+                'user' => 'public',
+                'stripeServiceResponses' => [],
+                'expectedWebClientRequestData' => [
+                    'event' =>  'customer.subscription.deleted',
+                    'user' => 'public@simplytestable.com',
+                    'plan_name' => 'Foo Plan Name',
+                    'actioned_by' => 'user',
+                    'is_during_trial' => 0,
+                ],
+                'expectedPlanName' => 'public',
+            ],
+            'customer.subscription.deleted; trial to active' => [
+                'stripeEventFixtures' => [
+                    'customer.subscription.updated.statuschange' => [
+                        'data' => [
+                            'object' => [
+                                'status' => 'active',
+                            ],
+                            'previous_attributes' => [
+                                'status' => 'trialing',
+                            ],
+                        ],
+                    ],
+                    'customer.subscription.deleted' => [
+                        'data' => [
+                            'object' => [
+                                'plan' => [
+                                    'name' => 'Foo Plan Name',
+                                    'amount' => 10000,
+                                    'currency' => 'eur'
+                                ],
+                                'trial_start' => 1,
+                                'trial_end' => 4,
+                                'canceled_at' => 5,
+                            ],
+                        ],
+                    ],
+                ],
+                'user' => 'public',
+                'stripeServiceResponses' => [],
+                'expectedWebClientRequestData' => null,
+                'expectedPlanName' => 'public',
+            ],
+        ];
+    }
+}
