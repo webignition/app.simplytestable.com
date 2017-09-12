9228fcacbe685364f0f24ff870159fca2deb6311
Jon Cram <jon@webignition.net>
2017-09-12 11:33:43 +0100

    PSR2 CustomerSubscriptionCreatedListener

--- src/SimplyTestable/ApiBundle/EventListener/Stripe/CustomerSubscriptionCreatedListener.php
+++ src/SimplyTestable/ApiBundle/EventListener/Stripe/CustomerSubscriptionCreatedListener.php
@@ -2,38 +2,43 @@
 
 namespace SimplyTestable\ApiBundle\EventListener\Stripe;
 
+use SimplyTestable\ApiBundle\Event\Stripe\DispatchableEvent;
+
 class CustomerSubscriptionCreatedListener extends CustomerSubscriptionListener
 {
-   
-    public function onCustomerSubscriptionCreated(\SimplyTestable\ApiBundle\Event\Stripe\DispatchableEvent $event) {
+    /**
+     * @param DispatchableEvent $event
+     */
+    public function onCustomerSubscriptionCreated(DispatchableEvent $event)
+    {
         $this->setEvent($event);
-        
+
         $stripeSubscription = $this->getStripeSubscription();
-        
+
         $webClientData = array_merge($this->getDefaultWebClientData(), array(
-            'status' => $stripeSubscription->getStatus(),            
+            'status' => $stripeSubscription->getStatus(),
             'plan_name' => $stripeSubscription->getPlan()->getName()
         ));
-        
+
         if ($stripeSubscription->isTrialing()) {
             $webClientData = array_merge($webClientData, array(
                 'has_card' => (int)$this->getStripeCustomer()->hasCard(),
                 'trial_start' => $stripeSubscription->getTrialPeriod()->getStart(),
                 'trial_end' => $stripeSubscription->getTrialPeriod()->getEnd(),
                 'trial_period_days' => $stripeSubscription->getPlan()->getTrialPeriodDays()
-            ));        
+            ));
         }
-        
-        if ($stripeSubscription->isActive()) {            
+
+        if ($stripeSubscription->isActive()) {
             $webClientData = array_merge($webClientData, array(
                 'current_period_start' => $stripeSubscription->getCurrentPeriod()->getStart(),
                 'current_period_end' => $stripeSubscription->getCurrentPeriod()->getEnd(),
                 'amount' => $stripeSubscription->getPlan()->getAmount(),
                 'currency' => $stripeSubscription->getPlan()->getCurrency()
-            ));             
+            ));
         }
-        
-        $this->issueWebClientEvent($webClientData);        
+
+        $this->issueWebClientEvent($webClientData);
         $this->markEntityProcessed();
     }
 }
