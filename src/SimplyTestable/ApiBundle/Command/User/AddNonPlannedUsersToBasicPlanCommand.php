27652dad84fe09e640702dc81d60706a441f22cf
Jon Cram <jon@webignition.net>
2017-09-26 17:44:26 +0100

    Define User/AddNonPlannedUsersToBasicPlanCommand as a service

--- src/SimplyTestable/ApiBundle/Command/User/AddNonPlannedUsersToBasicPlanCommand.php
+++ src/SimplyTestable/ApiBundle/Command/User/AddNonPlannedUsersToBasicPlanCommand.php
@@ -1,17 +1,56 @@
 <?php
 namespace SimplyTestable\ApiBundle\Command\User;
 
-use Symfony\Component\Console\Input\InputArgument;
+use Doctrine\ORM\EntityManager;
+use SimplyTestable\ApiBundle\Entity\Account\Plan\Plan;
+use SimplyTestable\ApiBundle\Services\ApplicationStateService;
+use SimplyTestable\ApiBundle\Services\UserAccountPlanService;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-
 use SimplyTestable\ApiBundle\Command\BaseCommand;
 
 class AddNonPlannedUsersToBasicPlanCommand extends BaseCommand
 {
+    const RETURN_CODE_OK = 0;
     const RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE = 1;
 
+    /**
+     * @var ApplicationStateService
+     */
+    private $applicationStateService;
+
+    /**
+     * @var UserAccountPlanService
+     */
+    private $userAccountPlanService;
+
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @param ApplicationStateService $applicationStateService
+     * @param UserAccountPlanService $userAccountPlanService
+     * @param EntityManager $entityManager
+     * @param string|null $name
+     */
+    public function __construct(
+        ApplicationStateService $applicationStateService,
+        UserAccountPlanService $userAccountPlanService,
+        EntityManager $entityManager,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->applicationStateService = $applicationStateService;
+        $this->userAccountPlanService = $userAccountPlanService;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
     protected function configure()
     {
         $this
@@ -21,66 +60,50 @@ class AddNonPlannedUsersToBasicPlanCommand extends BaseCommand
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
-        $applicationStateService = $this->getContainer()->get('simplytestable.services.applicationstateservice');
-
-        if ($applicationStateService->isInMaintenanceReadOnlyState()) {
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->applicationStateService->isInMaintenanceReadOnlyState()) {
             $output->writeln('In maintenance-read-only mode, I can\'t do that right now');
+
             return self::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE;
         }
 
-        if ($this->isDryRun($input)) {
+        $isDryRun = $input->getOption('dry-run');
+
+        if ($isDryRun) {
             $output->writeln('<comment>This is a DRY RUN, no data will be written</comment>');
         }
 
         $output->writeln('Finding users that have no plan ...');
 
-        $users = $this->getUserAccountPlanService()->findUsersWithNoPlan();
+        $users = $this->userAccountPlanService->findUsersWithNoPlan();
 
-        if (count($users) === 0) {
+        if (empty($users)) {
             $output->writeln('No users found that have no plan. Done.');
-            return true;
+
+            return self::RETURN_CODE_OK;
         }
 
         $output->writeln('['.count($users).'] users found with no plan');
 
-        $plan = $this->getAccountPlanService()->find('basic');
+        $planRepository = $this->entityManager->getRepository(Plan::class);
+        $basicPlan = $planRepository->findOneBy([
+            'name' => 'basic',
+        ]);
 
         foreach ($users as $user) {
             $output->writeln('Setting basic plan for ' . $user->getUsername());
 
-            if (!$this->isDryRun($input)) {
-                $this->getUserAccountPlanService()->subscribe($user, $plan);
+            if (!$isDryRun) {
+                $this->userAccountPlanService->subscribe($user, $basicPlan);
             }
         }
 
         $output->writeln('');
-    }
 
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\UserAccountPlanService
-     */
-    private function getUserAccountPlanService() {
-        return $this->getContainer()->get('simplytestable.services.useraccountplanservice');
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\AccountPlanService
-     */
-    private function getAccountPlanService() {
-        return $this->getContainer()->get('simplytestable.services.accountplanservice');
-    }
-
-
-    /**
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @return int
-     */
-    private function isDryRun(InputInterface $input) {
-        return $input->getOption('dry-run');
+        return self::RETURN_CODE_OK;
     }
 }
--- src/SimplyTestable/ApiBundle/Tests/Functional/Command/User/AddNonPlannedUsersToBasicPlanCommandTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Command/User/AddNonPlannedUsersToBasicPlanCommandTest.php
@@ -2,137 +2,149 @@
 
 namespace SimplyTestable\ApiBundle\Tests\Functional\Command\User;
 
+use SimplyTestable\ApiBundle\Command\User\AddNonPlannedUsersToBasicPlanCommand;
 use SimplyTestable\ApiBundle\Controller\MaintenanceController;
+use SimplyTestable\ApiBundle\Entity\User;
+use SimplyTestable\ApiBundle\Services\ApplicationStateService;
 use SimplyTestable\ApiBundle\Tests\Factory\UserFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
 use SimplyTestable\ApiBundle\Tests\Functional\ConsoleCommandTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
-class AddNonPlannedUsersToBasicPlanCommandTest extends ConsoleCommandTestCase
+class AddNonPlannedUsersToBasicPlanCommandTest extends BaseSimplyTestableTestCase
 {
     /**
-     * @var UserFactory
+     * @var AddNonPlannedUsersToBasicPlanCommand
      */
-    private $userFactory;
+    private $command;
+
+//    /**
+//     * @var UserFactory
+//     */
+//    private $userFactory;
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->userFactory = new UserFactory($this->container);
+        $this->command = $this->container->get('simplytestable.command.user.addnonplanneduserstobascicplan');
+
+//        $this->userFactory = new UserFactory($this->container);
     }
 
-    /**
-     * @return string
-     */
-    protected function getCommandName()
+    public function testRunInMaintenanceReadOnlyModeReturnsStatusCode1()
     {
-        return 'simplytestable:user:add-non-planned-users-to-basic-plan';
-    }
+        $applicationStateService = $this->container->get('simplytestable.services.applicationstateservice');
+        $applicationStateService->setState(ApplicationStateService::STATE_MAINTENANCE_READ_ONLY);
 
-    /**
-     * @return \Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand[]
-     */
-    protected function getAdditionalCommands()
-    {
-        return array(
-            new \SimplyTestable\ApiBundle\Command\User\AddNonPlannedUsersToBasicPlanCommand()
-        );
-    }
-
-    public function testAssignInMaintenanceReadOnlyModeReturnsStatusCode1()
-    {
-        $maintenanceController = new MaintenanceController();
-        $maintenanceController->setContainer($this->container);
-        $maintenanceController->enableReadOnlyAction();
-
-        $this->assertReturnCode(1);
-
-        $maintenanceController->disableReadOnlyAction();
-    }
-
-    public function testPublicUserIsNotAssignedBasicPlan()
-    {
-        $this->assertReturnCode(0);
-
-        $userAccountPlan = $this->getUserAccountPlanService()->getForUser($this->getUserService()->getPublicUser());
-        $this->assertEquals('public', $userAccountPlan->getPlan()->getName());
-    }
-
-    public function testAdminUserIsNotAssignedBasicPlan()
-    {
-        $this->assertReturnCode(0);
-        $this->assertNull($this->getUserAccountPlanService()->getForUser($this->getUserService()->getAdminUser()));
-    }
-
-    public function testRegularUsersWithoutPlansAreAssignedTheBasicPlanWhenNoUsersHavePlans()
-    {
-        $userEmailAddresses = array(
-            'user1@example.com',
-            'user2@example.com',
-            'user3@example.com'
-        );
-
-        $users = array();
-
-        foreach ($userEmailAddresses as $userEmailAddress) {
-            $user = $this->userFactory->create([
-                UserFactory::KEY_EMAIL => $userEmailAddress,
-                UserFactory::KEY_PLAN_NAME => null,
-            ]);
-
-            $users[] = $user;
-        }
-
-        foreach ($users as $user) {
-            $this->assertNull($this->getUserAccountPlanService()->getForUser($user));
-        }
-
-        $this->assertReturnCode(0);
-
-        foreach ($users as $user) {
-            $this->assertEquals('basic', $this->getUserAccountPlanService()->getForUser($user)->getPlan()->getName());
-        }
-    }
-
-    public function testRegularUsersWithoutPlansAreAssignedTheBasicPlanWhenSomeUsersHavePlans()
-    {
-        $fooPlan = $this->createAccountPlan('test-foo-plan');
-
-        $user1 = $this->userFactory->create([
-            UserFactory::KEY_EMAIL => 'user1@example.com',
-            UserFactory::KEY_PLAN_NAME => 'test-foo-plan',
-        ]);
-
-        $this->getUserAccountPlanService()->subscribe($user1, $fooPlan);
-
-        $userEmailAddresses = array(
-            'user2@example.com',
-            'user3@example.com'
-        );
-
-        $users = array();
-
-        foreach ($userEmailAddresses as $userEmailAddress) {
-            $user = $this->userFactory->create([
-                UserFactory::KEY_EMAIL => $userEmailAddress,
-                UserFactory::KEY_PLAN_NAME => null,
-            ]);
-
-            $users[] = $user;
-        }
-
-        foreach ($users as $user) {
-            $this->assertNull($this->getUserAccountPlanService()->getForUser($user));
-        }
-
-        $this->assertReturnCode(0);
-
-        foreach ($users as $user) {
-            $this->assertEquals('basic', $this->getUserAccountPlanService()->getForUser($user)->getPlan()->getName());
-        }
+        $returnCode = $this->command->run(new ArrayInput([]), new BufferedOutput());
 
         $this->assertEquals(
-            'test-foo-plan',
-            $this->getUserAccountPlanService()->getForUser($user1)->getPlan()->getName()
+            AddNonPlannedUsersToBasicPlanCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $returnCode
         );
+
+        $applicationStateService->setState(ApplicationStateService::STATE_ACTIVE);
+    }
+
+    /**
+     * @dataProvider runDataProvider
+     *
+     * @param array $userValuesCollection
+     * @param array $args
+     * @param array $expectedUserPlanNames
+     */
+    public function testRun($userValuesCollection, $args, $expectedUserPlanNames)
+    {
+        $userFactory = new UserFactory($this->container);
+        $userAccountPlanService = $this->container->get('simplytestable.services.useraccountplanservice');
+
+        /* @var User[] $users */
+        $users = [];
+
+        foreach ($userValuesCollection as $userValues) {
+            $users[] = $userFactory->create($userValues);
+        }
+
+        $returnCode = $this->command->run(new ArrayInput($args), new BufferedOutput());
+
+        $this->assertEquals(
+            AddNonPlannedUsersToBasicPlanCommand::RETURN_CODE_OK,
+            $returnCode
+        );
+
+        foreach ($users as $userIndex => $user) {
+            $userAccountPlan = $userAccountPlanService->getForUser($user);
+
+            $expectedUserPlanName = $expectedUserPlanNames[$userIndex];
+
+            if (empty($expectedUserPlanName)) {
+                $this->assertNull($userAccountPlan);
+            } else {
+                $this->assertEquals(
+                    $expectedUserPlanName,
+                    $userAccountPlan->getPlan()->getName()
+                );
+            }
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function runDataProvider()
+    {
+        return [
+            'no users' => [
+                'userValuesCollection' => [],
+                'args' => [],
+                'expectedUserPlanNames' => [],
+            ],
+            'selection of users' => [
+                'userValuesCollection' => [
+                    [
+                        UserFactory::KEY_EMAIL => 'public@simplytestable.com',
+                    ],
+                    [
+                        UserFactory::KEY_EMAIL => 'no-plan@simplytestable.com',
+                        UserFactory::KEY_PLAN_NAME => null,
+                    ],
+                    [
+                        UserFactory::KEY_EMAIL => 'agency@simplytestable.com',
+                        UserFactory::KEY_PLAN_NAME => 'agency',
+                    ],
+                ],
+                'args' => [],
+                'expectedUserPlanNames' => [
+                    'public',
+                    'basic',
+                    'agency',
+                ],
+            ],
+            'selection of users; dry-run' => [
+                'userValuesCollection' => [
+                    [
+                        UserFactory::KEY_EMAIL => 'public@simplytestable.com',
+                    ],
+                    [
+                        UserFactory::KEY_EMAIL => 'no-plan@simplytestable.com',
+                        UserFactory::KEY_PLAN_NAME => null,
+                    ],
+                    [
+                        UserFactory::KEY_EMAIL => 'agency@simplytestable.com',
+                        UserFactory::KEY_PLAN_NAME => 'agency',
+                    ],
+                ],
+                'args' => [
+                    '--dry-run' => true,
+                ],
+                'expectedUserPlanNames' => [
+                    'public',
+                    null,
+                    'agency',
+                ],
+            ],
+        ];
     }
 }
