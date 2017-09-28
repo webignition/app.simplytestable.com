a99f28e7a02d82d4762a82684cc9a8edfe305a8c
Jon Cram <jon@webignition.net>
2017-09-28 11:42:29 +0100

    Finish tidying up MaintenanceController

--- src/SimplyTestable/ApiBundle/Controller/MaintenanceController.php
+++ src/SimplyTestable/ApiBundle/Controller/MaintenanceController.php
@@ -2,18 +2,9 @@
 
 namespace SimplyTestable\ApiBundle\Controller;
 
-use SimplyTestable\ApiBundle\Command\Job\EnqueuePrepareAllCommand;
-use SimplyTestable\ApiBundle\Command\Maintenance\DisableReadOnlyCommand;
-use SimplyTestable\ApiBundle\Command\Maintenance\EnableBackupReadOnlyCommand;
-use SimplyTestable\ApiBundle\Command\Maintenance\EnableReadOnlyCommand;
-use SimplyTestable\ApiBundle\Command\Task\EnqueueCancellationForAwaitingCancellationCommand;
-use SimplyTestable\ApiBundle\Command\Tasks\RequeueQueuedForAssignmentCommand;
-use SimplyTestable\ApiBundle\Command\Worker\TaskNotificationCommand;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\BufferedOutput;
-use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -58,7 +49,7 @@ class MaintenanceController extends ApiController
             $this->container->get('simplytestable.command.maintenance.disablereadonly'),
             $this->container->get('simplytestable.command.job.enqueueprepareall'),
             $this->container->get('simplytestable.command.tasks.requeuequeuedforassignment'),
-//            TaskNotificationCommand::class,
+            $this->container->get('simplytestable.command.worker.tasknotification'),
             $this->container->get('simplytestable.command.task.enqueuecancellationforawaitingcancellationcommand'),
         ];
 
