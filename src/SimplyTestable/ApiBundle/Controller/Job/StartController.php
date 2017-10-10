991e6ead2d23b54c168620c13e558ef0f6f0af69
Jon Cram <webignition@gmail.com>
2017-10-10 10:17:19 +0100

    Re-implement Job/StartController tests (#114)

--- src/SimplyTestable/ApiBundle/Controller/Job/StartController.php
+++ src/SimplyTestable/ApiBundle/Controller/Job/StartController.php
@@ -6,15 +6,27 @@ use SimplyTestable\ApiBundle\Entity\Account\Plan\Constraint as AccountPlanConstr
 use SimplyTestable\ApiBundle\Adapter\Job\Configuration\Start\RequestAdapter;
 use SimplyTestable\ApiBundle\Exception\Services\Job\Start\Exception as JobStartServiceException;
 use SimplyTestable\ApiBundle\Controller\ApiController;
+use SimplyTestable\ApiBundle\Services\JobService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use SimplyTestable\ApiBundle\Exception\Services\Job\UserAccountPlan\Enforcement\Exception as UserAccountPlanEnforcementException;
+use SimplyTestable\ApiBundle\Exception\Services\Job\UserAccountPlan\Enforcement\Exception
+    as UserAccountPlanEnforcementException;
 use SimplyTestable\ApiBundle\Entity\Job\Configuration as JobConfiguration;
+use Symfony\Component\HttpFoundation\Response;
 
-class StartController extends ApiController {
-
-    public function startAction(Request $request, $site_root_url) {
+class StartController extends ApiController
+{
+    /**
+     * @param Request $request
+     * @param string $site_root_url
+     *
+     * @return RedirectResponse|Response
+     * @throws JobStartServiceException
+     */
+    public function startAction(Request $request, $site_root_url)
+    {
         $applicationStateService = $this->container->get('simplytestable.services.applicationstateservice');
+        $jobStartService = $this->container->get('simplytestable.services.job.startservice');
 
         if ($applicationStateService->isInMaintenanceReadOnlyState()) {
             return $this->sendServiceUnavailableResponse();
@@ -22,10 +34,6 @@ class StartController extends ApiController {
 
         if ($applicationStateService->isInMaintenanceBackupReadOnlyState()) {
             return $this->sendServiceUnavailableResponse();
-        }
-
-        if (!$request->attributes->has('site_root_url')) {
-            $request->attributes->set('site_root_url', $site_root_url);
         }
 
         $requestAdapter = new RequestAdapter(
@@ -39,18 +47,14 @@ class StartController extends ApiController {
         $jobConfiguration->setUser($this->getUser());
 
         try {
-            $job = $this->getJobStartService()->start($jobConfiguration);
+            $job = $jobStartService->start($jobConfiguration);
 
             return $this->redirect($this->generateUrl('job_job_status', array(
                 'site_root_url' => $job->getWebsite()->getCanonicalUrl(),
                 'test_id' => $job->getId()
             )));
         } catch (JobStartServiceException $jobStartServiceException) {
-            if ($jobStartServiceException->isUnroutableWebsiteException()) {
-                return $this->rejectAsUnroutableAndRedirect($jobConfiguration);
-            }
-
-            throw $jobStartServiceException;
+            return $this->rejectAsUnroutableAndRedirect($jobConfiguration);
         } catch (UserAccountPlanEnforcementException $userAccountPlanEnforcementException) {
             return $this->rejectAsPlanLimitReachedAndRedirect(
                 $jobConfiguration,
@@ -59,8 +63,15 @@ class StartController extends ApiController {
         }
     }
 
-
-    public function retestAction(Request $request, $site_root_url, $test_id) {
+    /**
+     * @param Request $request
+     * @param string $site_root_url
+     * @param int $test_id
+     *
+     * @return RedirectResponse|Response
+     */
+    public function retestAction(Request $request, $site_root_url, $test_id)
+    {
         $job = $this->getJobService()->getById($test_id);
         if (is_null($job)) {
             return $this->sendFailureResponse();
@@ -77,28 +88,39 @@ class StartController extends ApiController {
 
         $taskTypeOptionsArray = array();
         foreach ($job->getTaskTypeOptions() as $taskTypeOptions) {
-            $taskTypeOptionsArray[strtolower($taskTypeOptions->getTaskType()->getName())] = $taskTypeOptions->getOptions();
+            $taskTypeNameKey = strtolower($taskTypeOptions->getTaskType()->getName());
+
+            $taskTypeOptionsArray[$taskTypeNameKey] = $taskTypeOptions->getOptions();
         }
 
-        if ($request->request->count()) {
-            $request->request->set('type', $job->getType()->getName());
-            $request->request->set('test-types', $taskTypeNames);
-            $request->request->set('test-type-options', $taskTypeOptionsArray);
-        } else {
-            $request->query->set('type', $job->getType()->getName());
-            $request->query->set('test-types', $taskTypeNames);
-            $request->query->set('test-type-options', $taskTypeOptionsArray);
-        }
+        $request->request->set('type', $job->getType()->getName());
+        $request->request->set('test-types', $taskTypeNames);
+        $request->request->set('test-type-options', $taskTypeOptionsArray);
+        $request->attributes->set('site_root_url', $site_root_url);
 
         return $this->startAction($request, $job->getWebsite()->getCanonicalUrl());
     }
 
-    private function rejectAsUnroutableAndRedirect(JobConfiguration $jobConfiguration) {
+    /**
+     * @param JobConfiguration $jobConfiguration
+     *
+     * @return RedirectResponse
+     */
+    private function rejectAsUnroutableAndRedirect(JobConfiguration $jobConfiguration)
+    {
         return $this->rejectAndRedirect($jobConfiguration, 'unroutable');
     }
 
-
-    private function rejectAsPlanLimitReachedAndRedirect(JobConfiguration $jobConfiguration, AccountPlanConstraint $constraint) {
+    /**
+     * @param JobConfiguration $jobConfiguration
+     * @param AccountPlanConstraint $constraint
+     *
+     * @return RedirectResponse
+     */
+    private function rejectAsPlanLimitReachedAndRedirect(
+        JobConfiguration $jobConfiguration,
+        AccountPlanConstraint $constraint
+    ) {
         return $this->rejectAndRedirect($jobConfiguration, 'plan-constraint-limit-reached', $constraint);
     }
 
@@ -129,47 +151,10 @@ class StartController extends ApiController {
     }
 
     /**
-     *
-     * @return boolean
+     * @return JobService
      */
-    private function isTestEnvironment() {
-        return $this->get('kernel')->getEnvironment() == 'test';
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Entity\User
-     */
-    public function getUser() {
-        if (!$this->isTestEnvironment()) {
-            return parent::getUser();
-        }
-
-        if  (is_null($this->getRequestValue('user'))) {
-            return $this->getUserService()->getPublicUser();
-        }
-
-        return $this->getUserService()->findUserByEmail($this->getRequestValue('user'));
-    }
-
-
-    /**
-     *
-     * @return \SimplyTestable\ApiBundle\Services\JobService
-     */
-    private function getJobService() {
+    private function getJobService()
+    {
         return $this->get('simplytestable.services.jobservice');
     }
-
-    /**
-     * @return \SimplyTestable\ApiBundle\Services\Job\StartService
-     */
-    private function getJobStartService() {
-        return $this->get('simplytestable.services.job.startservice');
-    }
-
-
-
-
 }
--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/Job/Start/StartAction/FullSite/ActionTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Start\StartAction\FullSite;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\Job\Start\ActionTest as BaseActionTest;
-
-abstract class ActionTest extends BaseActionTest {
-}
