9b029ffb2658e6155df5c6e4b8198088fddb4773
Jon Cram <jon@webignition.net>
2017-10-11 19:15:43 +0100

    Remove cruft

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/ScheduledJob/Update/ActionTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Controller/ScheduledJob/Update/ActionTest.php
@@ -4,22 +4,11 @@ namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\ScheduledJob\Upda
 
 use SimplyTestable\ApiBundle\Tests\Functional\Controller\ScheduledJob\ActionTest as BaseActionTest;
 
-abstract class ActionTest extends BaseActionTest {
-
-    protected function setUp() {
+abstract class ActionTest extends BaseActionTest
+{
+    protected function setUp()
+    {
         parent::setUp();
         $this->getRouter()->getContext()->setMethod('POST');
     }
-
-
-    /**
-     * @param array $postData
-     * @param array $queryData
-     * @return \SimplyTestable\ApiBundle\Controller\ScheduledJob\UpdateController
-     */
-    protected function getCurrentController(array $postData = [], array $queryData = []) {
-        return parent::getCurrentController($postData, $queryData);
-    }
-
-
 }
