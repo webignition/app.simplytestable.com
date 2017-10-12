0300573eecfd3b6da9c987abb4c5fe1781fcce1b
Jon Cram <jon@webignition.net>
2017-10-12 13:10:34 +0100

    Re-implement JobConfiguration/GetController tests

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/JobConfiguration/Get/GetAction/GetTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\JobConfiguration\Get\GetAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\JobConfiguration\ActionTest;
-
-abstract class GetTest extends ActionTest {
-}
