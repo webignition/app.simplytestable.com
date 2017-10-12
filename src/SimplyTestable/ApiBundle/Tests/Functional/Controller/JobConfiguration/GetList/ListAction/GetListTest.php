f563685fced3317c976026335c0064b9c1a2b621
Jon Cram <webignition@gmail.com>
2017-10-12 13:32:59 +0100

    Re-implement JobConfiguration/GetListController tests (#142)

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/JobConfiguration/GetList/ListAction/GetListTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\JobConfiguration\GetList\ListAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\JobConfiguration\ActionTest;
-
-abstract class GetListTest extends ActionTest {
-}
