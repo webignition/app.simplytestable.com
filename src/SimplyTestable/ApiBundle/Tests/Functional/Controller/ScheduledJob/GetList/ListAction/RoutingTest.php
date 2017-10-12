06c0de94e96ebec51151784cc0d1a43104535c88
Jon Cram <webignition@gmail.com>
2017-10-12 16:57:53 +0100

    Re-implement ScheduledJob/GetListController tests (#146)

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/ScheduledJob/GetList/ListAction/RoutingTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\ScheduledJob\GetList\ListAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\RoutingTest as BaseRoutingTest;
-
-class RoutingTest extends BaseRoutingTest {
-}
