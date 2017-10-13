d70f169d083fe7ca6aa40fe9d72dcd4f3306f452
Jon Cram <webignition@gmail.com>
2017-10-13 15:55:07 +0100

    Re-implement TeamInviteController tests (#148)

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/TeamInvite/ActivateAndAcceptAction/RoutingTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\TeamInvite\ActivateAndAcceptAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\RoutingTest as BaseRoutingTest;
-
-class RoutingTest extends BaseRoutingTest {}
--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/TeamInvite/DeclineAction/RoutingTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\TeamInvite\DeclineAction;
-
-use SimplyTestable\ApiBundle\Tests\Functional\Controller\RoutingTest as BaseRoutingTest;
-
-class RoutingTest extends BaseRoutingTest {}
