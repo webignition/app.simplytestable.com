760e459a46ec5250ee23ac11ca0255fed02f2b42
Jon Cram <webignition@gmail.com>
2017-10-20 15:39:01 +0100

    Test UserStripeEventController (#161)

---
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Controller/UserStripeEventControllerTest.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace SimplyTestable\ApiBundle\Tests\Functional\Controller;
+
+use SimplyTestable\ApiBundle\Controller\UserStripeEventController;
+use SimplyTestable\ApiBundle\Tests\Factory\UserFactory;
+use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
+
+class UserStripeEventControllerTest extends BaseSimplyTestableTestCase
+{
+    /**
+     * @var UserStripeEventController
+     */
+    private $userStripeEventController;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->userStripeEventController = new UserStripeEventController();
+        $this->userStripeEventController->setContainer($this->container);
+    }
+
+    public function testListActionGetRequest()
+    {
+        $userFactory = new UserFactory($this->container);
+        $user = $userFactory->create();
+
+        $router = $this->container->get('router');
+        $requestUrl = $router->generate('user_list_stripe_events', [
+            'email_canonical' => $user->getEmail(),
+            'type' => 'customer.subscription.created',
+        ]);
+
+        $this->getCrawler([
+            'url' => $requestUrl,
+            'method' => 'POST',
+            'user' => $user
+        ]);
+
+        $response = $this->getClientResponse();
+
+        $this->assertTrue($response->isSuccessful());
+    }
+
+    /**
+     * @dataProvider listActionClientFailureDataProvider
+     *
+     * @param string $userName
+     * @param string $emailCanonical
+     */
+    public function testListActionClientFailure($userName, $emailCanonical)
+    {
+        $userFactory = new UserFactory($this->container);
+        $users = $userFactory->createPublicAndPrivateUserSet();
+
+        $user = $users[$userName];
+        $this->setUser($user);
+
+        $response = $this->userStripeEventController->listAction($emailCanonical);
+
+        $this->assertTrue($response->isClientError());
+    }
+
+    /**
+     * @return array
+     */
+    public function listActionClientFailureDataProvider()
+    {
+        return [
+            'public user' => [
+                'userName' => 'public',
+                'emailCanonical' => 'foo@example.com',
+            ],
+            'incorrect user' => [
+                'userName' => 'private',
+                'emailCanonical' => 'foo@example.com',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider listActionDataProvider
+     *
+     * @param array $stripeEventFixtures
+     * @param string $userName
+     * @param string $type
+     * @param array $expectedResponseData
+     */
+    public function testListActionSuccess($stripeEventFixtures, $userName, $type, $expectedResponseData)
+    {
+        $stripeEventService = $this->container->get('simplytestable.services.stripeeventservice');
+
+        $userFactory = new UserFactory($this->container);
+        $users = $userFactory->createPublicPrivateAndTeamUserSet();
+        $user = $users[$userName];
+
+        $this->setUser($user);
+
+        foreach ($stripeEventFixtures as $stripeEventFixtureData) {
+            $eventUser = $users[$stripeEventFixtureData['userName']];
+
+            $stripeEventService->create(
+                $stripeEventFixtureData['stripeId'],
+                $stripeEventFixtureData['type'],
+                false,
+                'data',
+                $eventUser
+            );
+        }
+
+        $response = $this->userStripeEventController->listAction($user->getEmailCanonical(), $type);
+
+        $this->assertTrue($response->isSuccessful());
+
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertCount(count($expectedResponseData), $responseData);
+
+        $this->assertEquals($expectedResponseData, $responseData);
+    }
+
+    /**
+     * @return array
+     */
+    public function listActionDataProvider()
+    {
+        return [
+            'private user, no events' => [
+                'stripeEventFixtures' => [],
+                'userName' => 'private',
+                'type' => null,
+                'expectedResponseData' => [],
+            ],
+            'private user, no type' => [
+                'stripeEventFixtures' => [
+                    [
+                        'stripeId' => 'foo1',
+                        'type' => 'customer.subscription.created',
+                        'data' => 'data',
+                        'userName' => 'public',
+                    ],
+                    [
+                        'stripeId' => 'foo2',
+                        'type' => 'customer.subscription.updated',
+                        'data' => 'data',
+                        'userName' => 'public',
+                    ],
+                    [
+                        'stripeId' => 'foo3',
+                        'type' => 'customer.subscription.deleted',
+                        'data' => 'data',
+                        'userName' => 'public',
+                    ],
+                    [
+                        'stripeId' => 'foo4',
+                        'type' => 'customer.subscription.created',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                    [
+                        'stripeId' => 'foo5',
+                        'type' => 'customer.subscription.updated',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                    [
+                        'stripeId' => 'foo6',
+                        'type' => 'customer.subscription.deleted',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                ],
+                'userName' => 'private',
+                'type' => null,
+                'expectedResponseData' => [
+                    [
+                        'stripe_id' => 'foo6',
+                        'type' => 'customer.subscription.deleted',
+                        'is_live' => false,
+                        'stripe_event_data' => 'data',
+                        'user' => 'private@example.com',
+                        'is_processed' => false,
+                    ],
+                    [
+                        'stripe_id' => 'foo5',
+                        'type' => 'customer.subscription.updated',
+                        'is_live' => false,
+                        'stripe_event_data' => 'data',
+                        'user' => 'private@example.com',
+                        'is_processed' => false,
+                    ],
+                    [
+                        'stripe_id' => 'foo4',
+                        'type' => 'customer.subscription.created',
+                        'is_live' => false,
+                        'stripe_event_data' => 'data',
+                        'user' => 'private@example.com',
+                        'is_processed' => false,
+                    ],
+                ],
+            ],
+            'private user, with type' => [
+                'stripeEventFixtures' => [
+                    [
+                        'stripeId' => 'foo1',
+                        'type' => 'customer.subscription.created',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                    [
+                        'stripeId' => 'foo2',
+                        'type' => 'customer.subscription.updated',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                    [
+                        'stripeId' => 'foo3',
+                        'type' => 'customer.subscription.deleted',
+                        'data' => 'data',
+                        'userName' => 'private',
+                    ],
+                ],
+                'userName' => 'private',
+                'type' => 'customer.subscription.updated',
+                'expectedResponseData' => [
+                    [
+                        'stripe_id' => 'foo2',
+                        'type' => 'customer.subscription.updated',
+                        'is_live' => false,
+                        'stripe_event_data' => 'data',
+                        'user' => 'private@example.com',
+                        'is_processed' => false,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
