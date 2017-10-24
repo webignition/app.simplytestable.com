dce2a0ff70bd0e04413c190a9df26506f02158cd
Jon Cram <jon@webignition.net>
2017-10-24 14:07:33 +0100

    Update GetController test to include task config

--- src/SimplyTestable/ApiBundle/Tests/Functional/Controller/JobConfiguration/JobConfigurationGetControllerTest.php
+++ src/SimplyTestable/ApiBundle/Tests/Functional/Controller/JobConfiguration/JobConfigurationGetControllerTest.php
@@ -4,7 +4,10 @@ namespace SimplyTestable\ApiBundle\Tests\Functional\Controller\JobConfiguration;
 
 use SimplyTestable\ApiBundle\Controller\JobConfiguration\GetController;
 use SimplyTestable\ApiBundle\Entity\User;
+use SimplyTestable\ApiBundle\Services\JobTypeService;
+use SimplyTestable\ApiBundle\Services\TaskTypeService;
 use SimplyTestable\ApiBundle\Tests\Factory\JobConfigurationFactory;
+use SimplyTestable\ApiBundle\Tests\Factory\JobTaskConfigurationFactory;
 use SimplyTestable\ApiBundle\Tests\Factory\UserFactory;
 use SimplyTestable\ApiBundle\Tests\Functional\BaseSimplyTestableTestCase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -19,16 +22,6 @@ class JobConfigurationGetControllerTest extends BaseSimplyTestableTestCase
     private $jobConfigurationGetController;
 
     /**
-     * @var JobConfiguration
-     */
-    private $jobConfiguration;
-
-    /**
-     * @var User
-     */
-    private $user;
-
-    /**
      * {@inheritdoc}
      */
     protected function setUp()
@@ -37,15 +30,6 @@ class JobConfigurationGetControllerTest extends BaseSimplyTestableTestCase
 
         $this->jobConfigurationGetController = new GetController();
         $this->jobConfigurationGetController->setContainer($this->container);
-
-        $userFactory = new UserFactory($this->container);
-        $this->user = $userFactory->createAndActivateUser();
-        $this->setUser($this->user);
-
-        $jobConfigurationFactory = new JobConfigurationFactory($this->container);
-        $this->jobConfiguration = $jobConfigurationFactory->create([
-            JobConfigurationFactory::KEY_USER => $this->user,
-        ]);
     }
 
     public function testRequest()
@@ -53,9 +37,14 @@ class JobConfigurationGetControllerTest extends BaseSimplyTestableTestCase
         $userFactory = new UserFactory($this->container);
         $user = $userFactory->create();
 
+        $jobConfigurationFactory = new JobConfigurationFactory($this->container);
+        $jobConfiguration = $jobConfigurationFactory->create([
+            JobConfigurationFactory::KEY_USER => $user,
+        ]);
+
         $router = $this->container->get('router');
         $requestUrl = $router->generate('jobconfiguration_get_get', [
-            'label' => $this->jobConfiguration->getLabel(),
+            'label' => $jobConfiguration->getLabel(),
         ]);
 
         $this->getCrawler([
@@ -76,23 +65,91 @@ class JobConfigurationGetControllerTest extends BaseSimplyTestableTestCase
         $this->jobConfigurationGetController->getAction('foo');
     }
 
-    public function testGetActionSuccess()
+    /**
+     * @dataProvider getActionSuccessDataProvider
+     *
+     * @param array $jobConfigurationValues
+     * @param array $expectedResponseData
+     */
+    public function testGetActionSuccess($jobConfigurationValues, $expectedResponseData)
     {
-        $response = $this->jobConfigurationGetController->getAction($this->jobConfiguration->getLabel());
+        $userFactory = new UserFactory($this->container);
+        $user = $userFactory->createAndActivateUser();
+        $this->setUser($user);
+
+        $jobConfigurationValues[JobConfigurationFactory::KEY_USER] = $user;
+
+        $jobConfigurationFactory = new JobConfigurationFactory($this->container);
+        $jobConfiguration = $jobConfigurationFactory->create($jobConfigurationValues);
+
+        $response = $this->jobConfigurationGetController->getAction($jobConfiguration->getLabel());
 
         $this->assertTrue($response->isSuccessful());
+        $this->assertEquals($response->headers->get('content-type'), 'application/json');
 
         $responseData = json_decode($response->getContent(), true);
 
-        $this->assertEquals(
-            [
-                'label' => $this->jobConfiguration->getLabel(),
-                'user' => $this->jobConfiguration->getUser()->getEmailCanonical(),
-                'website' => $this->jobConfiguration->getWebsite()->getCanonicalUrl(),
-                'type' => $this->jobConfiguration->getType()->getName(),
+        $this->assertEquals($expectedResponseData, $responseData);
+    }
+
+    /**
+     * @return array
+     */
+    public function getActionSuccessDataProvider()
+    {
+        return [
+            'without task configuration' => [
+                'jobConfigurationValues' => [
+                    JobConfigurationFactory::KEY_LABEL => 'foo',
+                    JobConfigurationFactory::KEY_WEBSITE_URL => 'http://foo.example.com/',
+                    JobConfigurationFactory::KEY_TYPE => JobTypeService::FULL_SITE_NAME,
+                ],
+                'expectedResponseData' => [
+                'label' => 'foo',
+                'user' => 'user@example.com',
+                'website' => 'http://foo.example.com/',
+                'type' => JobTypeService::FULL_SITE_NAME,
                 'task_configurations' => [],
+                ],
             ],
-            $responseData
-        );
+            'with task configuration' => [
+                'jobConfigurationValues' => [
+                    JobConfigurationFactory::KEY_LABEL => 'bar',
+                    JobConfigurationFactory::KEY_WEBSITE_URL => 'http://bar.example.com/',
+                    JobConfigurationFactory::KEY_TYPE => JobTypeService::SINGLE_URL_NAME,
+                    JobConfigurationFactory::KEY_TASK_CONFIGURATIONS => [
+                        [
+                            JobTaskConfigurationFactory::KEY_TYPE => TaskTypeService::HTML_VALIDATION_TYPE,
+                            JobTaskConfigurationFactory::KEY_OPTIONS => [
+                                'html-validation-foo' => 'html-validation-bar',
+                            ],
+                        ],
+                        [
+                            JobTaskConfigurationFactory::KEY_TYPE => TaskTypeService::CSS_VALIDATION_TYPE,
+                        ],
+                    ],
+                ],
+                'expectedResponseData' => [
+                    'label' => 'bar',
+                    'user' => 'user@example.com',
+                    'website' => 'http://bar.example.com/',
+                    'type' => JobTypeService::SINGLE_URL_NAME,
+                    'task_configurations' => [
+                        [
+                            'type' => TaskTypeService::HTML_VALIDATION_TYPE,
+                            'options' => [
+                                'html-validation-foo' => 'html-validation-bar',
+                            ],
+                            'is_enabled' => true,
+                        ],
+                        [
+                            'type' => TaskTypeService::CSS_VALIDATION_TYPE,
+                            'options' => [],
+                            'is_enabled' => true,
+                        ],
+                    ],
+                ],
+            ],
+        ];
     }
 }
