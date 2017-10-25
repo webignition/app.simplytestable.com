fd67d80736b2a62341dab6db619590068d8048de
Jon Cram <jon@webignition.net>
2017-10-25 11:38:57 +0100

    Integrate with UserController

--- src/SimplyTestable/ApiBundle/Controller/UserController.php
+++ src/SimplyTestable/ApiBundle/Controller/UserController.php
@@ -2,10 +2,8 @@
 
 namespace SimplyTestable\ApiBundle\Controller;
 
-use SimplyTestable\ApiBundle\Services\Team\InviteService;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class UserController extends ApiController
@@ -17,67 +15,21 @@ class UserController extends ApiController
     {
         $userAccountPlanService = $this->container->get('simplytestable.services.useraccountplanservice');
         $accountPlanService = $this->container->get('simplytestable.services.accountplanservice');
-        $stripeService = $this->container->get('simplytestable.services.stripeservice');
-        $jobUserAccountPlanEnforcementService = $this->container->get(
-            'simplytestable.services.jobuseraccountplanenforcementservice'
-        );
-        $teamService = $this->container->get('simplytestable.services.teamservice');
-        $teamInviteService = $this->container->get('simplytestable.services.teaminviteservice');
-
         $user = $this->getUser();
 
-        $userAccountPlan = $userAccountPlanService->getForUser($this->getUser());
+        $userAccountPlan = $userAccountPlanService->getForUser($user);
         if (empty($userAccountPlan)) {
             $basicPlan = $accountPlanService->find('basic');
 
-            $userAccountPlan = $userAccountPlanService->subscribe(
+            $userAccountPlanService->subscribe(
                 $user,
                 $basicPlan
             );
         }
 
-        $userSummary = [
-            'email' => $user->getEmailCanonical(),
-            'user_plan' => $userAccountPlan
-        ];
+        $userSummaryFactory = $this->container->get('simplytestable.services.usersummaryfactory');
 
-        $userAccountPlanStripeCustomer = $userAccountPlan->getStripeCustomer();
-
-        $includeStripeCustomerInSummary =
-            !empty($userAccountPlanStripeCustomer) && $user->getId() == $userAccountPlan->getUser()->getId();
-
-        if ($includeStripeCustomerInSummary) {
-            $userSummary['stripe_customer'] = $stripeService->getCustomer($userAccountPlan)->__toArray();
-        }
-
-        $planConstraints = [];
-
-        $plan = $userAccountPlan->getPlan();
-
-        $creditsPerMonthConstraint = $plan->getConstraintNamed('credits_per_month');
-        if (!empty($creditsPerMonthConstraint)) {
-            $jobUserAccountPlanEnforcementService->setUser($this->getUser());
-            $planConstraints['credits'] = [
-                'limit' => $plan->getConstraintNamed('credits_per_month')->getLimit(),
-                'used' => $jobUserAccountPlanEnforcementService->getCreditsUsedThisMonth()
-            ];
-        }
-
-        $urlsPerJobConstraint = $userAccountPlan->getPlan()->getConstraintNamed('urls_per_job');
-        if (!empty($urlsPerJobConstraint)) {
-            $planConstraints['urls_per_job'] = $urlsPerJobConstraint->getLimit();
-        }
-
-        $userSummary['plan_constraints'] = $planConstraints;
-
-        $hasTeamInvite = !$plan->getIsPremium() && $teamInviteService->hasAnyForUser($user);
-
-        $userSummary['team_summary'] = [
-            'in' => $teamService->hasForUser($this->getUser()),
-            'has_invite' => $hasTeamInvite
-        ];
-
-        return $this->sendResponse($userSummary);
+        return new JsonResponse($userSummaryFactory->create());
     }
 
     /**
