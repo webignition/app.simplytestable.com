6b029aeb45251adc0dec3d11d37842be98285776
Jon Cram <jon@webignition.net>
2017-10-25 06:37:24 +0100

    Tidy up Entity/UserPostActivationProperties

--- src/SimplyTestable/ApiBundle/Entity/UserPostActivationProperties.php
+++ src/SimplyTestable/ApiBundle/Entity/UserPostActivationProperties.php
@@ -2,66 +2,51 @@
 namespace SimplyTestable\ApiBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use SimplyTestable\ApiBundle\Entity\User;
 use SimplyTestable\ApiBundle\Entity\Account\Plan\Plan as AccountPlan;
 
-use JMS\SerializerBundle\Annotation as SerializerAnnotation;
-
 /**
- * 
+ *
  * @ORM\Entity
  * @ORM\Table(
  *     name="UserPostActivationProperties"
  * )
- * @SerializerAnnotation\ExclusionPolicy("all")
  */
 class UserPostActivationProperties
-{    
+{
     /**
-     * 
-     * @var integer
-     * 
+     * @var int
+     *
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
-     * 
      */
     protected $id;
-    
+
     /**
-     *
      * @var User
-     * 
+     *
      * @ORM\ManyToOne(targetEntity="SimplyTestable\ApiBundle\Entity\User")
      * @ORM\JoinColumn(name="user_id", referencedColumnName="id", nullable=false)
-     * 
      */
     protected $user;
-    
-    
+
     /**
-     *
      * @var AccountPlan
-     * 
+     *
      * @ORM\ManyToOne(targetEntity="SimplyTestable\ApiBundle\Entity\Account\Plan\Plan")
      * @ORM\JoinColumn(name="accountplan_id", referencedColumnName="id", nullable=false)
-     * 
      */
     protected $accountPlan;
-    
+
     /**
-     *
      * @var string
+     *
      * @ORM\Column(type="string", nullable=true)
      */
     protected $coupon;
 
-
-
     /**
-     * Get id
-     *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -69,22 +54,15 @@ class UserPostActivationProperties
     }
 
     /**
-     * Set coupon
-     *
      * @param string $coupon
-     * @return UserPostActivationProperties
      */
     public function setCoupon($coupon)
     {
         $this->coupon = $coupon;
-
-        return $this;
     }
 
     /**
-     * Get coupon
-     *
-     * @return string 
+     * @return string
      */
     public function getCoupon()
     {
@@ -92,21 +70,14 @@ class UserPostActivationProperties
     }
 
     /**
-     * Set user
-     *
      * @param User $user
-     * @return UserPostActivationProperties
      */
     public function setUser(User $user)
     {
         $this->user = $user;
-
-        return $this;
     }
 
     /**
-     * Get user
-     *
      * @return User
      */
     public function getUser()
@@ -115,21 +86,14 @@ class UserPostActivationProperties
     }
 
     /**
-     * Set accountPlan
-     *
      * @param AccountPlan $accountPlan
-     * @return UserPostActivationProperties
      */
     public function setAccountPlan(AccountPlan $accountPlan)
     {
         $this->accountPlan = $accountPlan;
-
-        return $this;
     }
 
     /**
-     * Get accountPlan
-     *
      * @return AccountPlan
      */
     public function getAccountPlan()
