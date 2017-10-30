655746430a78061ced02be60e99be9de4d26a116
Jon Cram <jon@webignition.net>
2017-10-30 17:59:47 +0000

    Autoload content type web resource map parameter

--- src/SimplyTestable/ApiBundle/DependencyInjection/SimplyTestableApiExtension.php
+++ src/SimplyTestable/ApiBundle/DependencyInjection/SimplyTestableApiExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -15,6 +16,13 @@ use Symfony\Component\DependencyInjection\Loader;
 class SimplyTestableApiExtension extends Extension
 {
     /**
+     * @var string[]
+     */
+    private $parameterFiles = [
+        'content_type_web_resource_map.yml',
+    ];
+
+    /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
@@ -22,7 +30,19 @@ class SimplyTestableApiExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $fileLocator = new FileLocator([
+            __DIR__.'/../Resources/config',
+        ]);
+
+        $loader = new Loader\YamlFileLoader($container, $fileLocator);
         $loader->load('services.yml');
+
+        foreach ($this->parameterFiles as $parameterFile) {
+            $parameterName = str_replace('.yml', '', $parameterFile);
+            $container->setParameter(
+                $parameterName,
+                Yaml::parse(file_get_contents($fileLocator->locate($parameterFile)))
+            );
+        }
     }
 }
