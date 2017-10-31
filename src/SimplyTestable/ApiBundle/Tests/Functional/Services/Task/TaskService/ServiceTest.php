ec82fac8b8b1a78cb81cee7d4780332f3687f13b
Jon Cram <jon@webignition.net>
2017-10-31 19:04:25 +0000

    Remove obsolete tests

--- src/SimplyTestable/ApiBundle/Tests/Functional/Services/Task/TaskService/ServiceTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace SimplyTestable\ApiBundle\Tests\Functional\Services\Task\TaskService;
-
-use SimplyTestable\ApiBundle\Tests\Functional\AbstractBaseTestCase;
-
-abstract class ServiceTest extends AbstractBaseTestCase {
-}
