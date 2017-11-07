e751699ac8f38490e43e8c65d50c0e464ad01c38
Jon Cram <jon@webignition.net>
2017-11-07 16:39:21 +0000

    Decouple StateService from EntityService

--- src/SimplyTestable/ApiBundle/Services/StateService.php
+++ src/SimplyTestable/ApiBundle/Services/StateService.php
@@ -1,12 +1,34 @@
 <?php
 namespace SimplyTestable\ApiBundle\Services;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 use SimplyTestable\ApiBundle\Entity\State;
 
-class StateService extends EntityService
+class StateService
 {
     const EXCEPTION_MESSAGE_UNKNOWN_STATE = 'Unknown state "%s"';
     const EXCEPTION_CODE_UNKNOWN_STATE = 1;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var EntityRepository
+     */
+    private $stateRepository;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->stateRepository = $entityManager->getRepository(State::class);
+    }
 
     /**
      * @var State[]
@@ -14,21 +36,18 @@ class StateService extends EntityService
     private $states = [];
 
     /**
-     * {@inheritdoc}
-     */
-    protected function getEntityName()
-    {
-        return State::class;
-    }
-
-    /**
      * @param string $name
+     *
      * @return State
      */
     public function fetch($name)
     {
         if (!isset($this->states[$name])) {
-            if (!$this->has($name)) {
+            $state = $this->stateRepository->findOneBy([
+                'name' => $name,
+            ]);
+
+            if (empty($state)) {
                 throw new \InvalidArgumentException(
                     sprintf(
                         self::EXCEPTION_MESSAGE_UNKNOWN_STATE,
@@ -38,7 +57,7 @@ class StateService extends EntityService
                 );
             }
 
-            $this->states[$name] = $this->find($name);
+            $this->states[$name] = $state;
         }
 
         return $this->states[$name];
@@ -58,30 +77,5 @@ class StateService extends EntityService
         }
 
         return $states;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return State
-     */
-    private function find($name)
-    {
-        /* @var State $state */
-        $state = $this->getEntityRepository()->findOneBy([
-           'name' => $name
-        ]);
-
-        return $state;
-    }
-
-    /**
-     * @param string $name
-     *
-     * @return bool
-     */
-    public function has($name)
-    {
-        return !is_null($this->find($name));
     }
 }
