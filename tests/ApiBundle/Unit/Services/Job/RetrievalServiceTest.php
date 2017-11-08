dddd8549a0b639212a1939317a11e18fc180741f
Jon Cram <jon@webignition.net>
2017-11-08 19:36:34 +0000

    Set user automatically in Job/RetrievalService

--- tests/ApiBundle/Unit/Services/Job/RetrievalServiceTest.php
+++ tests/ApiBundle/Unit/Services/Job/RetrievalServiceTest.php
@@ -2,30 +2,22 @@
 
 namespace Tests\ApiBundle\Unit\Services\Job;
 
-use SimplyTestable\ApiBundle\Entity\User;
+use Mockery\MockInterface;
 use SimplyTestable\ApiBundle\Repository\JobRepository;
 use SimplyTestable\ApiBundle\Services\Job\RetrievalService;
 use SimplyTestable\ApiBundle\Services\Team\Service as TeamService;
 use SimplyTestable\ApiBundle\Exception\Services\Job\RetrievalServiceException as JobRetrievalServiceException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Tests\ApiBundle\Factory\ModelFactory;
 
 class RetrievalServiceTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @dataProvider retrieveFailureDataProvider
-     *
-     * @param User $user
-     * @param string $expectedExceptionMessage
-     * @param int $expectedExceptionCode
-     */
-    public function testRetrieveFailure(
-        $user,
-        $expectedExceptionMessage,
-        $expectedExceptionCode
-    ) {
-        $this->expectException(JobRetrievalServiceException::class);
-        $this->expectExceptionMessage($expectedExceptionMessage);
-        $this->expectExceptionCode($expectedExceptionCode);
+    public function testRetrieveFailure()
+    {
+        $user = ModelFactory::createUser([
+            ModelFactory::USER_EMAIL => 'user1@example.com',
+        ]);
 
         /* @var TeamService $teamService */
         $teamService = \Mockery::mock(TeamService::class);
@@ -36,35 +28,24 @@ class RetrievalServiceTest extends \PHPUnit_Framework_TestCase
             ->shouldReceive('find')
             ->andReturnNull();
 
-        $retrievalService = new RetrievalService($teamService, $jobRepository);
-        if (!empty($user)) {
-            $retrievalService->setUser($user);
-        }
+        $token = \Mockery::mock(TokenInterface::class);
+        $token
+            ->shouldReceive('getUser')
+            ->andReturn($user);
+
+        /* @var TokenStorageInterface|MockInterface $tokenStorage */
+        $tokenStorage = \Mockery::mock(TokenStorageInterface::class);
+        $tokenStorage
+            ->shouldReceive('getToken')
+            ->andReturn($token);
+
+        $retrievalService = new RetrievalService($teamService, $jobRepository, $tokenStorage);
+
+        $this->expectException(JobRetrievalServiceException::class);
+        $this->expectExceptionMessage('Job [1] not found');
+        $this->expectExceptionCode(JobRetrievalServiceException::CODE_JOB_NOT_FOUND);
 
         $retrievalService->retrieve(1);
-    }
-
-    /**
-     * @return array
-     */
-    public function retrieveFailureDataProvider()
-    {
-        $user1 = ModelFactory::createUser([
-            ModelFactory::USER_EMAIL => 'user1@example.com',
-        ]);
-
-        return [
-            'user not set' => [
-                'user' => null,
-                'expectedExceptionMessage' => 'User not set',
-                'expectedExceptionCode' => JobRetrievalServiceException::CODE_USER_NOT_SET,
-            ],
-            'invalid job' => [
-                'user' => $user1,
-                'expectedExceptionMessage' => 'Job [1] not found',
-                'expectedExceptionCode' => JobRetrievalServiceException::CODE_JOB_NOT_FOUND,
-            ],
-        ];
     }
 
     /**
