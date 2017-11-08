a9d38f24effca6e717f5c306fca2d7e5a99b08df
Jon Cram <jon@webignition.net>
2017-11-08 18:01:14 +0000

    Moved ScheduledJobDeleteController into ScheduledJobController

--- src/SimplyTestable/ApiBundle/Controller/ScheduledJobController.php
+++ src/SimplyTestable/ApiBundle/Controller/ScheduledJobController.php
@@ -10,6 +10,7 @@ use Cron\Exception\InvalidPatternException;
 use SimplyTestable\ApiBundle\Exception\Services\ScheduledJob\Exception as ScheduledJobException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
 
 class ScheduledJobController extends Controller
@@ -115,5 +116,25 @@ class ScheduledJobController extends Controller
                 ])
             ]);
         }
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function deleteAction($id)
+    {
+        $scheduledJobService = $this->container->get('simplytestable.services.scheduledjob.service');
+
+        $scheduledJob = $scheduledJobService->get($id);
+
+        if (empty($scheduledJob)) {
+            throw new NotFoundHttpException();
+        }
+
+        $scheduledJobService->delete($scheduledJob);
+
+        return new Response();
     }
 }
