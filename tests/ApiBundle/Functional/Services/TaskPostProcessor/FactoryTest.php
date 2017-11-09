c64a73cc442139d812da39e716b327c6a2865266
Jon Cram <jon@webignition.net>
2017-11-09 12:54:55 +0000

    Create UrlDiscoveryTaskPostProcessor

---
+++ tests/ApiBundle/Functional/Services/TaskPostProcessor/FactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\ApiBundle\Functional\Services\TaskPostProcessor;
+
+use SimplyTestable\ApiBundle\Services\TaskPostProcessor\UrlDiscoveryTaskPostProcessor;
+use SimplyTestable\ApiBundle\Services\TaskPreProcessor\Factory;
+use Tests\ApiBundle\Functional\AbstractBaseTestCase;
+
+class FactoryTest extends AbstractBaseTestCase
+{
+    /**
+     * @var Factory
+     */
+    private $factory;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->factory = $this->container->get('simplytestable.services.taskpostprocessor.factory');
+    }
+
+    public function testGetUrlDiscoveryTaskPostProcessor()
+    {
+        $taskTypeService = $this->container->get('simplytestable.services.tasktypeservice');
+
+        $this->assertInstanceOf(
+            UrlDiscoveryTaskPostProcessor::class,
+            $this->factory->getPreprocessor($taskTypeService->getUrlDiscoveryTaskType())
+        );
+    }
+}
