67134b226e5e6116ebf2c927e5208e900c1e9dd4
Jon Cram <jon@webignition.net>
2017-11-10 15:34:04 +0000

    Rename WorkerTaskCancellationService definition

--- tests/ApiBundle/Functional/Services/Resque/JobFactoryTest.php
+++ tests/ApiBundle/Functional/Services/Resque/JobFactoryTest.php
@@ -18,6 +18,7 @@ use SimplyTestable\ApiBundle\Services\StateService;
 use SimplyTestable\ApiBundle\Services\TaskService;
 use SimplyTestable\ApiBundle\Services\WorkerActivationRequestService;
 use SimplyTestable\ApiBundle\Services\WorkerTaskAssignmentService;
+use SimplyTestable\ApiBundle\Services\WorkerTaskCancellationService;
 use Tests\ApiBundle\Functional\AbstractBaseTestCase;
 
 class JobFactoryTest extends AbstractBaseTestCase
@@ -254,7 +255,7 @@ class JobFactoryTest extends AbstractBaseTestCase
                     'serviceIds' => [
                         'simplytestable.services.applicationstateservice',
                         TaskService::class,
-                        'simplytestable.services.workertaskcancellationservice',
+                        WorkerTaskCancellationService::class,
                         'logger',
                         'doctrine.orm.entity_manager',
                     ],
