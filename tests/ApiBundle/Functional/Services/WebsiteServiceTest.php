df3c417e833c4013a1b120ba537160228c8e23a9
Jon Cram <webignition@gmail.com>
2018-05-16 11:35:45 +0100

    Reject new jobs where the url is a local windows path (#287)

--- tests/ApiBundle/Functional/Services/WebsiteServiceTest.php
+++ tests/ApiBundle/Functional/Services/WebsiteServiceTest.php
@@ -8,14 +8,51 @@ use Tests\ApiBundle\Functional\AbstractBaseTestCase;
 
 class WebsiteServiceTest extends AbstractBaseTestCase
 {
-    public function testFetch()
+    /**
+     * @dataProvider getDataProvider
+     *
+     * @param string $url
+     * @param string $expectedWebsiteCanonicalUrl
+     */
+    public function testGet($url, $expectedWebsiteCanonicalUrl)
     {
         $websiteService = $this->container->get(WebSiteService::class);
-
-        $url = 'http://example.com/';
         $website = $websiteService->get($url);
 
         $this->assertInstanceOf(Website::class, $website);
-        $this->assertEquals($url, $website->getCanonicalUrl());
+        $this->assertEquals($expectedWebsiteCanonicalUrl, $website->getCanonicalUrl());
+    }
+
+    /**
+     * @return array
+     */
+    public function getDataProvider()
+    {
+        return [
+            'incomplete domain name' => [
+                'url' => 'http://foo',
+                'expectedWebsiteCanonicalUrl' => 'http://foo/',
+            ],
+            'unix-like local path' => [
+                'url' => '/home/users/foo',
+                'expectedWebsiteCanonicalUrl' => '/home/users/foo',
+            ],
+            'windows-like local path' => [
+                'url' => 'c:\Users\foo\Desktop\file.html',
+                'expectedWebsiteCanonicalUrl' => 'c:\Users\foo\Desktop\file.html',
+            ],
+            'not a valid url' => [
+                'url' => 'vertical-align:top',
+                'expectedWebsiteCanonicalUrl' => 'vertical-align:top',
+            ],
+            'complete http' => [
+                'url' => 'http://example.com/',
+                'expectedWebsiteCanonicalUrl' => 'http://example.com/',
+            ],
+            'complete https' => [
+                'url' => 'https://example.com/',
+                'expectedWebsiteCanonicalUrl' => 'https://example.com/',
+            ],
+        ];
     }
 }
