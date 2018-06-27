a52ac86706d578ec2dd6b97bd1706e81051bc7d4
Jon Cram <webignition@gmail.com>
2018-06-27 15:45:02 +0100

    Remove use of ResqueJobFactory from Controller/WorkerController (#311)

--- tests/ApiBundle/Unit/Controller/WorkerControllerTest.php
+++ tests/ApiBundle/Unit/Controller/WorkerControllerTest.php
@@ -11,7 +11,6 @@ use Tests\ApiBundle\Factory\MockFactory;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\ServiceUnavailableHttpException;
-use webignition\ResqueJobFactory\ResqueJobFactory;
 use SimplyTestable\ApiBundle\Services\Resque\QueueService as ResqueQueueService;
 
 /**
@@ -75,134 +74,6 @@ class WorkerControllerTest extends \PHPUnit_Framework_TestCase
             ],
         ];
     }
-//
-//    /**
-//     * @dataProvider activateActionWorkerInWrongStateDataProvider
-//     *
-//     * @param string $stateName
-//     */
-//    public function testActivateActionSuccessWorkerInWrongState($stateName)
-//    {
-//        $resqueQueueService = $this->container->get(QueueService::class);
-//        $entityManager = $this->container->get('doctrine.orm.entity_manager');
-//        $workerActivationRequestRepository = $entityManager->getRepository(WorkerActivationRequest::class);
-//
-//        $hostname = 'foo.worker.simplytestable.com';
-//        $token = 'token';
-//
-//        $workerFactory = new WorkerFactory($this->container);
-//        $workerFactory->create([
-//            WorkerFactory::KEY_HOSTNAME => $hostname,
-//            WorkerFactory::KEY_TOKEN => $token,
-//            WorkerFactory::KEY_STATE => $stateName,
-//        ]);
-//
-//        $request = new Request([], [
-//            'hostname' => $hostname,
-//            'token' => $token,
-//        ]);
-//
-//        $response = $this->workerController->activateAction($request);
-//
-//        $this->assertTrue($response->isSuccessful());
-//        $this->assertEmpty($workerActivationRequestRepository->findAll());
-//        $this->assertTrue($resqueQueueService->isEmpty('worker-activate-verify'));
-//    }
-//
-//    /**
-//     * @return array
-//     */
-//    public function activateActionWorkerInWrongStateDataProvider()
-//    {
-//        return [
-//            Worker::STATE_ACTIVE => [
-//                'stateName' => Worker::STATE_ACTIVE,
-//            ],
-//            Worker::STATE_OFFLINE => [
-//                'stateName' => Worker::STATE_OFFLINE,
-//            ],
-//            Worker::STATE_DELETED => [
-//                'stateName' => Worker::STATE_DELETED,
-//            ],
-//        ];
-//    }
-//
-//    /**
-//     * @dataProvider activateActionSuccessDataProvider
-//     *
-//     * @param string $existingActivationRequestStateName
-//     */
-//    public function testActivateActionSuccess($existingActivationRequestStateName)
-//    {
-//        $entityManager = $this->container->get('doctrine.orm.entity_manager');
-//        $resqueQueueService = $this->container->get(QueueService::class);
-//        $stateService = $this->container->get(StateService::class);
-//        $workerActivationRequestRepository = $entityManager->getRepository(WorkerActivationRequest::class);
-//
-//        $this->assertEmpty($workerActivationRequestRepository->findAll());
-//
-//        $hostname = 'foo.worker.simplytestable.com';
-//        $token = 'token';
-//
-//        $workerFactory = new WorkerFactory($this->container);
-//        $worker = $workerFactory->create([
-//            WorkerFactory::KEY_HOSTNAME => $hostname,
-//            WorkerFactory::KEY_TOKEN => $token,
-//            WorkerFactory::KEY_STATE => Worker::STATE_UNACTIVATED,
-//        ]);
-//
-//        if (!empty($existingActivationRequestStateName)) {
-//            $activationRequest = new WorkerActivationRequest();
-//            $activationRequest->setState($stateService->get($existingActivationRequestStateName));
-//            $activationRequest->setWorker($worker);
-//            $activationRequest->setToken($token);
-//
-//            $entityManager->persist($activationRequest);
-//            $entityManager->flush();
-//        }
-//
-//        $request = new Request([], [
-//            'hostname' => $hostname,
-//            'token' => $token,
-//        ]);
-//
-//        $response = $this->workerController->activateAction($request);
-//
-//        $this->assertTrue($response->isSuccessful());
-//
-//        $activationRequest = $workerActivationRequestRepository->findOneBy([
-//            'worker' => $worker,
-//        ]);
-//
-//        $this->assertInstanceOf(WorkerActivationRequest::class, $activationRequest);
-//        $this->assertEquals($worker, $activationRequest->getWorker());
-//
-//        $this->assertTrue($resqueQueueService->contains(
-//            'worker-activate-verify',
-//            ['id' => $activationRequest->getWorker()->getId()]
-//        ));
-//    }
-//
-//    /**
-//     * @return array
-//     */
-//    public function activateActionSuccessDataProvider()
-//    {
-//        return [
-//            'no existing request' => [
-//                'existingActivationRequestStateName' => null,
-//            ],
-//            WorkerActivationRequestService::STARTING_STATE => [
-//                'existingActivationRequestStateName' => WorkerActivationRequestService::STARTING_STATE,
-//            ],
-//            WorkerActivationRequestService::VERIFIED_STATE => [
-//                'existingActivationRequestStateName' => WorkerActivationRequestService::VERIFIED_STATE,
-//            ],
-//            WorkerActivationRequestService::FAILED_STATE => [
-//                'existingActivationRequestStateName' => WorkerActivationRequestService::FAILED_STATE,
-//            ],
-//        ];
-//    }
 
     /**
      * @param array $services
@@ -223,10 +94,6 @@ class WorkerControllerTest extends \PHPUnit_Framework_TestCase
             $services[ResqueQueueService::class] = MockFactory::createResqueQueueService();
         }
 
-        if (!isset($services[ResqueJobFactory::class])) {
-            $services[ResqueJobFactory::class] = MockFactory::createResqueJobFactory();
-        }
-
         if (!isset($services[WorkerActivationRequestService::class])) {
             $services[WorkerActivationRequestService::class] = MockFactory::createWorkerActivationRequestService();
         }
@@ -239,7 +106,6 @@ class WorkerControllerTest extends \PHPUnit_Framework_TestCase
             $services[ApplicationStateService::class],
             $services[EntityManagerInterface::class],
             $services[ResqueQueueService::class],
-            $services[ResqueJobFactory::class],
             $services[WorkerActivationRequestService::class],
             $services[StateService::class]
         );
