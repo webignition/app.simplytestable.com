a4f34b80ec443c598138ea6b5549aca80339f226
Jon Cram <jon@webignition.net>
2018-06-28 15:40:42 +0100

    Create PostmarkClient service, update StripeWebHookMailNotificationSender

--- src/SimplyTestable/ApiBundle/Services/StripeWebHookMailNotificationSender.php
+++ src/SimplyTestable/ApiBundle/Services/StripeWebHookMailNotificationSender.php
@@ -2,14 +2,21 @@
 
 namespace SimplyTestable\ApiBundle\Services;
 
-use SimplyTestable\ApiBundle\Services\Mail\Service as MailService;
+use Postmark\Models\PostmarkException;
+use Postmark\PostmarkClient;
+use Psr\Log\LoggerInterface;
 
 class StripeWebHookMailNotificationSender
 {
     /**
-     * @var MailService
+     * @var PostmarkClient
      */
-    private $mailService;
+    private $postmarkClient;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     /**
      * @var array
@@ -17,14 +24,17 @@ class StripeWebHookMailNotificationSender
     private $parameters;
 
     /**
-     * @param MailService $mailService
+     * @param PostmarkClient $postmarkClient
+     * @param LoggerInterface $logger
      * @param $parameters
      */
     public function __construct(
-        MailService $mailService,
+        PostmarkClient $postmarkClient,
+        LoggerInterface $logger,
         $parameters
     ) {
-        $this->mailService = $mailService;
+        $this->postmarkClient = $postmarkClient;
+        $this->logger = $logger;
         $this->parameters = $parameters;
     }
 
@@ -34,12 +44,22 @@ class StripeWebHookMailNotificationSender
      */
     public function send($rawWebHookData, $eventType)
     {
-        $message = $this->mailService->getNewMessage();
-        $message->setFrom($this->parameters['sender_email'], $this->parameters['sender_name']);
-        $message->addTo($this->parameters['recipient_email']);
-        $message->setSubject(str_replace('{{ event-type }}', $eventType, $this->parameters['subject']));
-        $message->setTextMessage($rawWebHookData);
-
-        $this->mailService->getSender()->send($message);
+        try {
+            $this->postmarkClient->sendEmail(
+                $this->parameters['sender_email'],
+                $this->parameters['recipient_email'],
+                str_replace('{{ event-type }}', $eventType, $this->parameters['subject']),
+                null,
+                $rawWebHookData
+            );
+        } catch (PostmarkException $postmarkException) {
+            $this->logger->error(sprintf(
+                'Postmark failure [%s] [$s]',
+                $postmarkException->httpStatusCode,
+                $postmarkException->postmarkApiErrorCode
+            ), [
+                'message' => $postmarkException->getMessage(),
+            ]);
+        }
     }
 }
