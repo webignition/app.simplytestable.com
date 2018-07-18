fc4c2210d39006849476055e3daeaefc1f691848
Jon Cram <webignition@gmail.com>
2018-07-18 11:44:42 +0100

    Move bundle config out of bundle (#334)

--- src/SimplyTestable/ApiBundle/DependencyInjection/SimplyTestableApiExtension.php
+++ src/SimplyTestable/ApiBundle/DependencyInjection/SimplyTestableApiExtension.php
@@ -16,33 +16,11 @@ use Symfony\Component\Yaml\Yaml;
 class SimplyTestableApiExtension extends Extension
 {
     /**
-     * @var string[]
-     */
-    private $parameterFiles = [
-        'content_type_web_resource_map.yml',
-        'curl_options.yml',
-        'css_validation_domains_to_ignore.yml',
-        'js_static_analysis_domains_to_ignore.yml',
-    ];
-
-    /**
      * {@inheritDoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        $fileLocator = new FileLocator([
-            __DIR__.'/../Resources/config',
-        ]);
-
-        foreach ($this->parameterFiles as $parameterFile) {
-            $parameterName = str_replace('.yml', '', $parameterFile);
-            $container->setParameter(
-                $parameterName,
-                Yaml::parse(file_get_contents($fileLocator->locate($parameterFile)))
-            );
-        }
     }
 }
