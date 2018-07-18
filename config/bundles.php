03bddbbf9514419128750eb71da1567d15cf8d9e
Jon Cram <jon@webignition.net>
2018-07-18 17:16:45 +0100

    Switch to webignition/resque-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -7,10 +7,10 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
-    ResqueBundle\Resque\ResqueBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    ResqueBundle\Resque\ResqueBundle::class => ['all' => true],
 ];
