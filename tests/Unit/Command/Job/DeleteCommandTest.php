42b7be39bc796b76058bfb3143df4ad425787542
Jon Cram <webignition@gmail.com>
2018-09-12 19:37:22 +0100

    Create command to delete job (#353)

---
+++ tests/Unit/Command/Job/DeleteCommandTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Unit\Command\Job;
+
+use App\Command\Job\DeleteCommand;
+use App\Resque\Job\Job\PrepareJob;
+use App\Tests\Factory\MockFactory;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class DeleteCommandTest extends \PHPUnit\Framework\TestCase
+{
+    public function testRunInMaintenanceReadOnlyMode()
+    {
+        $jobId = 1;
+
+        $resqueQueueService = MockFactory::createResqueQueueService();
+        $resqueQueueService
+            ->shouldReceive('enqueue')
+            ->withArgs(function (PrepareJob $prepareJob) use ($jobId) {
+                $this->assertEquals(['id' => $jobId], $prepareJob->args);
+
+                return true;
+            });
+
+        $command = new DeleteCommand(
+            MockFactory::createApplicationStateService(true),
+            MockFactory::createEntityManager()
+        );
+
+        $returnCode = $command->run(new ArrayInput([
+            'id' => $jobId,
+        ]), new BufferedOutput());
+
+        $this->assertEquals(
+            DeleteCommand::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE,
+            $returnCode
+        );
+    }
+}
