aa0702351c49e3a9460b5196bf9bab73bb34f2e7
Jon Cram <jon@webignition.net>
2018-09-12 18:51:30 +0100

    Create command to delete job

---
+++ src/Command/Job/DeleteCommand.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Command\Job;
+
+use App\Entity\Job\TaskTypeOptions;
+use App\Entity\Task\Task;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Job\Job;
+use App\Services\ApplicationStateService;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+
+class DeleteCommand extends Command
+{
+    const NAME = 'simplytestable:job:delete';
+    const RETURN_CODE_OK = 0;
+    const RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE = 1;
+
+    /**
+     * @var ApplicationStateService
+     */
+    private $applicationStateService;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @param ApplicationStateService $applicationStateService
+     * @param EntityManagerInterface $entityManager
+     * @param string|null $name
+     */
+    public function __construct(
+        ApplicationStateService $applicationStateService,
+        EntityManagerInterface $entityManager,
+        $name = null
+    ) {
+        parent::__construct($name);
+
+        $this->applicationStateService = $applicationStateService;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName(self::NAME)
+            ->setDescription('Delete a job')
+            ->addArgument('id', InputArgument::REQUIRED, 'id of job to delete')
+            ->addOption('force')
+            ->addOption('dry-run')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($this->applicationStateService->isInReadOnlyMode()) {
+            return self::RETURN_CODE_IN_MAINTENANCE_READ_ONLY_MODE;
+        }
+
+        $jobRepository = $this->entityManager->getRepository(Job::class);
+
+        /* @var Job $job */
+        $job = $jobRepository->find((int)$input->getArgument('id'));
+
+        if (empty($job)) {
+            return self::RETURN_CODE_OK;
+        }
+
+        $isDryRun = $input->getOption('dry-run');
+
+        $output->writeln('<info>Processing job</info> <comment>' . $job->getId() . '</comment>');
+
+        if ($isDryRun) {
+            $output->writeln('<info>This is a DRY RUN, no changes will be saved</info>');
+        }
+
+        $confirmDelete = $input->getOption('force');
+
+        if (!$confirmDelete) {
+            $questionHelper = $this->getHelper('question');
+            $confirmationQuestion = new ConfirmationQuestion(
+                '<comment>Delete job</comment> ' . $job->getId() . '? (y/<comment>n</comment>) ',
+                false
+            );
+
+            $confirmDelete = $questionHelper->ask($input, $output, $confirmationQuestion);
+        }
+
+        if (!$confirmDelete) {
+            $output->writeln('<error>Cancelled</error>');
+
+            return self::RETURN_CODE_OK;
+        }
+
+        /* @var Task[] $tasks */
+        $tasks = $job->getTasks();
+        $taskCount = count($tasks);
+
+        $output->writeln([
+            '',
+            '<comment>' . $taskCount . '</comment> tasks to remove ...'
+        ]);
+
+        foreach ($tasks as $taskIndex => $task) {
+            $taskNumber = $taskIndex + 1;
+
+            $output->writeln(sprintf(
+                '    <info>%s of %s</info> ... removing task <comment>%s</comment>',
+                $taskNumber,
+                $taskCount,
+                $task->getId()
+            ));
+
+            $this->entityManager->remove($task);
+
+            if (!$isDryRun) {
+                $this->entityManager->flush();
+            }
+        }
+
+        /* @var TaskTypeOptions[] $jobTaskTypeOptions */
+        $jobTaskTypeOptions = $job->getTaskTypeOptions();
+        $jobTaskTypeOptionsCount = count($jobTaskTypeOptions);
+
+        $output->writeln([
+            '',
+            '<comment>' . $jobTaskTypeOptionsCount . '</comment> task type options to remove ...'
+        ]);
+
+        foreach ($jobTaskTypeOptions as $taskTypeOption) {
+            $output->writeln(sprintf(
+                '    <info>Removing task type options for</info> <comment>%s</comment>',
+                $taskTypeOption->getTaskType()->getName()
+            ));
+
+            $this->entityManager->remove($taskTypeOption);
+
+            if (!$isDryRun) {
+                $this->entityManager->flush();
+            }
+        }
+
+        $output->writeln([
+            '',
+            'Removing job <comment>' . $job->getId() . '</comment>',
+            '',
+        ]);
+
+        $this->entityManager->remove($job);
+
+        if (!$isDryRun) {
+            $this->entityManager->flush();
+        }
+
+        if ($isDryRun) {
+            $output->writeln('<info>This is a DRY RUN, no changes will be saved</info>');
+        }
+
+        $output->writeln('<info>Done!</info>');
+
+        return self::RETURN_CODE_OK;
+    }
+}
---
+++ tests/Functional/Command/Job/DeleteCommandTest.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace App\Tests\Functional\Command\Job;
+
+use App\Entity\Job\TaskTypeOptions;
+use App\Entity\Task\Task;
+use App\Services\TaskTypeService;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Command\Job\DeleteCommand;
+use App\Entity\Job\Job;
+use App\Tests\Factory\JobFactory;
+use App\Tests\Functional\AbstractBaseTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+
+class DeleteCommandTest extends AbstractBaseTestCase
+{
+    /**
+     * @var JobFactory
+     */
+    private $jobFactory;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var DeleteCommand
+     */
+    private $deleteCommand;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->jobFactory = new JobFactory(self::$container);
+        $this->entityManager = self::$container->get(EntityManagerInterface::class);
+        $this->deleteCommand = self::$container->get(DeleteCommand::class);
+    }
+
+    /**
+     * @dataProvider runDataProvider
+     *
+     * @param array $jobValuesCollection
+     * @param int $jobIndexToDelete
+     * @param array $expectedJobValues
+     * @param array $expectedRemainingJobValuesCollection
+     */
+    public function testRun(
+        array $jobValuesCollection,
+        int $jobIndexToDelete,
+        array $expectedJobValues,
+        array $expectedRemainingJobValuesCollection
+    ) {
+        $jobRepository = $this->entityManager->getRepository(Job::class);
+        $taskRepository = $this->entityManager->getRepository(Task::class);
+        $jobTaskTypeOptionsRepository = $this->entityManager->getRepository(TaskTypeOptions::class);
+
+        /* @var Job[] $jobs */
+        $jobs = $this->jobFactory->createResolveAndPrepareCollection($jobValuesCollection);
+
+        $expectedJobExists = !empty($expectedJobValues);
+
+        $job = null;
+        if ($expectedJobExists) {
+            $job = $jobs[$jobIndexToDelete];
+
+            $this->assertInstanceOf(Job::class, $job);
+            $this->assertNotEmpty($taskRepository->findBy([
+                'job' => $job,
+            ]));
+
+            $jobTaskTypeOptionsCollection = $jobTaskTypeOptionsRepository->findBy([
+                'job' => $job,
+            ]);
+
+            if (empty($expectedJobValues[JobFactory::KEY_TEST_TYPE_OPTIONS])) {
+                $this->assertEmpty($jobTaskTypeOptionsCollection);
+            } else {
+                foreach ($jobTaskTypeOptionsCollection as $jobTaskTypeOptions) {
+                    $taskType = $jobTaskTypeOptions->getTaskType();
+                    $taskTypeName = $taskType->getName();
+
+                    $expectedTaskTypeOptions = $expectedJobValues[JobFactory::KEY_TEST_TYPE_OPTIONS][$taskTypeName];
+
+                    $this->assertEquals($expectedTaskTypeOptions, $jobTaskTypeOptions->getOptions());
+                }
+            }
+        }
+
+        $jobId = empty($job)
+            ? -1
+            : $job->getId();
+
+        $returnCode = $this->deleteCommand->run(new ArrayInput([
+            'id' => $jobId,
+            '--force' => true,
+        ]), new BufferedOutput());
+
+        $this->assertEquals(DeleteCommand::RETURN_CODE_OK, $returnCode);
+        $this->assertNull($jobRepository->find($jobId));
+
+        if ($expectedJobExists) {
+            $this->assertEmpty($taskRepository->findBy([
+                'job' => $job,
+            ]));
+
+            $this->assertEmpty($jobTaskTypeOptionsRepository->findBy([
+                'job' => $job,
+            ]));
+        }
+
+        $remainingJobs = $jobRepository->findAll();
+
+        foreach ($remainingJobs as $jobIndex => $remainingJob) {
+            $expectedRemainingJobValues = $expectedRemainingJobValuesCollection[$jobIndex];
+
+            $this->assertEquals(
+                $expectedRemainingJobValues[JobFactory::KEY_SITE_ROOT_URL],
+                (string)$remainingJob->getWebsite()
+            );
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function runDataProvider()
+    {
+        return [
+            'job not found; no jobs' => [
+                'jobValuesCollection' => [],
+                'jobIndexToDelete' => 0,
+                'expectedJobValues' => [],
+                'expectedRemainingJobValuesCollection' => [],
+            ],
+            'job not found; job does not exist' => [
+                'jobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://example.com/',
+                    ],
+                ],
+                'jobIndexToDelete' => 1,
+                'expectedJobValues' => [],
+                'expectedRemainingJobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://example.com/',
+                    ],
+                ],
+            ],
+            'job exists, no task type options' => [
+                'jobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://one.example.com/',
+                        JobFactory::KEY_DOMAIN => 'one.example.com',
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://two.example.com/',
+                        JobFactory::KEY_DOMAIN => 'two.example.com',
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://three.example.com/',
+                        JobFactory::KEY_DOMAIN => 'three.example.com',
+                    ],
+                ],
+                'jobIndexToDelete' => 1,
+                'expectedJobValues' => [
+                    JobFactory::KEY_SITE_ROOT_URL => 'http://two.example.com/',
+                    JobFactory::KEY_TEST_TYPE_OPTIONS => [],
+                ],
+                'expectedRemainingJobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://one.example.com/',
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://three.example.com/',
+                    ],
+                ],
+            ],
+            'job exists, has task type options' => [
+                'jobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://one.example.com/',
+                        JobFactory::KEY_DOMAIN => 'one.example.com',
+                        JobFactory::KEY_TEST_TYPES => [
+                            TaskTypeService::CSS_VALIDATION_TYPE,
+                        ],
+                        JobFactory::KEY_TEST_TYPE_OPTIONS => [
+                            TaskTypeService::CSS_VALIDATION_TYPE => [
+                                'domains-to-ignore' => [
+                                    'foo',
+                                ],
+                                'ignore-common-cdns' => true,
+                            ],
+                        ],
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://two.example.com/',
+                        JobFactory::KEY_DOMAIN => 'two.example.com',
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://three.example.com/',
+                        JobFactory::KEY_DOMAIN => 'three.example.com',
+                    ],
+                ],
+                'jobIndexToDelete' => 0,
+                'expectedJobValues' => [
+                    JobFactory::KEY_SITE_ROOT_URL => 'http://one.example.com/',
+                    JobFactory::KEY_TEST_TYPE_OPTIONS => [
+                        TaskTypeService::CSS_VALIDATION_TYPE => [
+                            'domains-to-ignore' => [
+                                'foo',
+                            ],
+                            'ignore-common-cdns' => true,
+                        ],
+                    ],
+                ],
+                'expectedRemainingJobValuesCollection' => [
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://two.example.com/',
+                    ],
+                    [
+                        JobFactory::KEY_SITE_ROOT_URL => 'http://three.example.com/',
+                    ],
+                ],
+            ],
+        ];
+    }
+}
