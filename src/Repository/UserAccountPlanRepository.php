9e4f430c998492a58718a742181a0ad6b5e9e3e2
Jon Cram <jon@webignition.net>
2019-03-05 15:29:51 +0000

    Register as service

--- src/Repository/UserAccountPlanRepository.php
+++ src/Repository/UserAccountPlanRepository.php
@@ -1,19 +1,27 @@
 <?php
+
 namespace App\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use App\Entity\UserAccountPlan;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
-class UserAccountPlanRepository extends EntityRepository
+class UserAccountPlanRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserAccountPlan::class);
+    }
 
-    public function findUserIdsWithPlan() {
+    public function findUserIdsWithPlan(): array
+    {
         $queryBuilder = $this->createQueryBuilder('UserAccountPlan');
         $queryBuilder->select('User.id');
         $queryBuilder->leftJoin('App\Entity\User', 'User', 'WITH', 'User.id = UserAccountPlan.user');
 
         $result = $queryBuilder->getQuery()->getResult();
 
-        $ids = array();
+        $ids = [];
 
         foreach ($result as $idResult) {
             $ids[] = $idResult['id'];
