60e36b84eceedfe501ddc3d47b8b55495af53098
Jon Cram <webignition@gmail.com>
2019-03-05 15:25:32 +0000

    Register UserRepository (#398)

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -1,17 +1,20 @@
 <?php
+
 namespace App\Repository;
 
-use Doctrine\ORM\EntityRepository;
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
-class UserRepository extends EntityRepository
+class UserRepository extends ServiceEntityRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
 
-    /**
-     *
-     * @param array $ids
-     * @return array
-     */
-    public function findAllNotWithIds($ids = array()) {
+    public function findAllNotWithIds(array $ids = []): array
+    {
         $queryBuilder = $this->createQueryBuilder('User');
         $queryBuilder->select('User');
 
