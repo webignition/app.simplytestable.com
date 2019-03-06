a6c4a304ef0f31272a14c7ffce84acf60412e1d5
Jon Cram <jon@webignition.net>
2019-03-06 19:06:22 +0000

    Add functional controller test

---
+++ tests/Functional/Controller/Job/Job/JobControllerIsAuthorisedActionTest.php
@@ -0,0 +1,79 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace App\Tests\Functional\Controller\Job\Job;
+
+use App\Tests\Services\JobFactory;
+use App\Tests\Services\UserFactory;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * @group Controller/Job/JobController
+ */
+class JobControllerIsAuthorisedActionTest extends AbstractJobControllerTest
+{
+    private $users;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->users = $this->userFactory->createPublicAndPrivateUserSet();
+    }
+
+    /**
+     * @dataProvider requestDataProvider
+     */
+    public function testRequest(string $ownerName, string $userName, bool $expectedIsAuthorised)
+    {
+        $owner = $this->users[$ownerName];
+        $user = $this->users[$userName];
+
+        $job = $this->jobFactory->create([
+            JobFactory::KEY_USER => $owner,
+        ]);
+
+        $this->getCrawler([
+            'url' => self::$container->get('router')->generate('job_job_isauthorised', [
+                'test_id' => $job->getId(),
+            ]),
+            'user' => $user,
+        ]);
+
+        /* @var JsonResponse $response */
+        $response = $this->getClientResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertInstanceOf(JsonResponse::class, $response);
+
+        $decodedResponseData = json_decode($response->getContent());
+        $this->assertEquals($expectedIsAuthorised, $decodedResponseData);
+    }
+
+    public function requestDataProvider(): array
+    {
+        return [
+            'owner=public, user=public' => [
+                'ownerName' => 'public',
+                'userName' => 'public',
+                'expectedIsAuthorised' => true,
+            ],
+            'owner=public, user=private' => [
+                'ownerName' => 'public',
+                'userName' => 'private',
+                'expectedIsAuthorised' => true,
+            ],
+            'owner=private, user=private' => [
+                'ownerName' => 'private',
+                'userName' => 'private',
+                'expectedIsAuthorised' => true,
+            ],
+            'owner=private, user=public' => [
+                'ownerName' => 'private',
+                'userName' => 'public',
+                'expectedIsAuthorised' => false,
+            ],
+        ];
+    }
+}
