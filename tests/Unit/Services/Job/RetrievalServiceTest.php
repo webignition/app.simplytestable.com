dd83618822e7e5c9c5a2533da3c640696aa6dda8
Jon Cram <jon@webignition.net>
2019-03-06 17:49:44 +0000

    Fix test

--- tests/Unit/Services/Job/RetrievalServiceTest.php
+++ tests/Unit/Services/Job/RetrievalServiceTest.php
@@ -2,51 +2,39 @@
 
 namespace App\Tests\Unit\Services\Job;
 
+use App\Services\Job\AuthorisationService;
 use Mockery\Mock;
 use App\Repository\JobRepository;
 use App\Services\Job\RetrievalService;
-use App\Services\Team\Service as TeamService;
 use App\Exception\Services\Job\RetrievalServiceException as JobRetrievalServiceException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use App\Tests\Factory\ModelFactory;
 
 class RetrievalServiceTest extends \PHPUnit\Framework\TestCase
 {
     public function testRetrieveFailure()
     {
-        $user = ModelFactory::createUser([
-            ModelFactory::USER_EMAIL => 'user1@example.com',
-        ]);
+        $jobId = 1;
 
-        /* @var Mock|TeamService $teamService */
-        $teamService = \Mockery::mock(TeamService::class);
+        /* @var TokenStorageInterface|Mock $tokenStorage */
+        $tokenStorage = \Mockery::mock(TokenStorageInterface::class);
+
+        /* @var AuthorisationService $authorisationService */
+        $authorisationService = \Mockery::mock(AuthorisationService::class);
 
         /* @var Mock|JobRepository $jobRepository */
         $jobRepository = \Mockery::mock(JobRepository::class);
         $jobRepository
-            ->shouldReceive('find')
-            ->andReturnNull();
+            ->shouldReceive('exists')
+            ->with($jobId)
+            ->andReturn(false);
 
-        /* @var Mock|TokenInterface $token */
-        $token = \Mockery::mock(TokenInterface::class);
-        $token
-            ->shouldReceive('getUser')
-            ->andReturn($user);
-
-        /* @var TokenStorageInterface|Mock $tokenStorage */
-        $tokenStorage = \Mockery::mock(TokenStorageInterface::class);
-        $tokenStorage
-            ->shouldReceive('getToken')
-            ->andReturn($token);
-
-        $retrievalService = new RetrievalService($teamService, $tokenStorage, $jobRepository);
+        $retrievalService = new RetrievalService($tokenStorage, $jobRepository, $authorisationService);
 
         $this->expectException(JobRetrievalServiceException::class);
         $this->expectExceptionMessage('Job [1] not found');
         $this->expectExceptionCode(JobRetrievalServiceException::CODE_JOB_NOT_FOUND);
 
-        $retrievalService->retrieve(1);
+        $retrievalService->retrieve($jobId);
     }
 
     /**
