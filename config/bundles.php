ccef29ff4080aff5a5c6b207d0d7afe89c3eea52
Jon Cram <webignition@gmail.com>
2019-03-07 10:45:23 +0000

    Revert "Update cron/cron-bundle (#460)"
    This reverts commit fe264819ecf3debb171c47616a3486fe78d70439.

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,7 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Cron\CronBundle\CronCronBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
@@ -12,5 +13,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     ResqueBundle\Resque\ResqueBundle::class => ['all' => true],
-    Cron\CronBundle\CronCronBundle::class => ['all' => true],
 ];
