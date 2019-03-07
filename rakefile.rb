1897b0d08fd4c9f5d7f5901cf23b7dace7903e94
Jon Cram <jon@webignition.net>
2019-03-07 12:25:03 +0000

    Remove old rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,11 @@
 require 'fileutils'
 
-task :default do  
-  get_composer  
+task :default do
+  get_composer
   run_commands([
     "git pull",
     "export SYMFONY_ENV=prod && ./composer.phar install",
-    "rm -Rf app/cache/prod/*",    
+    "rm -Rf app/cache/prod/*",
     "export SYMFONY_ENV=prod && php app/console cache:warmup"
   ])
 end
@@ -24,6 +24,6 @@ end
 
 def run_commands(commands)
   commands.each do|command|
-    puts `#{command}`    
-  end   
+    puts `#{command}`
+  end
 end
