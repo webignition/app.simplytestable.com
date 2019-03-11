323907665d49969a8a1da2d040d0710d530d0789
Jon Cram <jon@webignition.net>
2019-03-11 19:01:57 +0000

    /job/{test_id}/

--- tests/Functional/Controller/Job/Job/JobControllerStatusActionTest.php
+++ tests/Functional/Controller/Job/Job/JobControllerStatusActionTest.php
@@ -11,6 +11,23 @@ class JobControllerStatusActionTest extends AbstractJobControllerTest
 {
     const CANONICAL_URL = 'http://example.com/';
 
+    public function testStatusActionGetRequestShortRoute()
+    {
+        $job = $this->jobFactory->create([
+            JobFactory::KEY_URL => 'http://example.com',
+        ]);
+
+        $this->getCrawler([
+            'url' => self::$container->get('router')->generate('job_job_status_short', [
+                'test_id' => $job->getId(),
+            ])
+        ]);
+
+        $response = $this->getClientResponse();
+
+        $this->assertTrue($response->isSuccessful());
+    }
+
     public function testStatusActionGetRequest()
     {
         $job = $this->jobFactory->create([
