992d76dfced1bf06efcb0b17ec17cba31fe0a77c
Jon Cram <jon@webignition.net>
2019-04-22 19:06:29 +0100

    Remove cron/cron-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Cron\CronBundle\CronCronBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
